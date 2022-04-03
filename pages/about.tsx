4c3ae74e03b3f708095e9d47f56ae178cf39db9b
jettblu <jettblu.fabrications@gmail.com>
2022-04-02 23:52:09 -0400

    Add about content

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,7 @@ const About: NextPage = () => {
   return (
     <div className="h-screen">
       <Head>
-        <title>Art</title>
+        <title>About</title>
         <meta name="description" content="Crypto made simple." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -21,12 +21,13 @@ const About: NextPage = () => {
           {/* padding div for space between top and main elements */}
         </div>
       
-
-        <div className="text-center max-w-2xl mx-auto content-center">
-           <p>Hey, this is an about page!</p>
-        </div>
-
-
+      <div className="lg:px-[30%]">
+        <h1 className="text-4xl font-bold sans mb-5">
+              About Kryptik
+        </h1>
+        <p className="leading-loose mb-2 text-justify">Key pairs can do wonderful things, but existing wallets like Metamask and Rainbow confine their magic to a single blockchain. The Kryptik wallet is built to break through the borders of Ethereum and streamline digital ownership across the internet.</p>
+        <p className="leading-loose text-justify"> One <span className="hover:text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-green-500 font-bold">secure</span> wallet. One <span className="hover:text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-green-500 font-bold">simple</span> interface. One <span className="hover:text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-green-500 font-bold">magical</span> future.</p>
+      </div>
 
       </main>
 
