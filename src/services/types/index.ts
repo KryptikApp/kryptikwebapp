4ef956160769d7d035f6d7282a0bab6cc9974b1e
jettblu <jettblu.fabrications@gmail.com>
2022-04-05 02:07:06 -0400

    Add service enum types

---
+++ src/services/types/index.ts
@@ -0,0 +1,16 @@
+enum ServiceState{
+    started=0,
+    stopped=1,
+    unstarted=2
+}
+
+enum Status
+{
+    Success = 0,
+    Failure = 1,
+    Pending = 2,
+    Done = 3
+}
+
+export{ServiceState, Status}
+
