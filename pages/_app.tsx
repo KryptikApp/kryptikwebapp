d82e2192fcdebe7dd65151ccc1c4fd005b15b522
jettblu <jettblu.fabrications@gmail.com>
2022-04-08 00:46:23 -0400

    Add layout wrapper to main app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../components/AuthProvider'
+import Layout from '../components/Layout'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <AuthProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AuthProvider>
   )
 }
