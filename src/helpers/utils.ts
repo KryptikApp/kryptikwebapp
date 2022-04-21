99392a9fc27a7c27d13e961feed73bc075f83905
jettblu <jettblu.fabrications@gmail.com>
2022-04-21 00:13:38 -0400

    Add local jwt creator

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,7 +1,10 @@
 import { BigNumber, ethers } from "ethers"; 
+import * as jose from "jose"
 import { Web3Provider } from "@ethersproject/providers";
 import HDSeedLoop, { HDKeyring, Network, NetworkFamily} from "hdseedloop"
 import Web3Service from "./../services/Web3Service";
+import { firebaseConfig, firebaseserviceKey } from "../../secrets";
+import { arrayify } from "ethers/lib/utils";
 
 
 
@@ -18,5 +21,18 @@ export const hexToInt = (s: string) => {
 export const reloadApp = () => {
   window.location.reload();
 };
+
+// creates custom token for firebase auth 
+export const createCustomFirebaseToken = async(uid:string):Promise<string> => {
+  const jwt = await new jose.SignJWT({ 'uid': uid })
+  .setProtectedHeader({ alg: 'RS256' })
+  .setIssuedAt()
+  .setSubject('kryptikapp@gmail.com')
+  .setIssuer('kryptikapp@gmail.com')
+  .setAudience('https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit')
+  .setExpirationTime('1h')
+  .sign(arrayify(firebaseserviceKey.private_key));
+  return jwt;
+}
 
 
