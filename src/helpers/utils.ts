326872ee4d7d2bf3896d088b51f104d4739744d0
jettblu <jettblu.fabrications@gmail.com>
2022-04-22 17:36:52 -0400

    Fix custom token generation

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -4,7 +4,7 @@ import { Web3Provider } from "@ethersproject/providers";
 import HDSeedLoop, { HDKeyring, Network, NetworkFamily} from "hdseedloop"
 import Web3Service from "./../services/Web3Service";
 import { firebaseConfig, firebaseserviceKey } from "../../secrets";
-import { arrayify } from "ethers/lib/utils";
+import * as crypto from "crypto"
 
 
 
@@ -22,17 +22,35 @@ export const reloadApp = () => {
   window.location.reload();
 };
 
+
 // creates custom token for firebase auth 
 export const createCustomFirebaseToken = async(uid:string):Promise<string> => {
+  // let arrayKey:Uint8Array = pemToArray(firebaseserviceKey.private_key);
+  let keyObj = crypto.createPrivateKey(firebaseserviceKey.private_key);
+  // create jwt
   const jwt = await new jose.SignJWT({ 'uid': uid })
   .setProtectedHeader({ alg: 'RS256' })
   .setIssuedAt()
-  .setSubject('kryptikapp@gmail.com')
-  .setIssuer('kryptikapp@gmail.com')
+  .setSubject('firebase-adminsdk-m4jam@kryptikapp-50542.iam.gserviceaccount.com')
+  .setIssuer('firebase-adminsdk-m4jam@kryptikapp-50542.iam.gserviceaccount.com')
   .setAudience('https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit')
   .setExpirationTime('1h')
-  .sign(arrayify(firebaseserviceKey.private_key));
+  .sign(keyObj);
   return jwt;
+}
+
+// helper function that removes line breaks in string
+function removeLines(str:string) {
+  return str.replace("\n", "");
+}
+
+// transforms key in pem form to a uint8array
+function pemToArray(pem:string):Uint8Array{
+  pem = removeLines(pem);
+  pem = pem.replace('-----BEGIN PRIVATE KEY-----', '');
+  pem = pem.replace('-----END PRIVATE KEY-----', '');
+  let arrayKey:Buffer = Buffer.from(pem, "base64");
+  return Uint8Array.from(arrayKey);
 }
 
 
