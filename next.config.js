a9ffef757a1cbe49cff7335e6b90eaf68f8a9607
jettblu <jettblu.fabrications@gmail.com>
2022-04-25 12:41:17 -0400

    Update webconfig settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: function (config, options) {
+    console.log(options.webpack.version); // 5.18.0
+    config.experiments = { asyncWebAssembly: true };
+    return config;
+  },
 }
 
 module.exports = nextConfig
