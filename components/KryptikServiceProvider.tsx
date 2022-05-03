9e1710abc085d8a8e54ea99032207bd1a7dbf114
jettblu <jettblu.fabrications@gmail.com>
2022-05-02 23:43:39 -0400

    Update service provider context

--- components/KryptikServiceProvider.tsx
+++ components/KryptikServiceProvider.tsx
@@ -1,49 +1,30 @@
 import { createContext, useContext, useState } from 'react'
-import { defaultWallet } from '../models/defaultWallet';
-import { IWallet } from '../models/IWallet';
+import { useKryptikWeb3Service } from '../src/helpers/web3Helper';
 import Web3Service from '../src/services/Web3Service';
 
 // NOTE: lots of projects use redux to track data updates, but react conext works!
 
 let defaultState = {
     kryptikService: new Web3Service(),
-    setKryptikService: (newService:Web3Service) => {},
+    loading: false,
+    clear: () => {},
 }
 
 // Create Context object
-const KryptikContext = createContext(defaultState);
+const KryptikServiceContext = createContext(defaultState);
 
 // Export Provider
 export function KryptikServiceProvider(props:any) {
 	const {value, children} = props
-    const setKryptik = (newKryptikService:Web3Service) => {
-        console.log("Kryptik service provider seting wallet....");
-        setKryptikState({...kryptikState, kryptikService:newKryptikService})
-    }
-    
-    // this allows us to update wallet from within a child component. Heck yeah!
-    let initState={
-        kryptikService: new Web3Service(),
-        setKryptikService: setKryptik
-    }
-    
-    new Web3Service().StartSevice().then(ks=>{
-        initState = {
-            kryptikService: ks,
-            setKryptikService: setKryptik
-        } 
-    });
-        
-    const [kryptikState, setKryptikState] = useState(initState)
-	
+    let kryptikServiceState = useKryptikWeb3Service();
 	return (
-	   <KryptikContext.Provider value={kryptikState}>
+	   <KryptikServiceContext.Provider value={kryptikServiceState}>
 		{children}
-	   </KryptikContext.Provider>
+	   </KryptikServiceContext.Provider>
 	)
 }
 
 // Export useContext Hook.
-export function useKryptikContext() {
-	return useContext(KryptikContext);
+export function useKryptikServiceContext() {
+	return useContext(KryptikServiceContext);
 }
