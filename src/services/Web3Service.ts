8a78d3922774a40923dcc8427055978ff7099d23
jettblu <jettblu.fabrications@gmail.com>
2022-05-02 23:45:49 -0400

    Add wallet connector to web 3 service

--- src/services/Web3Service.ts
+++ src/services/Web3Service.ts
@@ -12,8 +12,9 @@ import {
     Connection,
   } from '@solana/web3.js';
 
-import HDSeedLoop, { Network, NetworkFamily, NetworkFromTicker, SeedLoop } from "hdseedloop";
+import HDSeedLoop, { Network, NetworkFamily, NetworkFromTicker, SeedLoop, SerializedSeedLoop } from "hdseedloop";
 import { IWallet } from "../../models/IWallet";
+import { defaultWallet } from "../../models/defaultWallet";
 
 const NetworkDbsRef = collection(firestore, "networks")
 
@@ -73,17 +74,62 @@ class Web3Service extends BaseService{
     }
 
     // set wallet for kryptik web 3 service
-    setWallet(inputWallet:IWallet):IWallet{
+    // should only be used by functions within class
+    // use connect wallet to set wallet 
+    private internalSetWallet(inputWallet:IWallet):IWallet{
         this.wallet = inputWallet;
         this.isWalletSet = true;
         return this.wallet;
     }
+
 
     // get wallet for kryptik web 3 service
     getWallet():IWallet|null{
         if(!this.isWalletSet) return null;
         return this.wallet;
     }
+
+    connectKryptikWallet = async (seed?:string): Promise<IWallet> => {   
+        // check for kryptik wallet in local storage
+        let existingWallet = window.localStorage.getItem("kryptikWallet");
+        let seedloopKryptik:HDSeedLoop;
+        // if there is already a seedloop available... use it!
+        if(existingWallet){
+          let currSeedloop:SerializedSeedLoop = JSON.parse(existingWallet);
+          seedloopKryptik = HDSeedLoop.deserialize(currSeedloop);
+        }
+        else{
+            // use imported seed to create seedloop
+            if(seed){
+              seedloopKryptik = new HDSeedLoop({mnemonic:seed})
+            }
+            else{
+              seedloopKryptik = new HDSeedLoop();
+            }
+            
+        }
+        let ethNetwork = NetworkFromTicker("eth");
+        // get all ethereum addreses for wallet
+        let etheAddysAll = await seedloopKryptik.getAddresses(ethNetwork);
+        let ethAddyFirst = etheAddysAll[0];
+        let newKryptikWallet:IWallet = {
+            ...defaultWallet,
+            walletProviderName: "kryptik",
+            connected: true,
+            seedLoop: seedloopKryptik,
+            ethAddress: ethAddyFirst
+        };
+        console.log("New kryptik wallet:");
+        console.log(newKryptikWallet);
+        // serialize seed loop for storage
+        let hdseedloopSerialized:SerializedSeedLoop =  await newKryptikWallet.seedLoop.serialize();
+        // uncomment to save seed loop in local storage
+        // window.localStorage.setItem("kryptikWallet", JSON.stringify(hdseedloopSerialized));
+        // set wallet for internal use
+        this.internalSetWallet(newKryptikWallet);
+        // returns new kryptik wallet that adheres to wallet interface
+        return newKryptikWallet;
+    };
 
     async InternalStartService(){
         try{
