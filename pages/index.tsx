bb264ea8ee657712c7891b64b1d336cb3ab00c57
jettblu <jettblu.fabrications@gmail.com>
2022-05-02 23:44:06 -0400

    Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Splash.module.css'
-import Link from 'next/link'
-import Navbar from '../components/Navbar'
-import { useState } from 'react'
+import { useAuthContext } from '../components/AuthProvider'
 
 const Home: NextPage = () => {
-
+  const authContext = useAuthContext();
   const handleGetStarted = () =>{
     console.log("get started!");
   }
@@ -28,6 +24,7 @@ const Home: NextPage = () => {
           <button onClick={()=>handleGetStarted()} className="bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded my-5">
                             Get Started
           </button>
+          <p>{authContext.authUser.uid}</p>
         </div>
 
     </div>
