bca10cb6dc48cb94afd0547e23303e7e0f4be13c
jettblu <jettblu.fabrications@gmail.com>
2022-05-16 15:00:57 -0700

    Update navprofile colors

--- components/NavProfile.tsx
+++ components/NavProfile.tsx
@@ -11,31 +11,31 @@ const NavProfile:NextPage = () => {
     console.log(router.pathname)
         
     return(
-        <nav className="fixed bottom-0 inset-x-0 bg-white-400 border flex justify-between text-sm text-blue-600 hover:cursor-pointer">
+        <nav className="fixed bottom-0 inset-x-0 bg-slate-50 flex justify-between text-sm text-sky-600 hover:cursor-pointer">
 
         <Link href="../profile/security">
-            <div  className="w-full block py-5 px-3 text-center hover:bg-blue-200 hover:text-blue-800">
+            <div  className="w-full block py-5 px-3 text-center hover:bg-slate-200 hover:text-sky-800">
                 <AiOutlineLock className="w-6 h-6 mb-2 mx-auto" size="20"/>
-                <span className={`${router.pathname == "/profile/security" ? "font-bold" : ""}`}>Wallet</span>
+                <span className={`${router.pathname == "/profile/security" ? "font-bold" : ""}`}>Security</span>
             </div>
         </Link>
         
         <Link href="../profile/">
-            <div  className="w-full block py-5 px-3 text-center hover:bg-blue-200 hover:text-blue-800">
+            <div  className="w-full block py-5 px-3 text-center hover:bg-slate-200 hover:text-sky-800">
                 <AiOutlineUser className="w-6 h-6 mb-2 mx-auto" size="20"/>
                 <span className={`${router.pathname == "/profile" ? "font-bold" : ""}`}>Profile</span>
             </div>
         </Link>
     
         <Link href="../profile/avatar">
-            <div  className="w-full block py-5 px-3 text-center hover:bg-blue-200 hover:text-blue-800">
+            <div  className="w-full block py-5 px-3 text-center hover:bg-slate-200 hover:text-sky-800">
                 <AiOutlineCamera className="w-6 h-6 mb-2 mx-auto" size="20"/>
                 <span className={`${router.pathname == "/profile/avatar" ? "font-bold" : ""}`}>Avatar</span>
             </div>
         </Link>
     
         <Link href="../profile/settings">
-            <div  className="w-full block py-5 px-3 text-center hover:bg-blue-200 hover:text-blue-800">
+            <div  className="w-full block py-5 px-3 text-center hover:bg-slate-200 hover:text-sky-800">
                 <AiOutlineSetting className="w-6 h-6 mb-2 mx-auto" size="20"/>
                 <span className={`${router.pathname == "/profile/settings" ? "font-bold" : ""}`}>Settings</span>
             </div>
