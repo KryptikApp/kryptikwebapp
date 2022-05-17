9c553b3359d5fdbb442f4a0608b57f0559bd3335
jettblu <jettblu.fabrications@gmail.com>
2022-05-16 23:18:34 -0700

    Update avatar file loader

--- pages/profile/avatar.tsx
+++ pages/profile/avatar.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { getDownloadURL, ref, StorageReference, uploadBytes } from 'firebase/storage'
 
-import Divider from '../../components/Divider'
-import { storage } from '../../src/helpers/firebaseHelper'
+
+import { generateStoragePath, storage } from '../../src/helpers/firebaseHelper'
 import { useKryptikAuthContext } from '../../components/KryptikAuthProvider'
 import NavProfile from '../../components/NavProfile'
 import { getFileName } from '../../src/helpers/utils'
@@ -25,6 +25,7 @@ const Profile: NextPage = () => {
   let fileInit:Blob = new Blob();
   //UNCOMMENT for file uploads
   const [imageFile, setImageFile] = useState(fileInit);
+  const [loadingUpdate, setloadingUpdate] = useState(false);
 
   // uploads file to device
   const uploadToClient = (event:any) => {
@@ -49,12 +50,10 @@ const Profile: NextPage = () => {
   const uploadToRemote = async():Promise<string>=>{
     console.log("firebase image upload starting....");
     let fileName:string = getFileName(imageUrl);
-    let storageFilePath:string = `avatars/${authUser.uid}/${fileName}`
+    let storageFilePath:string = generateStoragePath(fileName, authUser);
     const storageRef = ref(storage, storageFilePath);
     // placeholder for upload return url
     let imageUploadUrl:string = imageUrl;
-    // upload image to firebase
-    console.log("Uploading bytes...");
     // upload file to db
     await uploadBytes(storageRef, imageFile);
     // get url for upload
@@ -64,13 +63,15 @@ const Profile: NextPage = () => {
 
 
   const handleClickUpload = async() =>{
+    setloadingUpdate(true);
+    // don't upload if still in upload process
+    if(loadingUpdate) return;
     // upload file to firebase
     let urlImageUpload:string = await uploadToRemote();
     authUser.photoUrl = urlImageUpload;
-    console.log("Avatar url:");
-    console.log(urlImageUpload);
     // update user's profile photo
     await updateCurrentUserKryptik(authUser);
+    setloadingUpdate(false);
   }
 
   return (
@@ -111,14 +112,20 @@ const Profile: NextPage = () => {
                   <p className="text-slate-500 text-sm">Your avatar will be shown to other Kryptik users. Square images work best.</p>
                     {/* upload button */}
                     <div className="item-end">
-                      <button onClick={()=>handleClickUpload()} className="bg-transparent hover:bg-sky-400 text-sky-500 font-semibold hover:text-white py-2 px-4 border border-sky-500 hover:border-transparent rounded my-5">
+                      <button onClick={()=>handleClickUpload()} className={`bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-4 ${loadingUpdate?"hover:cursor-not-allowed":""} border border-green-500 hover:border-transparent rounded-lg my-5`} disabled={loadingUpdate}>
                         Upload
+                        {
+                                    !loadingUpdate?"":
+                                    <svg role="status" className="inline w-4 h-4 ml-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                                    </svg>
+                        }
                       </button>
                     </div>     
         </div>
     </div>
            
-
     <div className="h-[7rem]">
       {/* padding div for space between top and main elements */}
     </div>
