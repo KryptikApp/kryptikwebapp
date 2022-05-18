c68de6c99bbb8229a08e0f2b68d84f308d7d678c
jettblu <jettblu.fabrications@gmail.com>
2022-05-18 16:19:45 -0700

    Delete redundant firebase functions and models

--- src/helpers/firebaseHelper.ts
+++ src/helpers/firebaseHelper.ts
@@ -7,7 +7,7 @@ import { firebaseConfig } from "../../secrets";
 import { signInWithCustomToken, User, UserCredential } from "firebase/auth";
 import { useEffect, useState } from "react";
 
-import { UserDB } from "../models/user"
+import { defaultUser, UserDB, UserExtraData } from "../models/user"
 
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -23,11 +23,6 @@ export  {
 
 // user auth helper code
 
-// interface for extra user data
-export interface UserExtraData{
-  isTwoFactorAuth: boolean,
-  remoteShare: string
-}
 
 export const formatAuthUser = function(user:any):UserDB
 {
@@ -35,6 +30,7 @@ export const formatAuthUser = function(user:any):UserDB
         uid: user.uid,
         email: user.email,
         name: user.displayName,
+        bio: user.bio,
         photoUrl: user.photoURL,
       };
 };
@@ -45,13 +41,15 @@ export const formatUserExtraData = function(docIn:DocumentSnapshot<DocumentData>
     if(dataIn){
       formatted = {
         isTwoFactorAuth: dataIn.isTwoFactorAuth,
-        remoteShare: dataIn.remoteShare
+        remoteShare: dataIn.remoteShare,
+        bio: dataIn.bio
       }
     }
     else{
       formatted = {
         isTwoFactorAuth: false,
-        remoteShare: ""
+        remoteShare: "",
+        bio: ""
       }
     }
     return formatted;
@@ -95,72 +93,27 @@ export const getUserPhotoPath = function(user:UserDB):string{
 }
 
 export function useFirebaseAuth() {
-    //create dummy user of type userDB
-    let dummyUser:UserDB = {
-        uid: "",
-        email:"",
-        name: "",
-        photoUrl: ""
-
-    }
     // init state 
-    const [authUser, setAuthUser] = useState(dummyUser);
+    const [authUser, setAuthUser] = useState(defaultUser);
     const [loading, setLoading] = useState(true);
 
     // clear current auth state
     const clear = () => {
-      setAuthUser(dummyUser);
+      setAuthUser(defaultUser);
       setLoading(true);
-    };
-
-    // routine to run when auth. state changes
-    const authStateChanged = async (user:any) => {
-        console.log("AUTH State changed!");
-        console.log(user);
-        if (!user) {
-          setAuthUser(dummyUser)
-          setLoading(false)
-          return;
-        }
-    
-        setLoading(true)
-        var formattedUser = formatAuthUser(user);
-        setAuthUser(formattedUser);    
-        setLoading(false);
     };
     
     const updateCurrentUserKryptik = async(user:User)=>{
       await updateCurrentUser(firebaseAuth, user);
     }
 
-    const signInWithToken = async(customToken:string, data:UserExtraData|null) =>
-    {
-        let signInCred:UserCredential = await signInWithCustomToken(firebaseAuth, customToken);
-        let dbUser:UserDB = formatAuthUser(signInCred.user);
-        let docRef = doc(firestore, "users", dbUser.uid);
-        // write extra data to database if not yet set
-        if(!(await getDoc(docRef)).exists() && data){
-            try{
-              await writeExtraUserData(dbUser, data)
-            }
-            catch(err){
-              console.log(err);
-            }
-        }
-    }
   
     const signOut = () =>
       firebaseAuth.signOut().then(clear);
   
-    useEffect(() => {
-      const unsubscribe = firebaseAuth.onAuthStateChanged(authStateChanged);
-      return () => unsubscribe();
-    }, []);
-  
     return {
       authUser,
       loading,
-      signInWithToken,
       updateCurrentUserKryptik,
       signOut
     };
