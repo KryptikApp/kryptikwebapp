5160cbd2c924359b37c61a940c4d5bd4c2b20ed8
jettblu <jettblu.fabrications@gmail.com>
2022-05-18 16:19:18 -0700

    Update profile header display in wallet

--- pages/wallet/index.tsx
+++ pages/wallet/index.tsx
@@ -4,11 +4,10 @@ import { useRouter } from 'next/router'
 import Divider from '../../components/Divider'
 import { useKryptikAuthContext } from '../../components/KryptikAuthProvider'
 import ListBalance from '../../components/lists/ListBalance'
-import Link from 'next/link'
 import HeaderProfile from '../../components/HeaderProfile'
 
 const Profile: NextPage = () => {
-  const { authUser, loading, kryptikWallet, kryptikService, getUserPhotoPath } = useKryptikAuthContext();
+  const { authUser, loading } = useKryptikAuthContext();
   const router = useRouter();
   // ROUTE PROTECTOR: Listen for changes on loading and authUser, redirect if needed
   useEffect(() => {
@@ -21,7 +20,7 @@ const Profile: NextPage = () => {
     <div>
       
         <div className="text-center max-w-2xl mx-auto content-center">
-          <HeaderProfile user={authUser}/>
+          <HeaderProfile user={authUser} showBio={false} center={true}/>
           <div className="flex justify-start mt-5">
             <h2 className="font-medium text-slate-700">Your Balances</h2>
           </div>
