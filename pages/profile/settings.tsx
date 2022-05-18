365ac033ae07e6e38fa0ab361b08796a18152481
jettblu <jettblu.fabrications@gmail.com>
2022-05-18 16:16:46 -0700

    Update signout from settings page

--- pages/profile/settings.tsx
+++ pages/profile/settings.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import Divider from '../../components/Divider'
 import { useKryptikAuthContext } from '../../components/KryptikAuthProvider'
 import NavProfile from '../../components/NavProfile'
+import toast, { Toaster } from 'react-hot-toast'
 
 
 const Settings: NextPage = () => {
-  const { authUser, loading, getUserPhotoPath, signOut } = useKryptikAuthContext();
+  const { authUser, loading, signOut } = useKryptikAuthContext();
   const router = useRouter();
   // ROUTE PROTECTOR: Listen for changes on loading and authUser, redirect if needed
   useEffect(() => {
@@ -15,15 +15,36 @@ const Settings: NextPage = () => {
       router.push('/')
   }, [authUser, loading])
 
+  const handleLogout = function(){
+    try{
+      signOut();
+      router.push('/');
+    }
+    catch(e){
+      toast.error("Unable to sign out. Please contact support.");
+    }
+  }
+
 
   return (
     <div>
-      
-        <div className="text-center max-w-2xl mx-auto content-center">
-          <h1>Settings</h1>
+        <div className="h-[2rem]">
+        {/* padding div for space between top and main elements */}
         </div>
 
-        
+        <div className="lg:px-[30%]">
+          <h1 className="text-4xl font-bold sans mb-5">
+                  Settings
+          </h1>
+          <p className="mb-2 text-justify">Your Krytpik wallet settings will be managed on this page.</p>
+          <button onClick={()=>handleLogout()} className="bg-transparent hover:bg-red-500 text-black-500 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded my-5">
+                            Logout
+          </button>
+        </div>
+
+    <div className="h-[7rem]">
+      {/* padding div for space between top and main elements */}
+    </div> 
     <NavProfile/>
     </div>
  
