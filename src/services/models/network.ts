1ec1bac512113ffae9a0204594029a827b21a86c
jettblu <jettblu.fabrications@gmail.com>
2022-05-31 13:45:27 -0700

    Add default networkdb

--- src/services/models/network.ts
+++ src/services/models/network.ts
@@ -13,3 +13,18 @@ export interface NetworkDb {
     provider:string,
     coingeckoId:string
 }
+
+
+export const defaultNetworkDb:NetworkDb = {
+    fullName: "Ethereum",
+    ticker: "eth",
+    iconPath:"https://firebasestorage.googleapis.com/v0/b/kryptikapp-50542.appspot.com/o/eth.png?alt=media&token=cc1091fb-ef28-4008-a91e-5709818c452e",
+    isSupported: true,
+    about: "Ethereum is the community-run technology powering the cryptocurrency ether (ETH) and thousands of decentralized applications.",
+    whitePaperPath: "https://ethereum.org/en/whitepaper/",
+    chainId: 60,
+    hexColor: "#3c3c3d",
+    dateCreated: new Date('July 29, 2015 03:24:00'),
+    provider: "https://eth-mainnet.alchemyapi.io/v2/NnS19sbjsKljODizz9zB-C8Fw511M-ej",
+    coingeckoId: "ethereum"
+}
