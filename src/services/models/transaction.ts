4742c653695ab0cff45acc4278938d3fb280987b
jettblu <jettblu.fabrications@gmail.com>
2022-06-03 17:16:40 -0700

    Add transaction models

--- src/services/models/transaction.ts
+++ src/services/models/transaction.ts
@@ -1,5 +1,8 @@
+import { BigNumberish, BytesLike } from "ethers";
+import { AccessListish } from "ethers/lib/utils";
 import { Network } from "hdseedloop";
-import { defaultNetwork } from "./network";
+import { KryptikProvider } from "../Web3Service";
+import { defaultNetwork, NetworkDb } from "./network";
 
 export default interface TransactionFeeData{
     network: Network,
@@ -9,6 +12,9 @@ export default interface TransactionFeeData{
     lowerBoundUSD: number,
     upperBoundCrypto: number,
     upperBoundUsd: number,
+    gasLimit:number,
+    maxFeePerGas:number,
+    maxPriorityFeePerGas:number,
     isFresh: boolean
 }
 
@@ -18,5 +24,45 @@ export const defaultTransactionFeeData:TransactionFeeData = {
     lowerBoundCrypto: 0,
     lowerBoundUSD: 0,
     upperBoundCrypto: 0,
-    upperBoundUsd: 0
+    upperBoundUsd: 0,
+    gasLimit:0, 
+    maxFeePerGas: 0,
+    maxPriorityFeePerGas: 0,
+}
+
+export interface EVMTransaction{
+    sendAccount:string,
+    kryptikProvider:KryptikProvider,
+    networkDb:NetworkDb
+    toAddress:string,
+    // how much gas we're willing to use
+    gasLimit: string,
+    // max fee per gas unit we're willing to pay
+    maxFeePerGas: string,
+    // max tip per gas unit we're willing to pay
+    maxPriorityFeePerGas: string,
+    // value in token we are sending
+    value: string
+}
+
+export type TransactionRequest = {
+    to?: string,
+    from?: string,
+    nonce?: BigNumberish,
+
+    gasLimit?: BigNumberish,
+    gasPrice?: BigNumberish,
+
+    data?: BytesLike,
+    value?: BigNumberish,
+    chainId?: number
+
+    type?: number;
+    accessList?: AccessListish;
+
+    maxPriorityFeePerGas?: BigNumberish;
+    maxFeePerGas?: BigNumberish;
+
+    customData?: Record<string, any>;
+    ccipReadEnabled?: boolean;
 }
