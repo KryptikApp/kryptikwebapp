a42d7b5974ea87ec6aae3e78f969c533c46398ac
jettblu <jettblu.fabrications@gmail.com>
2022-06-04 17:04:15 -0700

    Add create solana transaction method

--- src/handlers/wallet/transactionHandler.ts
+++ src/handlers/wallet/transactionHandler.ts
@@ -1,7 +1,24 @@
-import { EVMTransaction, TransactionRequest } from "../../services/models/transaction"
+import { PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { solToLamports } from "../../helpers/wallet/utils";
+import { EVMTransaction, SolTransaction, TransactionRequest } from "../../services/models/transaction"
 
 
-
+export const createSolTransaction = async function(txIn:SolTransaction):Promise<Transaction>{
+    if(!txIn.kryptikProvider.solProvider) throw(new Error(`No provider set for ${txIn.networkDb.fullName}. Unable to create transaction.`))
+    let lastBlockHash = await txIn.kryptikProvider.solProvider.getLatestBlockhash('finalized');
+    let fromPubKey:PublicKey = new PublicKey(txIn.sendAccount);
+    let toPubKey:PublicKey = new PublicKey(txIn.toAddress);
+    var transaction:Transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: fromPubKey,
+          toPubkey: toPubKey,
+          lamports: solToLamports(txIn.valueSol) //Remember 1 Lamport = 10^-9 SOL.
+        }),
+    );
+    transaction.recentBlockhash = lastBlockHash.blockhash;
+    transaction.feePayer = fromPubKey;
+    return transaction;
+}
 
 export const createEVMTransaction = async function(txIn:EVMTransaction):Promise<TransactionRequest>{
     const {sendAccount, toAddress, value, gasLimit, maxFeePerGas, maxPriorityFeePerGas, kryptikProvider, networkDb} = txIn;
