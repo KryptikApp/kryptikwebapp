d141a832ae5630f04c8c06715b3ca2147fdb5d60
jettblu <jettblu.fabrications@gmail.com>
2022-06-06 14:53:09 -0700

    Add network from networkdb method

--- src/helpers/wallet/utils.ts
+++ src/helpers/wallet/utils.ts
@@ -1,3 +1,6 @@
+import { Network, NetworkFamily, NetworkFamilyFromFamilyName, NetworkParameters} from "hdseedloop";
+import { NetworkDb } from "../../services/models/network";
+
 export const roundCryptoAmount = function(amountIn:number):number{
     return Number(amountIn.toPrecision(4));
 }
@@ -12,4 +15,18 @@ export const lamportsToSol = function(amountIn:number):number{
 
 export const solToLamports = function(amountIn:number):number{
     return amountIn*1000000000;
+}
+
+export const networkFromNetworkDb = function(nw: NetworkDb):Network{
+    let networkFamilyNameIn = nw.networkFamilyName;
+    if(!networkFamilyNameIn){
+        networkFamilyNameIn = "evm";
+    }
+    let network:Network = new Network({
+        fullName: nw.fullName,
+        ticker: nw.ticker,
+        chainId: nw.chainId,
+        networkFamilyName: networkFamilyNameIn
+    })
+    return network;
 }
