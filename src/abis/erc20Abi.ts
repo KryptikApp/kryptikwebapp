8f94273f5f3d740c6e84ae29110b595d9ce715a0
jettblu <jettblu.fabrications@gmail.com>
2022-06-10 00:10:55 -0700

    Add basic erc20 abi

---
+++ src/abis/erc20Abi.ts
@@ -0,0 +1,16 @@
+let erc20Abi = [
+    // Some details about the token
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+  
+    // Get the account balance
+    "function balanceOf(address) view returns (uint)",
+  
+    // Send some of your tokens to someone else
+    "function transfer(address to, uint amount)",
+  
+    // An event triggered whenever anyone transfers to someone else
+    "event Transfer(address indexed from, address indexed to, uint amount)"
+  ];
+
+export{erc20Abi}
