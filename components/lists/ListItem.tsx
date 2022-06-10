1c16abcb1386c6663236b3c03fd5f7d218c4c8aa
jettblu <jettblu.fabrications@gmail.com>
2022-06-10 12:08:28 -0700

    Add secondary image to list item

--- components/lists/ListItem.tsx
+++ components/lists/ListItem.tsx
@@ -5,18 +5,23 @@ interface Props{
     title:string,
     subtitle:string,
     imgSrc:string,
+    imgSrcSecondary?:string,
     amount:string,
     amountUSD:string,
     networkCoinGecko: string
 }
 
 const ListItem:NextPage<Props> = (props) => {
-    const {title, subtitle, imgSrc, amount, amountUSD, networkCoinGecko} = props;
+    const {title, subtitle, imgSrc, amount, amountUSD, networkCoinGecko, imgSrcSecondary} = props;
     return(
-      <li key={title} className="py-3 sm:py-4">
+      <li key={`${title} ${Math.random()}`} className="py-3 sm:py-4">
           <div className="flex items-center space-x-4">
               <div className="flex-shrink-0">
-                  <img className="w-8 h-8 rounded-full" src={imgSrc} alt="List Image"/>
+                  <img className="w-8 h-8 rounded-full inline" src={imgSrc} alt={`${title} image`}/>
+                  {
+                    imgSrcSecondary &&
+                    <img className="w-4 h-4 -ml-2 drop-shadow-lg mt-4 rounded-full inline" src={imgSrcSecondary} alt={`${title} secondary image`}/>
+                  }
               </div>
               <div className="flex-1 min-w-0">
               <Link href={{ pathname: '../coins/coinInfo', query: { network:  networkCoinGecko} }}>
