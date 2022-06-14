a7b26b7b7fb4283798c889b0cdb75e349f05fbc5
jettblu <jettblu.fabrications@gmail.com>
2022-06-14 14:28:35 -0700

    Fix list item loading skeleton

--- components/lists/ListItemLoading.tsx
+++ components/lists/ListItemLoading.tsx
@@ -10,18 +10,18 @@ const ListItemLoading:NextPage = () => {
                   <div className="w-8 h-8 rounded-full bg-gray-500 animate-pulse"/>
               </div>
               <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 w-20 w-40 h-4 mb-2 truncate bg-gray-400 animate-pulse rounded">
+                  <p className="text-sm font-medium text-gray-900 w-20 w-60 mx-auto h-4 mb-2 truncate bg-gray-400 animate-pulse rounded">
 
                   </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400 w-10 h-2 bg-gray-400 animate-pulse rounded">
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400 w-20 mx-auto h-3 bg-gray-400 animate-pulse rounded">
 
                   </p>
               </div>
               <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate w-10 mb-2 h-4 bg-gray-400 animate-pulse rounded">
+                  <p className="text-sm font-medium text-gray-900 truncate w-20 mx-auto mb-2 h-4 bg-gray-400 animate-pulse rounded">
 
                   </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400 w-5 h-2 bg-gray-400 animate-pulse rounded">
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400 w-5 h-3 mx-auto bg-gray-400 animate-pulse rounded">
 
                   </p>
               </div>
