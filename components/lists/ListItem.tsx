9f802a0455ba0eb4b50897e047871d4abeee8e95
jettblu <jettblu.fabrications@gmail.com>
2022-06-21 11:03:07 -0700

    Add route to coin info page from list item

--- components/lists/ListItem.tsx
+++ components/lists/ListItem.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
 interface Props{
     title:string,
@@ -8,13 +9,26 @@ interface Props{
     imgSrcSecondary?:string,
     amount:string,
     amountUSD:string,
-    networkCoinGecko: string
+    coinGeckoId: string,
+    // used for headers to asset
+    infoLink?:boolean,
+    networkTicker?:string,
+    tokenTicker?:string
 }
 
+
 const ListItem:NextPage<Props> = (props) => {
-    const {title, subtitle, imgSrc, amount, amountUSD, networkCoinGecko, imgSrcSecondary} = props;
+    
+    const router = useRouter();
+    const {title, subtitle, imgSrc, amount, amountUSD, infoLink, imgSrcSecondary, networkTicker, tokenTicker} = props;
+    // redirect to asset info page if infolink provided
+    const handleOnClick = function(){
+        if(infoLink){
+            router.push({ pathname: '../coins/coinInfo', query:{networkTicker:networkTicker, tokenTicker:tokenTicker?tokenTicker:undefined} })
+        }
+    }
     return(
-      <li key={`${title} ${Math.random()}`} className="py-3 sm:py-4">
+      <li key={`${title} ${Math.random()}`} className="py-3 sm:py-4 hover:cursor-pointer" onClick={()=>handleOnClick()}>
           <div className="flex items-center space-x-4">
               <div className="flex-shrink-0">
                   <img className="w-8 h-8 rounded-full inline" src={imgSrc} alt={`${title} image`}/>
@@ -24,11 +38,13 @@ const ListItem:NextPage<Props> = (props) => {
                   }
               </div>
               <div className="flex-1 min-w-0">
-              <Link href={{ pathname: '../coins/coinInfo', query: { network:  networkCoinGecko} }}>
-                  <p className="text-sm font-medium text-gray-900 truncate">
-                      {title}
-                  </p>
-                </Link>
+
+                    <div>
+                        <p className="text-sm font-medium text-gray-900 truncate">
+                            {title}
+                        </p>
+                    </div>
+              
                   <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                       {subtitle}
                   </p>
