672aecd979de931f53b07654ae2aeb68659de761
jettblu <jettblu.fabrications@gmail.com>
2022-06-21 23:26:59 -0700

    Add seperate transaction fee component

--- pages/wallet/send.tsx
+++ pages/wallet/send.tsx
@@ -12,12 +12,15 @@ import { getPriceOfTicker } from '../../src/helpers/coinGeckoHelper'
 import Divider from '../../components/Divider'
 import { useKryptikAuthContext } from '../../components/KryptikAuthProvider'
 import DropdownNetworks from '../../components/DropdownNetworks'
-import TransactionFeeData, { CreateTransactionParameters, defaultTransactionFeeData, defaultTxPublishedData, FeeDataParameters, SolTransactionParams, TransactionPublishedData, TransactionRequest } from '../../src/services/models/transaction'
+import TransactionFeeData, { CreateTransactionParameters, defaultTransactionFeeData, defaultTxPublishedData, FeeDataParameters, SolTransactionParams, TransactionPublishedData, TransactionRequest, TxType } from '../../src/services/models/transaction'
 import { formatAmountUi, formatTicker, networkFromNetworkDb, roundCryptoAmount, roundToDecimals, roundUsdAmount } from '../../src/helpers/wallet/utils'
 import { createSolTokenTransaction, createSolTransaction } from '../../src/handlers/wallet/transactionHandler'
 import { Transaction} from '@solana/web3.js'
 import { TokenParamsSpl } from '../../src/services/models/token'
 import {handlePublishTransaction} from '../../src/handlers/wallet/sendHandler'
+import TxFee from '../../components/transactions/TxFee'
+import { KeyPairEd25519 } from 'near-api-js/lib/utils/key_pair'
+import { baseEncode } from 'borsh'
 
 
 
@@ -78,7 +81,7 @@ const Send: NextPage = () => {
 }, [selectedTokenAndNetwork]);
 
   // retrieves wallet balances
-  const fetchFromAddress = async() =>{
+  const fetchFromAddress = async():Promise<string> =>{
      let accountAddress = await kryptikService.getAddressForNetworkDb(kryptikWallet, selectedTokenAndNetwork.baseNetworkDb);
      let network:Network = networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb);
       // handle empty address
@@ -87,10 +90,11 @@ const Send: NextPage = () => {
         setFromAddress(kryptikWallet.ethAddress);      
         setReadableFromAddress(truncateAddress(kryptikWallet.ethAddress, network));
         setSelectedTokenAndNetwork(defaultTokenAndNetwork);
-        return;
+        return kryptikWallet.ethAddress;
       }
       setFromAddress(accountAddress);
       setReadableFromAddress(truncateAddress(accountAddress, network));
+      return accountAddress;
   }
 
   // get price for selected token
@@ -104,13 +108,33 @@ const Send: NextPage = () => {
   // get fees for selected network
   const fetchNetworkFees = async(solTx?:Transaction) =>{
     setFeesLoaded(false);
+    // if token data present.. tx is a token transfer.... otherwise tx type is a native coin transfer
+    let txType:TxType = selectedTokenAndNetwork.tokenData?TxType.TransferToken:TxType.TransferNative;
+    let nw:Network =  networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb);
+    // use instead of from address as react state may not have updated
+    let accountAddress = await kryptikService.getAddressForNetworkDb(kryptikWallet, selectedTokenAndNetwork.baseNetworkDb);
+    let nearPubKey:string|undefined;
+    // get special address for near
+    if(nw.networkFamily == NetworkFamily.Near){
+      let tokenWallet = kryptikWallet.seedLoop.getWalletForAddress(nw, accountAddress);
+      if(!tokenWallet) return;
+      let keyPair:nacl.SignKeyPair = tokenWallet.createKeyPair();
+      let nearKey:KeyPairEd25519 = new KeyPairEd25519(baseEncode(keyPair.secretKey));
+      nearPubKey = nearKey.publicKey.toString();
+    }
     let feeDataParams:FeeDataParameters = {
       networkDb: selectedTokenAndNetwork.baseNetworkDb,
       tokenData: selectedTokenAndNetwork.tokenData,
       amountToken: amountCrypto,
+      sendAccount: accountAddress,
+      nearPubKeyString: nearPubKey,
+      txType: txType,
       solTransaction: solTx
     }
+    // fee data returned from service
     let transactionFeeDataFresh:TransactionFeeData|null = await kryptikService.getTransactionFeeData(feeDataParams);
+    console.log("Transaction fee data received");
+    console.log(transactionFeeDataFresh);
     if(transactionFeeDataFresh){
       setTransactionFeedata(transactionFeeDataFresh);
     }
@@ -129,8 +153,10 @@ const Send: NextPage = () => {
       if(nw.networkFamily != NetworkFamily.Solana){
         return;
       }
+      let accountAddress:string = await kryptikService.getAddressForNetworkDb(kryptikWallet, selectedTokenAndNetwork.baseNetworkDb);
+      
       let txIn:SolTransactionParams = {
-        sendAccount: fromAddress,
+        sendAccount: accountAddress,
         toAddress: toAddress==""?toAddress:placeHolderSolAddress,
         valueSol: Number(amountCrypto),
         kryptikProvider: kryptikProvider,
@@ -395,59 +421,14 @@ const Send: NextPage = () => {
                 <div className="max-w-xs mx-auto">
                     <DropdownNetworks selectedTokenAndNetwork={selectedTokenAndNetwork} selectFunction={setSelectedTokenAndNetwork} onlyWithValue={true} onLoadedFunction={handleDropdownLoaded}/>
                 </div>
-              {/* skeleton fee loader */}
+              
+              {/* show fees when dropdown is loaded */}
               {
-                (!dropdownLoaded) &&
+                dropdownLoaded?
+                <TxFee txFeeData={transactionFeeData} tokenAndNetwork={selectedTokenAndNetwork} feesLoaded={feesLoaded}/>:
                 <div className="w-40 h-6 mt-2 truncate bg-gray-300 animate-pulse rounded mx-auto"/>
               }
-              {/* case: network family is ethereum and network fees are different */}
-              {
-                  (transactionFeeData.isFresh && dropdownLoaded &&
-                  networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb).networkFamily!=NetworkFamily.Solana) && networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb).networkFamily==NetworkFamily.EVM && (transactionFeeData.lowerBoundUSD != transactionFeeData.upperBoundUSD) &&
-                  <div>
-                    <p className="text-slate-400 text-sm inline">Fees: {`$${roundUsdAmount(transactionFeeData.lowerBoundUSD)}-$${roundUsdAmount(transactionFeeData.upperBoundUSD)}`}</p>
-                      {/* spinner to show fees are updating */}
-                      {
-                        !feesLoaded &&
-                        <svg role="status" className="inline w-4 h-4 ml-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
-                        </svg>
-                      }
-                  </div>
-                  
-              }
-              {/* case: network family is ethereum and network fees are same */}
-              {
-                  (transactionFeeData.isFresh && dropdownLoaded &&
-                  networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb).networkFamily!=NetworkFamily.Solana) && networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb).networkFamily==NetworkFamily.EVM && (transactionFeeData.lowerBoundUSD == transactionFeeData.upperBoundUSD) &&
-                  <div>
-                    <p className="text-slate-400 text-sm inline">Fees: {`$${roundUsdAmount(transactionFeeData.upperBoundUSD)}`}</p>
-                    {/* spinner to show fees are updating */}
-                    {
-                        !feesLoaded &&
-                        <svg role="status" className="inline w-4 h-4 ml-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
-                        </svg>
-                      }
-                  </div>
-              }
-              {/* case: network family is solana */}
-              {
-                  (  networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb).networkFamily == NetworkFamily.Solana) &&
-                  <div>
-                    <p className="text-slate-400 text-sm inline">Fees will be calculated on review</p>
-                    {/* spinner to show fees are updating */}
-                    {
-                        !feesLoaded &&
-                        <svg role="status" className="inline w-4 h-4 ml-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
-                        </svg>
-                      }
-                  </div>
-              }
+              
               {/* next button... to set recipient */}
               <button onClick={()=>handleStartParameterSetting()} className={`bg-transparent hover:bg-sky-400 text-sky-400 font-semibold hover:text-white text-2xl py-2 px-20 ${isLoading?"hover:cursor-not-allowed":""} border border-sky-400 hover:border-transparent rounded-lg my-5`} disabled={isLoading}>      
                             Next
@@ -581,11 +562,9 @@ const Send: NextPage = () => {
                             <p className="text-slate-600 text-left">Network Fees</p>
                           </div>
                           <div className="flex-1 px-1">
-                            {
-                               (networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb).networkFamily == NetworkFamily.Solana || transactionFeeData.lowerBoundUSD == transactionFeeData.upperBoundUSD)?
-                              <p className="text-right">{`$${roundUsdAmount(transactionFeeData.upperBoundUSD)}`}</p>:
-                              <p className="text-right">{`$${roundUsdAmount(transactionFeeData.lowerBoundUSD)}-$${roundUsdAmount(transactionFeeData.upperBoundUSD)}`}</p>
-                            }
+                            <div className='text-right'>
+                             <TxFee txFeeData={transactionFeeData} tokenAndNetwork={selectedTokenAndNetwork} feesLoaded={feesLoaded}/>
+                            </div>
                           </div>
                     </div>
                     <div className="flex flex-row">
@@ -732,15 +711,15 @@ const Send: NextPage = () => {
                           </div>
                     </div>
                     <div className="flex flex-row">
+
                           <div className="flex-1">
                             <p className="text-slate-600 text-left">Network Fees</p>
                           </div>
+
                           <div className="flex-1 px-1">
-                            {
-                               networkFromNetworkDb(selectedTokenAndNetwork.baseNetworkDb).networkFamily == NetworkFamily.Solana?
-                              <p className="text-right">{`$${roundUsdAmount(transactionFeeData.upperBoundUSD)}`}</p>:
-                              <p className="text-right">{`$${roundUsdAmount(transactionFeeData.lowerBoundUSD)}-$${roundUsdAmount(transactionFeeData.upperBoundUSD)}`}</p>
-                            }
+                            <div className='text-right'>
+                              <TxFee txFeeData={transactionFeeData} tokenAndNetwork={selectedTokenAndNetwork} feesLoaded={feesLoaded}/>
+                            </div>
                           </div>
                     </div>
                     <div className="flex flex-row">
