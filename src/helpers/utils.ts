d60a5e7623ab48f3389427b1a969756409391cf8
jettblu <jettblu.fabrications@gmail.com>
2022-06-22 17:20:17 -0700

    Add number to BN object util

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -2,6 +2,7 @@ import { ethers } from "ethers";
 import * as jose from "jose"
 import { firebaseserviceKey } from "../../secrets";
 import * as crypto from "crypto"
+import BN from "bn.js";
 
 
 
@@ -83,5 +84,30 @@ function pemToArray(pem:string):Uint8Array{
   let arrayKey:Buffer = Buffer.from(pem, "base64");
   return Uint8Array.from(arrayKey);
 }
+
+// adapted from: https://github.com/SilentCicero/number-to-bn/blob/master/src/index.js
+export function numberToBN(arg:number|string):BN{
+  if (typeof arg === 'string' || typeof arg === 'number') {
+    var multiplier = new BN(1); // eslint-disable-line
+    var formattedString = String(arg).toLowerCase().trim();
+    var isHexPrefixed = formattedString.substr(0, 2) === '0x' || formattedString.substr(0, 3) === '-0x';
+    var stringArg = formattedString;
+    if(stringArg.startsWith("-")) throw(new Error("Number to convert must be positive"));
+    stringArg = stringArg === '' ? '0' : stringArg;
+
+    if ((!stringArg.match(/^-?[0-9]+$/) && stringArg.match(/^[0-9A-Fa-f]+$/))
+      || stringArg.match(/^[a-fA-F]+$/)
+      || (isHexPrefixed === true && stringArg.match(/^[0-9A-Fa-f]+$/))) {
+      return new BN(stringArg, 16).mul(multiplier);
+    }
+
+    if ((stringArg.match(/^-?[0-9]+$/) || stringArg === '') && isHexPrefixed === false) {
+      return new BN(stringArg, 10).mul(multiplier);
+    }
+  }
+  // if we got this far, something must have gone wrong
+  throw new Error('[number-to-bn] while converting number ' + JSON.stringify(arg) + ' to BN.js instance, error: invalid number value. Value must be an integer, hex string, BN or BigNumber instance. Note, decimals are not supported.');
+}
+
 
 
