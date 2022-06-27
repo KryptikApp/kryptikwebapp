fd93d728a62355c6ff4120e1b1279e9b179c3123
jettblu <jettblu.fabrications@gmail.com>
2022-06-27 00:06:39 -0700

    Add ens test to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,45 @@
 import type { NextPage } from 'next'
 import toast, { Toaster } from 'react-hot-toast';
 
-import styles from '../styles/Splash.module.css'
 import { useKryptikAuthContext } from '../components/KryptikAuthProvider';
+import { useEffect, useState } from 'react';
+import { IAccountResolverParams, IResolvedAccount, resolveAccount, defaultResolvedAccount } from '../src/helpers/resolvers/accountResolver';
+import { defaultNetworkDb } from '../src/services/models/network';
+import { ServiceState } from '../src/services/types';
+import { KryptikProvider } from '../src/services/models/provider';
+import { networkFromNetworkDb } from '../src/helpers/utils/networkUtils';
 
 const Home: NextPage = () => {
-  const kryptikAuthContext = useKryptikAuthContext();
+  const {kryptikWallet, kryptikService, authUser} = useKryptikAuthContext();
+  const [resolvedAccount, setResolvedAccount] = useState<IResolvedAccount>(defaultResolvedAccount);
+
+ const fetchResolvedAccount = async function(){
+    // make sure service is started
+    if(kryptikService.serviceState != ServiceState.started) return;
+    let nearNetworkDb = kryptikService.getNetworkDbByTicker("near");
+    if(!nearNetworkDb) return;
+    let network = networkFromNetworkDb(nearNetworkDb);
+    let provider = await kryptikService.getKryptikProviderForNetworkDb(nearNetworkDb)
+    let addys = await kryptikWallet.seedLoop.getAddresses(network);
+    console.log(addys[0]);
+    let resolveParams:IAccountResolverParams ={
+      account: addys[0],
+      networkDB: nearNetworkDb,
+      kryptikProvider: provider
+    }
+    let newResolvedAccount:IResolvedAccount|null = await resolveAccount(resolveParams)
+    if(newResolvedAccount && newResolvedAccount.isResolved){
+      setResolvedAccount(newResolvedAccount);
+    }
+  }
+
+  useEffect(() => {
+    fetchResolvedAccount();
+  }, [authUser])
+
+  useEffect(()=>{
+    fetchResolvedAccount();
+  }, [])
   
   const handleGetStarted = async() =>{
     console.log("Handle get started!");
@@ -27,9 +61,14 @@ const Home: NextPage = () => {
           <button onClick={()=>handleGetStarted()} className="bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded my-5">
                             Get Started
           </button>
-          <p>UID: {kryptikAuthContext.authUser.uid}</p>
-          <p>Wallet Connected: {kryptikAuthContext.kryptikWallet.connected?"True":"False"}</p>
-          <p>Ethereum Address: {kryptikAuthContext.kryptikWallet.ethAddress}</p>
+
+          <div className="dark:text-white">
+            <p>UID: {authUser.uid}</p>
+            <p>Wallet Connected: {kryptikWallet.connected?"True":"False"}</p>
+            <p>Ethereum Address: {kryptikWallet.ethAddress}</p>
+            <p>Resolved ETH Name: {resolvedAccount.name?resolvedAccount.name:"Not Set"}</p>
+          </div>
+
         </div>
 
     </div>
