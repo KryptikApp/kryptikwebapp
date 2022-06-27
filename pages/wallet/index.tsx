1498479e47fb7bff4a6889dd515aa9a9f43ac40a
jettblu <jettblu.fabrications@gmail.com>
2022-06-27 00:08:55 -0700

    Update icon color shades

--- pages/wallet/index.tsx
+++ pages/wallet/index.tsx
@@ -14,7 +14,7 @@ const Profile: NextPage = () => {
   const router = useRouter();
   // ROUTE PROTECTOR: Listen for changes on loading and authUser, redirect if needed
   useEffect(() => {
-    if (!loading && !authUser)
+    if (!loading && !authUser.isLoggedIn)
       router.push('/')
   }, [authUser, loading])
 
@@ -23,11 +23,11 @@ const Profile: NextPage = () => {
     <div>
       
         <div className="text-center max-w-2xl mx-auto content-center">
-          <HeaderProfile user={authUser} showBio={false} center={true}/>
+          <HeaderProfile showBio={false} center={true}/>
           <div className="flex items-center mx-auto content-center space-x-4 max-w-xs">
            <div className='flex-1'>
               <Link href="../wallet/send">
-                  <div  className="w-full py-5 px-3 text-center text-sky-600 hover:cursor-pointer hover:text-sky-800 hover:font-semibold hover:animate-pulse">
+                  <div  className="w-full py-5 px-3 text-center text-sky-500 hover:cursor-pointer hover:text-sky-800 hover:font-semibold hover:animate-pulse">
                       <AiFillPayCircle className="mx-auto" size="30"/>
                       <span className="text-gray-700 dark:text-gray-200">Swap</span>
                   </div>
@@ -35,7 +35,7 @@ const Profile: NextPage = () => {
             </div>
             <div className='flex-1'>
               <Link href="../wallet/receive">
-                  <div  className="w-full py-5 px-3 text-center text-sky-600 hover:cursor-pointer hover:text-sky-800 hover:font-semibold hover:animate-pulse">
+                  <div  className="w-full py-5 px-3 text-center text-sky-500 hover:cursor-pointer hover:text-sky-800 hover:font-semibold hover:animate-pulse">
                       <AiFillDownCircle className="mx-auto" size="30"/>
                       <span className="text-gray-700 dark:text-gray-200">Receive</span>
                   </div>
@@ -43,7 +43,7 @@ const Profile: NextPage = () => {
             </div>
             <div className='flex-1'>
             <Link href="../wallet/send">
-                <div  className="w-full py-5 px-3 text-center text-sky-600 hover:cursor-pointer hover:text-sky-800 hover:font-semibold hover:animate-pulse">
+                <div  className="w-full py-5 px-3 text-center text-sky-500 hover:cursor-pointer hover:text-sky-800 hover:font-semibold hover:animate-pulse">
                     <AiFillUpCircle className="mx-auto" size="30"/>
                     <span className="text-gray-700 dark:text-gray-200">Send</span>
                 </div>
