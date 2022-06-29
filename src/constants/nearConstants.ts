cc129c409a2b2b4513e487db055ceea4875f1b32
jettblu <jettblu.fabrications@gmail.com>
2022-06-29 13:21:54 -0700

    Add seperate constants folder

--- src/constants/nearConstants.ts
+++ src/constants/nearConstants.ts
@@ -1,8 +1,10 @@
-import { numberToBN } from "../../../helpers/utils";
+import { numberToBN } from "../helpers/utils";
 // Default amount of gas to be sent with the function calls. Used to pay for the fees
 // incurred while running the contract execution. The unused amount will be refunded back to
 // the originator.
 // Due to protocol changes that charge upfront for the maximum possible gas price inflation due to
 // full blocks, the price of max_prepaid_gas is decreased to `300 * 10**12`.
+
 // For discussion see https://github.com/nearprotocol/NEPs/issues/67
 export const DEFAULT_NEAR_FUNCTION_CALL_GAS = numberToBN('30000000000000');
+export const NEAR_INDEXER_URL = "https://api.kitwallet.app"
