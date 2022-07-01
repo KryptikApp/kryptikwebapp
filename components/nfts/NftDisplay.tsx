b1f2766f87b9de423c57aca60f66d1314a08cde5
jettblu <jettblu.fabrications@gmail.com>
2022-06-30 18:55:57 -0700

    Add NFT display

---
+++ components/nfts/NftDisplay.tsx
@@ -0,0 +1,161 @@
+import { NextPage } from "next";
+import { useContext, useState } from "react";
+import { INFTMetadata, ITraitType } from "../../src/parsers/nftMetaData";
+import { useKryptikThemeContext } from "../ThemeProvider";
+import CardDivider from "./cardDivider";
+
+interface Props {
+    nftMetaData: INFTMetadata
+}
+const NftDisplay:NextPage<Props> = (props) => {
+    const{nftMetaData} = {...props};
+    const{isDark} = useKryptikThemeContext();
+    const [showModal, setShowModal] = useState(false)
+    return(
+        <div className="">
+        {
+            (nftMetaData.image_preview_url || nftMetaData.image_url)?
+            <div data-modal-toggle={`${nftMetaData.id}Modal`} className="hover:cursor-pointer transition ease-in-out hover:scale-110" onClick={()=>setShowModal(true)}>
+                <img src={nftMetaData.image_preview_url?nftMetaData.image_preview_url:nftMetaData.image_url} className="w-56 h-56 rounded-md drop-shadow-lg object-cover border border-gray-200 dark:border-gray-800"/>
+                <p className="my-2 text-sm text-gray-400 dark:text-gray-500 font-semibold">{nftMetaData.name?nftMetaData.name:nftMetaData.collection.name}</p>
+            </div>:
+            <div className="transition ease-in-out hover:scale-110 hover:cursor-pointer" onClick={()=>setShowModal(true)}>
+            <div className="h-56 w-56 pt-20 rounded-md bg-gradient-to-r from-gray-100 to-white drop-shadow-lg dark:from-gray-900 dark:to-black text-lg dark:text-white text-center px-1 font-semibold">
+                 {nftMetaData.collection.name}
+            </div>
+            <p className="my-2 text-sm text-gray-400 dark:text-gray-500 font-semibold">{nftMetaData.name?nftMetaData.name:nftMetaData.collection.name}</p>
+            </div>
+        }
+        
+        {/* nft modal */}
+        <div id={`${nftMetaData.id}Modal`} tabIndex={-1} aria-hidden={showModal?"false":"true"} className={`${!showModal && "hidden"} modal fixed w-full h-full top-0 left-0 z-50 flex items-center justify-center overflow-y-auto`} style={{backgroundColor:`${isDark?"rgba(0, 0, 0, 0.9)":"rgba(0, 0, 0, 0.9)"}`}}>
+            {/* top right fixed close button  */}
+            <button type="button" className="invisible md:visible text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto fixed top-4 right-5 items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>setShowModal(false)}>
+                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+            </button>
+            {/* flex with card and image */}
+            <div className="flex flex-col md:flex-row flex-wrap opacity-100 m-4 md:min-w-[60%] max-w-[90%] md:max-w-[900px] max-h-screen">
+                <div className="md:hidden min-h-[2rem] dark:text-white">
+                        
+                        {/* padding div for space between top and main elements */}
+                </div>
+
+                {/* close button shown on small screens */}
+                <button type="button" className="md:hidden mb-2 text-black bg-white rounded-full font-bold text-sm p-1.5 ml-auto items-center dark:bg-white dark:text-black transition ease-in-out hover:scale-110" onClick={()=>setShowModal(false)}>
+                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+                </button>
+
+                {/* nft main image */}
+                <div className="flex-1 opacity-100">
+                        {
+                        (nftMetaData.image_original_url||nftMetaData.image_url)?
+                        <img src={nftMetaData.image_original_url?nftMetaData.image_original_url:nftMetaData.image_url} className="w-full h-fit rounded-lg drop-shadow-xl object-cover border border-gray-200 dark:border-gray-800"/>:            
+                        <div className="w-full min-h-[10rem] max-h-[20rem] pt-[3rem] rounded-md bg-gradient-to-r from-gray-100 to-white drop-shadow-lg dark:from-gray-900 dark:to-black text-lg dark:text-white text-center px-1 font-semibold overflow-y-auto no-scrollbar">
+                        {nftMetaData.name}
+                        </div>
+                        }
+                </div>
+                    
+                {/* nft info card */}
+                <div className="flex-1 bg-white dark:bg-black md:ml-6 mt-8 md:mt-0 rounded-lg min-h-[30rem] md:min-h-[25rem] h-fit md:max-h-[40rem] dark:border dark:border-gray-100 md:overflow-x-hidden overflow-y-auto no-scrollbar">
+                    <div className="mx-3 mt-3 dark:text-white">
+                        <div>
+                            <img className="inline object-cover w-6 h-6 rounded-full mr-2" src={nftMetaData.collection.image_url}/>
+                            <span className="font-semibold inline text-sm text-gray-400 dark:gray-500 mt-2">{nftMetaData.collection.name}</span>
+                        </div>
+                        <div>
+                            <h1 className="font-bold text-2xl">{nftMetaData.name}</h1>
+                        </div>
+                        <div className="my-4">
+                            {
+                                (!nftMetaData.isPoap) &&
+                                <div className="flex flex-wrap">
+                                <div className="mx-2 my-1 transition ease-in-out hover:scale-105">
+                                    <a href={`https://rarible.com/token/${nftMetaData.asset_contract.address}:${nftMetaData.token_id}`} target="_blank" rel="noopener noreferrer">
+                                        <div className="drop-shadow-lg bg-gray-50 hover:bg-gray-100 dark:hover-gray-800 border border-gray-100 dark:border-gray-800 dark:bg-black hover:cursor-pointer transition ease-in-out hover:scale-110 rounded-full py-1 px-1 w-fit">
+                                        <img className="w-5 h-5 inline mr-2" src="/nftPlatforms/logos/rarible.svg"></img>  
+                                        <span className="inline text-slate-700 dark:text-slate-200 font-bold text-md">Rarible</span>
+                                        </div>
+                                    </a>
+                                </div>
+                                
+                                <div className="mx-2 my-1 transition ease-in-out hover:scale-105">
+                                    <a href={`https://opensea.io/assets/ethereum/${nftMetaData.asset_contract.address}/${nftMetaData.token_id}`} target="_blank" rel="noopener noreferrer">
+                                        <div className="drop-shadow-lg bg-gray-50  hover:bg-gray-100 dark:hover-gray-800 border border-gray-100 dark:border-gray-800 dark:bg-black hover:cursor-pointer rounded-full py-1 px-1 w-fit">
+                                            <img className="w-5 h-5 inline mr-2" src="/nftPlatforms/logos/opensea.svg"></img>  
+                                            <span className="inline text-slate-700 dark:text-slate-200 font-bold text-md">Opensea</span>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div className="mx-2 my-1 transition ease-in-out hover:scale-105">
+                                    <a href={`https://etherscan.io/token/${nftMetaData.asset_contract.address}/a=?${nftMetaData.name}`} target="_blank" rel="noopener noreferrer">
+                                        <div className="drop-shadow-lg bg-gray-50  hover:bg-gray-100 dark:hover-gray-800 border border-gray-100 dark:border-gray-800 dark:bg-black hover:cursor-pointer transition ease-in-out hover:scale-110 rounded-full py-1 px-1 w-fit">
+                                            <img className="w-5 h-5 inline mr-2" src="/scanners/logos/etherscan.svg"></img>  
+                                            <span className="inline text-slate-700 dark:text-slate-200 font-bold text-md">Etherscan</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            }
+                            
+                            <CardDivider/>
+
+                            <div>
+                                <h2 className="text-lg dark:text-white font-bold">Description</h2>
+                                <p className="text-gray-400 dark:text-gray-300">
+                                    {nftMetaData.description}
+                                </p>
+                            </div>
+                            
+                            <div>
+
+                                
+                                {
+                                    (nftMetaData.traits && nftMetaData.traits.length>0)&& 
+                                    
+                                    <div>
+                                        <CardDivider/>
+                                        <h2 className="text-lg dark:text-white font-bold">Attributes</h2>
+                                        <div className="flex flex-wrap">
+                                        {
+                                             nftMetaData.traits.map((trait:ITraitType)=>(
+                                                <div className="hover:cursor-pointer transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 w-fit my-1 max-w-4 px-1 mx-2 rounded drop-shadow-lg">
+                                                    <p className="text-sm text-gray-400 dark:text-slate-300 font-bold">{trait.trait_type}</p>
+                                                    <p className="text-sm text-gray-400 dark:text-slate-300 truncate ...">{trait.value}</p>
+                                                </div>
+                                            ))
+                                        }
+                                        </div>
+                                    </div>
+                                }
+                            
+                            </div>
+                            {
+                                (nftMetaData.isPoap) &&
+                                <div>
+                                    <CardDivider/>
+                                    <h2 className="text-lg dark:text-white font-bold">About Poap</h2>
+                                    <p>POAPs are unique NFT badges given out to attendees of both virtual and real-world events.</p>
+                                </div>
+                            }
+                            <div className="h-[2rem] dark:text-white">
+                            {/* padding div for space between top and main elements */}
+                            </div>              
+                        </div>
+                    </div>
+                </div>
+                <div className="md:hidden min-h-[4rem] dark:text-white">
+                    
+                            {/* padding div for space between top and main elements */}
+                </div>
+            </div>
+        </div>
+
+
+        </div>
+        
+    )   
+}
+
+export default NftDisplay;
--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,16 +1,160 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
+import { useKryptikAuthContext } from '../components/KryptikAuthProvider'
+import NftDisplay from '../components/nfts/nftDisplay';
+import ProfileName from '../components/ProfileName';
+import { getUserPhotoPath } from '../src/helpers/firebaseHelper';
+import { INFTMetadata } from '../src/parsers/nftMetaData';
+import { listNftsByAddress } from '../src/requests/nfts/openSeaApi';
+import { listPoapsByAddress } from '../src/requests/nfts/poapApi';
 
 
 const Explore: NextPage = () => {
+  const enum ActiveCategory{
+    all=0,
+    poaps=1
+  }
+  const {authUser, kryptikWallet} = useKryptikAuthContext();
+  const [activeCategory, setActiveCategory] = useState(ActiveCategory.all);
+  const [isNFTFetched, setIsNFTFetched] = useState(true);
+  const [nftMetaDataList, setNftMetadataList] = useState<INFTMetadata[]>([]);
+  const [poapMetaDataList, setPoapMetadataList] = useState<INFTMetadata[]>([]);
+  
+  const fetchNFTData = async function(){
+    setIsNFTFetched(false);
+    // fetch eth nfts
+    let newNftMetadataList = await listNftsByAddress(kryptikWallet.ethAddress);
+    if(!newNftMetadataList){
+      setIsNFTFetched(true);
+      return;
+    }
+    // update eth state
+    setNftMetadataList(newNftMetadataList);
+
+    // fetch poaps
+    let poapsList = await listPoapsByAddress(kryptikWallet.ethAddress);
+    console.log("POAPS:");
+    console.log(poapsList);
+    if(!poapsList){
+      setIsNFTFetched(true);
+      return;
+    }
+    // update poap state
+    setPoapMetadataList(poapsList);
+    setIsNFTFetched(true);
+  }
+
+  useEffect(()=>{
+    
+    fetchNFTData();
+    
+  },[])
+
   return (
     <div>
-
-        <div className="h-[2rem]">
+       <div className="h-[2rem] dark:text-white">
           {/* padding div for space between top and main elements */}
         </div>
-      
-        <div className="text-center max-w-2xl mx-auto content-center dark:text-white">
-           <p>Hey, this is an explore page!</p>
+        <div className="flex flex-col md:flex-row">
+
+           <div className='flex-1'>
+            <div className="mx-auto text-center">
+              <img src={getUserPhotoPath(authUser)} alt="Profile Image" className="object-cover w-20 h-20 rounded-full mx-auto mb-2"/>
+              <ProfileName/>
+            </div>
+            <br/>
+
+            <div className="flex flex-col mx-6">
+
+              <div onClick={()=>setActiveCategory(ActiveCategory.all)} className={`${activeCategory == ActiveCategory.all?"bg-gradient-to-r from-gray-100 to-white dark:from-gray-900 dark:to-black":""} flex flex-row text-lg hover:cursor-pointer outline-0 transition ease-in-out hover:scale-110 text-slate-800 dark:text-slate-100 font-semibold rounded py-2 px-2`}>
+                <span className='w-5 min-w-5 mr-2'>🎨</span>
+                <h3 className="">All NFTs</h3>
+                <span className="grow text-right">{nftMetaDataList.length+poapMetaDataList.length}</span>
+              </div>
+
+              <div onClick={()=>setActiveCategory(ActiveCategory.poaps)} className={`${activeCategory == ActiveCategory.poaps?"bg-gradient-to-r from-gray-100 to-white dark:from-gray-900 dark:to-black":""} flex flex-row text-lg hover:cursor-pointer outline-0 transition ease-in-out hover:scale-110 text-slate-800 dark:text-slate-100 font-semibold rounded py-2 px-2`}>
+                 <span className='w-5 min-w-5 mr-2'>🏷️</span>
+                 <h3 className="">Proof of Attendance</h3>
+                 <span className="grow text-right">{poapMetaDataList.length}</span>
+              </div>
+
+            </div>             
+           </div>
+
+           <div className="flex-9 md:w-[80%]">
+            {/* show active category name */}
+            <div className="invisible md:visible dark:text-white">
+            {
+              activeCategory == ActiveCategory.all &&
+              <div className="font-bold text-2xl ml-20 mb-6">
+                <span className='w-10 min-w-10 mr-2 inline'>🎨</span>
+                <h1 className="inline">All</h1>
+              </div>
+            }
+             {
+              activeCategory == ActiveCategory.poaps &&
+              <div className="font-bold text-2xl ml-20 mb-6">
+                <span className='w-10 min-w-10 mr-2 inline'>🏷️</span>
+                <h1 className="inline">Proof of Attendance</h1>
+              </div>
+            }
+            </div>
+
+            {/* show loader while fetching nft data */}
+            {
+                !isNFTFetched &&
+                <div className='text-center content-center items-center place-items-center my-40 mr-4'>
+                    <p className="text-md font-bold text-slate-700 dark:text-slate-200 inline">Loading brilliant works of art!</p>
+                    <svg role="status" className="w-8 h-8 ml-6 text-white animate-spin text-center content-center inline" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                                              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                    </svg>
+                </div>
+            }
+            {/* nft gallery */}
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mx-auto place-items-center">
+              {/* all category */}
+              {
+                (activeCategory == ActiveCategory.all && isNFTFetched && nftMetaDataList.length !=0)?
+                nftMetaDataList.map((nftData:INFTMetadata)=>
+                (
+                  <NftDisplay nftMetaData={nftData}/>
+                )):
+                <div>
+                {
+                  (isNFTFetched && activeCategory == ActiveCategory.all)&&
+                  <div className="text-lg font-semibold text-lg dark:text-white text-center mt-40">
+                    <span className='w-5 min-w-5 mr-2 inline'>👻</span>
+                    <p className="inline">Nothing here!!</p>
+                  </div>
+                }
+                </div>
+                
+              }
+              {/* poap category */}
+              {
+                ((activeCategory == ActiveCategory.poaps || activeCategory == ActiveCategory.all) && isNFTFetched && poapMetaDataList.length !=0)?
+                poapMetaDataList.map((poapData:INFTMetadata)=>
+                (
+                  <NftDisplay nftMetaData={poapData}/>
+                )):
+                <div>
+                {
+                  (isNFTFetched && activeCategory == ActiveCategory.poaps) &&
+                  <div className="text-lg font-semibold text-lg dark:text-white text-center mt-40">
+                    <span className='w-5 min-w-5 mr-2 inline'>👻</span>
+                    <p className="inline">Nothing here!!</p>
+                  </div>
+                }
+                </div>
+                
+              }
+            </div>
+           </div>
+           <div className="md:hidden min-h-[4rem] dark:text-white">
+            
+                    {/* padding div for space between top and main elements */}
+            </div>
         </div>
 
     </div>
