da6059a6511946a8f6889b632f155817d9c445f4
jettblu <jettblu.fabrications@gmail.com>
2022-06-30 18:51:44 -0700

    Add card divider

---
+++ components/nfts/CardDivider.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const CardDivider:NextPage = () => {
+    return(
+        <div className="border border-gray-50 dark:border-[#1b1b1c] my-5 max-w-[95%] px-3 mx-auto"/>
+    )   
+}
+
+export default CardDivider;
