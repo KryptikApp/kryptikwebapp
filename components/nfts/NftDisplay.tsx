d8a21a47f1291c13c437120d6bbf8947f3279ff6
jettblu <jettblu.fabrications@gmail.com>
2022-07-07 16:24:55 -0700

    Update nft display explorer and alt description

--- components/nfts/NftDisplay.tsx
+++ components/nfts/NftDisplay.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from "next";
 import { useContext, useState } from "react";
-import { INFTMetadata, ITraitType } from "../../src/parsers/nftMetaData";
+import { INFTMetadata, ITraitType } from "../../src/parsers/nftEthereum";
+import { defaultNetworkDb } from "../../src/services/models/network";
+import { useKryptikAuthContext } from "../KryptikAuthProvider";
 import { useKryptikThemeContext } from "../ThemeProvider";
 import CardDivider from "./CardDivider";
+import EthExplorers from "./EthExplorers";
+import NearExplorers from "./NearExplorers";
+import SolExplorers from "./SolExplorers";
 
 interface Props {
     nftMetaData: INFTMetadata
@@ -10,14 +15,21 @@ interface Props {
 const NftDisplay:NextPage<Props> = (props) => {
     const{nftMetaData} = {...props};
     const{isDark} = useKryptikThemeContext();
-    const [showModal, setShowModal] = useState(false)
+    const {kryptikService} = useKryptikAuthContext();
+    const [showModal, setShowModal] = useState(false);
+    const networkDb = kryptikService.getNetworkDbByTicker(nftMetaData.networkTicker)||defaultNetworkDb;
     return(
         <div className="">
         {
             (nftMetaData.image_preview_url || nftMetaData.image_url)?
-            <div data-modal-toggle={`${nftMetaData.id}Modal`} className="hover:cursor-pointer transition ease-in-out hover:scale-110" onClick={()=>setShowModal(true)}>
+            <div className="hover:cursor-pointer transition ease-in-out hover:scale-110" onClick={()=>setShowModal(true)}>
                 <img src={nftMetaData.image_preview_url?nftMetaData.image_preview_url:nftMetaData.image_url} className="w-56 h-56 rounded-md drop-shadow-lg object-cover border border-gray-200 dark:border-gray-800"/>
-                <p className="my-2 text-sm text-gray-400 dark:text-gray-500 font-semibold">{nftMetaData.name?nftMetaData.name:nftMetaData.collection.name}</p>
+                <div className="flex">
+                    <p className="my-2 text-sm text-gray-400 dark:text-gray-500 font-semibold">{nftMetaData.name?nftMetaData.name:nftMetaData.collection.name}</p>
+                    <div className="flex-grow mt-3">
+                        <img className="h-4 w-4 float-right" src={networkDb.iconPath}/>
+                    </div>
+                </div>
             </div>:
             <div className="transition ease-in-out hover:scale-110 hover:cursor-pointer" onClick={()=>setShowModal(true)}>
             <div className="h-56 w-56 pt-20 rounded-md bg-gradient-to-r from-gray-100 to-white drop-shadow-lg dark:from-gray-900 dark:to-black text-lg dark:text-white text-center px-1 font-semibold">
@@ -60,53 +72,45 @@ const NftDisplay:NextPage<Props> = (props) => {
                 <div className="flex-1 bg-white dark:bg-black md:ml-6 mt-8 md:mt-0 rounded-lg min-h-[30rem] md:min-h-[25rem] h-fit md:max-h-[40rem] dark:border dark:border-gray-100 md:overflow-x-hidden overflow-y-auto no-scrollbar">
                     <div className="mx-3 mt-3 dark:text-white">
                         <div>
-                            <img className="inline object-cover w-6 h-6 rounded-full mr-2" src={nftMetaData.collection.image_url}/>
+                            {
+                                nftMetaData.collection.image_url?
+                                <img className="inline object-cover w-6 h-6 rounded-full mr-2" src={nftMetaData.collection.image_url}/>:
+                                <img className="inline object-cover w-6 h-6 rounded-full mr-2" src={nftMetaData.image_url}/>
+                            }
                             <span className="font-semibold inline text-sm text-gray-400 dark:gray-500 mt-2">{nftMetaData.collection.name}</span>
                         </div>
                         <div>
                             <h1 className="font-bold text-2xl">{nftMetaData.name}</h1>
                         </div>
+                        
                         <div className="my-4">
                             {
-                                (!nftMetaData.isPoap) &&
-                                <div className="flex flex-wrap">
-                                <div className="mx-2 my-1 transition ease-in-out hover:scale-105">
-                                    <a href={`https://rarible.com/token/${nftMetaData.asset_contract.address}:${nftMetaData.token_id}`} target="_blank" rel="noopener noreferrer">
-                                        <div className="drop-shadow-lg bg-gray-50 hover:bg-gray-100 dark:hover-gray-800 border border-gray-100 dark:border-gray-800 dark:bg-black hover:cursor-pointer transition ease-in-out hover:scale-110 rounded-full py-1 px-1 w-fit">
-                                        <img className="w-5 h-5 inline mr-2" src="/nftPlatforms/logos/rarible.svg"></img>  
-                                        <span className="inline text-slate-700 dark:text-slate-200 font-bold text-md">Rarible</span>
-                                        </div>
-                                    </a>
-                                </div>
-                                
-                                <div className="mx-2 my-1 transition ease-in-out hover:scale-105">
-                                    <a href={`https://opensea.io/assets/ethereum/${nftMetaData.asset_contract.address}/${nftMetaData.token_id}`} target="_blank" rel="noopener noreferrer">
-                                        <div className="drop-shadow-lg bg-gray-50  hover:bg-gray-100 dark:hover-gray-800 border border-gray-100 dark:border-gray-800 dark:bg-black hover:cursor-pointer rounded-full py-1 px-1 w-fit">
-                                            <img className="w-5 h-5 inline mr-2" src="/nftPlatforms/logos/opensea.svg"></img>  
-                                            <span className="inline text-slate-700 dark:text-slate-200 font-bold text-md">Opensea</span>
-                                        </div>
-                                    </a>
-                                </div>
-
-                                <div className="mx-2 my-1 transition ease-in-out hover:scale-105">
-                                    <a href={`https://etherscan.io/token/${nftMetaData.asset_contract.address}/a=?${nftMetaData.name}`} target="_blank" rel="noopener noreferrer">
-                                        <div className="drop-shadow-lg bg-gray-50  hover:bg-gray-100 dark:hover-gray-800 border border-gray-100 dark:border-gray-800 dark:bg-black hover:cursor-pointer transition ease-in-out hover:scale-110 rounded-full py-1 px-1 w-fit">
-                                            <img className="w-5 h-5 inline mr-2" src="/scanners/logos/etherscan.svg"></img>  
-                                            <span className="inline text-slate-700 dark:text-slate-200 font-bold text-md">Etherscan</span>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
+                                nftMetaData.networkTicker == "eth" && <EthExplorers nftMetaData={nftMetaData}/>
                             }
-                            
+                            {
+                                nftMetaData.networkTicker == "sol" && <SolExplorers nftMetaData={nftMetaData}/>
+                            }
+                            {
+                                nftMetaData.networkTicker == "near" && <NearExplorers nftMetaData={nftMetaData}/>
+                            }
                             <CardDivider/>
 
-                            <div>
+                            {
+                                nftMetaData.description?
+                                <div>
                                 <h2 className="text-lg dark:text-white font-bold">Description</h2>
-                                <p className="text-gray-400 dark:text-gray-300">
-                                    {nftMetaData.description}
-                                </p>
-                            </div>
+                                    <p className="text-gray-400 dark:text-gray-300">
+                                        {nftMetaData.description}
+                                    </p>
+                                
+                                </div>:
+
+                                <div>
+                                    <h2 className="text-lg dark:text-white font-bold">Description</h2>
+                                    <p className="text-gray-400 dark:text-gray-300">{nftMetaData.name} is a collectible. You can learn more about NFTs <a className="hover:cursor-pointer hover:text-sky-500 text-sky-400" href={`https://opensea.io/blog/guides/non-fungible-tokens/`} target="_blank" rel="noopener noreferrer">here</a>.</p>
+                                </div> 
+                            }
+                            
                             
                             <div>
 
