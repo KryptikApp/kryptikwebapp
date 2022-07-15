131e9d4957857561c479c8be5e3fddd8f49f4b6e
jettblu <jettblu.fabrications@gmail.com>
2022-07-15 10:52:39 -0700

    Add offline event handler and redirect page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,28 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import { KryptikAuthProvider } from '../components/KryptikAuthProvider'
 import { KryptikThemeProvider } from '../components/ThemeProvider'
+import router from 'next/router'
+import { useEffect } from 'react'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const handleOffline = function(){
+    router.push('../status/offline')
+  }
+
+  const addOfflineHandler = function(){
+    // handle if offline
+    if(!window.navigator.onLine){
+      handleOffline()
+    }
+    // add connection handler
+    window.addEventListener('offline', () => handleOffline())
+  }
+
+  useEffect(()=> 
+    addOfflineHandler(),[]
+  )
+  
   return(
       <KryptikAuthProvider>
         <KryptikThemeProvider>
---
+++ pages/status/offline.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import toast, { Toaster } from 'react-hot-toast';
+
+import Link from 'next/link';
+import { useKryptikAuthContext } from '../../components/KryptikAuthProvider';
+
+
+const Offline: NextPage = () => {
+
+  return (
+    <div>
+        <Toaster/>
+        <div className="h-[10rem]">
+          {/* padding div for space between top and main elements */}
+        </div>
+
+        <div className="text-center max-w-2xl mx-auto content-center">
+          <h1 className="text-5xl font-bold sans dark:text-white">
+              Kryptik App Requires An Internet Connection!
+          </h1>
+        </div>
+        <div className="h-[10rem]">
+          {/* padding div for space between top and main elements */}
+        </div>
+    </div>
+ 
+  )
+}
+
+export default Offline
