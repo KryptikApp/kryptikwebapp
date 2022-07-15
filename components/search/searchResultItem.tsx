20ececf7a401badb6644028fc511e50f4d75c14d
jettblu <jettblu.fabrications@gmail.com>
2022-07-15 10:51:56 -0700

    Add icon path to search result

--- components/search/searchResultItem.tsx
+++ components/search/searchResultItem.tsx
@@ -13,7 +13,16 @@ const SearchResultItem:NextPage<Props> = (props) => {
     const {searchResult} = {...props};
     return(
         <div onClick={()=>searchResult.onClickFunction?searchResult.onClickFunction(searchResult.onClickParams):defaultClickHandler()} className="font-semibold text-lg dark:text-slate-200 p-2 m-1 transition ease-in-out hover:scale-105 hover:cursor-pointer text-left">
-            {searchResult.resultString}
+            {
+                searchResult.iconPath?
+                <div>
+                    <img className="w-8 h-8 rounded-full inline" src={searchResult.iconPath} alt={`${searchResult.resultString} image`}/>
+                    <span className="inline ml-2">{searchResult.resultString}</span>
+                </div>
+                :
+                <span className="ml">{searchResult.resultString}</span>
+            }
+            
         </div>
         
     )   
