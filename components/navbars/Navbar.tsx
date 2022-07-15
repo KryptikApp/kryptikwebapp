b27de5a2d15f24c3b4ccb615e43036e2e5e094eb
jettblu <jettblu.fabrications@gmail.com>
2022-07-15 10:48:54 -0700

    Delete old handlers from main navbar

--- components/navbars/Navbar.tsx
+++ components/navbars/Navbar.tsx
@@ -15,33 +15,12 @@ import NavbarPrelaunch from "./NavbarPrelaunch";
 
 
 const Navbar:NextPage = () => {
-    const [isMenuMobile, setMenuMobile] = useState(false);
-    const {kryptikWallet, authUser} = useKryptikAuthContext();
-    const {hideBalances, updateHideBalances} = useKryptikThemeContext();
-    const router = useRouter();
-    const appStage = process.env.APP_STAGE;
-    console.log("Router path (Navbar):")
-    console.log(router.pathname)
-
-    // change style based on boolean
-    const menuWrapperClassName = isMenuMobile
-        ? "md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0"
-        : "hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0";
-
-    const handleHideBalances = function(isHideBalances:boolean){
-        updateHideBalances(isHideBalances, authUser.uid);
-        if(isHideBalances){
-            toast("Your balances will now be HIDDEN while browsing")
-        }
-        else{
-            toast("Your balances will now be VISIBLE while browsing")
-        }
-    }
-        
+    const appMode = process.env.NEXT_PUBLIC_APP_MODE;
+ 
     return(
         <div>
         {
-            appStage == "prelaunch"?
+            appMode == "prelaunch"?
             <NavbarPrelaunch/>
             :
             <NavbarProduction/>
