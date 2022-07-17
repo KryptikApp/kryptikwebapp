feb89d6bc5808ed57db20f41d026b862ec663a77
jettblu <jettblu.fabrications@gmail.com>
2022-07-16 22:50:10 -0700

    Delete proxy from solana nft request

--- src/requests/nfts/solanaApi.ts
+++ src/requests/nfts/solanaApi.ts
@@ -7,7 +7,7 @@ import { parseSolNFTMetaData } from "../../parsers/nftSolana";
 export const listSolanaNftsByAddress = async function(address:string, next=""):Promise<INFTMetadata[]|null>{
   console.log("Fetching solana data....");
     try { //add support for multiple pages
-        const url = `https://cors-anywhere.herokuapp.com/https://api-mainnet.magiceden.dev/v2/wallets/${address}/tokens`;
+        const url = `https://api-mainnet.magiceden.dev/v2/wallets/${address}/tokens`;
         const dataResponse = await KryptikFetch(url, {
           headers: {"Access-Control-Allow-Origin": "*"},
           timeout: 10000, // 10 secs
