4fe3b05f13b2b1de62f5af6140541e1385ca6390
jettblu <jettblu.fabrications@gmail.com>
2022-07-16 22:47:50 -0700

    Update vision text style

--- pages/vision.tsx
+++ pages/vision.tsx
@@ -11,11 +11,14 @@ const Vision: NextPage = () => {
         </div>
       
         <div className="dark:text-white">
-        <div className="max-w-2xl mx-auto">
-          <h1 className="text-4xl font-bold sans mb-5">
+        <div className="max-w-2xl mx-auto px-4 md:px-0 min-h-[100vh]">
+        <div className="min-h-[6rem]">
+            {/* padding div for space between top and main elements */}
+          </div>
+          <h1 className="text-5xl font-bold sans mb-5">
                 The Vision
           </h1>
-          <p className="leading-loose mb-2 text-justify">Kryptik is not the first crypto wallet and it won’t be the last, but after a year of hard work we believe it will be the best. Innovation is our slingshot, design is our rock and with enough hard work, we believe we can topple the hype and deliver a simple, secure online wallet that benefits everyone. This is the Kryptik vision. </p>
+          <p className="leading-loose mb-2 text-xl text-justify">Kryptik is not the first crypto wallet and it won’t be the last, but after a year of hard work we believe it will be the best. Innovation is our slingshot, design is our rock and with enough hard work, we believe we can topple the hype and deliver a simple, secure online wallet that benefits everyone. This is the Kryptik vision. </p>
         </div>
           <div className="timeline min-h-[1000px]">
             <ul>
