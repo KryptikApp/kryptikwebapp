8a785968b3e3a186b0c107f01111b2fc53502f9b
jettblu <jettblu.fabrications@gmail.com>
2022-07-20 00:12:49 -0700

    Update prelaunch about style

--- pages/vision.tsx
+++ pages/vision.tsx
@@ -15,7 +15,7 @@ const Vision: NextPage = () => {
         <div className="min-h-[5rem]">
             {/* padding div for space between top and main elements */}
           </div>
-          <h1 className="text-5xl font-bold sans mb-5">
+          <h1 className="text-left text-5xl font-bold sans mb-5">
                 The Vision
           </h1>
           <p className="leading-loose mb-2 text-xl text-justify">Kryptik is not the first crypto wallet and it won’t be the last, but after a year of hard work we believe it will be the best. Innovation is our slingshot, design is our rock and with enough hard work, we believe we can topple the hype and deliver a simple, secure online wallet that benefits everyone. This is the Kryptik vision. </p>
