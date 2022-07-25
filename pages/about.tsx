48f45b5fb2d05348f9dab570931484f00391b42b
jettblu <jettblu.fabrications@gmail.com>
2022-07-25 10:50:35 -0700

    Update about page style

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,49 +11,67 @@ const About: NextPage = () => {
 
     <div>
         <div className="dark:text-white">
-          <div className="max-w-2xl mx-auto px-4 md:px-0 min-h-[100vh]">
-          <div className="min-h-[20vh]">
-            {/* padding div for space between top and main elements */}
-          </div>
-            <h1 className="text-5xl text-left font-bold sans mb-5">
-                  The <span className="">Kryptik</span> Wallet
-            </h1>
+          <div className="min-h-[100vh]">
 
-            <p className="leading-loose mb-2 text-xl text-justify dark:text-gray-400">Kryptik is a simple wallet that lets you save, send, and collect value across the internet. With Kryptik you can access a world of possibilities across 10+ blockchains from a single app. One <span className="text-purple-500">secure</span> wallet. One <span className="text-green-500">simple</span> interface. One <span className="text-sky-500">magical</span> future. </p>
-          </div>
-          
-          <div className="min-h-[100vh] -mx-4 dark:bg-[#0f0f0f] xl:dark:bg-black">
-            <div className="max-w-2xl mx-auto">
-            <h2 className="text-5xl text-center md:text-left font-bold sans pt-4 md:pt-20 mb-5 md:mb-20">The Kryptik Way</h2>
+            <div className="min-h-[20vh]">
+              {/* padding div for space between top and main elements */}
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 place-items-center my-6 pb-6 max-w-3xl mx-auto">
-              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 px-2 rounded drop-shadow-lg max-w-[50%] min-h-[120px]">
-                  <h2 className="text-xl font-semibold">💻 Transparent</h2>
+
+            <div className="px-4 md:px-0  max-w-2xl mx-auto">
+              <h1 className="text-5xl text-left font-bold sans mb-5">
+                    The <span className="">Kryptik</span> Wallet
+              </h1>
+              <p className="leading-loose mb-2 text-xl text-justify dark:text-gray-400">Kryptik is a simple wallet that lets you save, send, and collect value across the internet. With Kryptik you can access a world of possibilities across 10+ blockchains from a single app. One <span className="text-purple-500">secure</span> wallet. One <span className="text-green-500">simple</span> interface. One <span className="text-sky-500">magical</span> future. </p>
+            </div>
+
+            <div className="min-h-[10vh]">
+              {/* padding div for space between about and more info */}
+            </div>
+
+            <div className="-mx-4">
+            {/* <div className="max-w-3xl mx-auto">
+            <h2 className="text-2xl text-center md:text-left font-semibold sans pt-4 md:pt-20 mb-5 dark:text-slate-100">Unlock the 🌎 of Crypto</h2>
+            </div> */}
+            <div className="">
+           
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center my-6 pb-6 max-w-3xl mx-auto">
+              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 pt-2 pb-8 px-4 rounded drop-shadow-lg max-w-[60%] md:max-w-[80%] min-h-[140px]">
+                  <div className="text-5xl">💻</div>
+                  <h2 className="text-xl font-semibold">Transparent</h2>
                   <p className="text-gray-500 dark:text-gray-400">Kryptik is <span className="text-green-500">100%</span> open source. View the code <a className="hover:cursor-pointer hover:text-sky-500 text-sky-400" href={`https://github.com/KryptikApp/kryptikwebapp`} target="_blank" rel="noopener noreferrer">here</a>.</p>
               </div>
-              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 px-2 rounded drop-shadow-lg max-w-[50%] min-h-[120px]">
-                  <h2 className="text-xl font-semibold">🕺🏾 Elegant</h2>
-                  <p className="text-gray-500 dark:text-gray-400">Kryptik is designed to be simple and beautiful.</p>
+              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 pt-2 pb-8 px-4 rounded drop-shadow-lg max-w-[60%] md:max-w-[80%] min-h-[140px]">
+                  <div className="text-5xl">🕺🏾 </div>
+                  <h2 className="text-xl font-semibold">Elegant</h2>
+                  <p className="text-gray-500 dark:text-gray-400">Kryptik is designed to be simple and beautiful to the max.</p>
               </div>
-              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 px-2 rounded drop-shadow-lg max-w-[50%] min-h-[120px]">
-                  <h2 className="text-xl font-semibold">🔒 Secure</h2>
-                  <p className="text-gray-500 dark:text-gray-400">Kryptik is noncustodial. We never touch your secret keys.</p>
+              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 pt-2 pb-8 px-4 rounded drop-shadow-lg max-w-[60%] md:max-w-[80%] min-h-[140px]">
+                  <div className="text-5xl">🔒 </div>
+                  <h2 className="text-xl font-semibold">Secure</h2>
+                  <p className="text-gray-500 dark:text-gray-400">Kryptik is noncustodial. We never touch your <span className="text-[#eacc15]">secret</span> keys.</p>
               </div>
-              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 px-2 rounded drop-shadow-lg max-w-[50%] min-h-[120px]">
-                  <h2 className="text-xl font-semibold">🦄 Powerful</h2>
-                  <p className="text-gray-500 dark:text-gray-400">Kryptik supports <span className="text-green-500">10+</span> blockchains from a single app.</p>
+              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 pt-2 pb-8 px-4 rounded drop-shadow-lg max-w-[60%] md:max-w-[80%] min-h-[140px]">
+                  <div className="text-5xl">🦄 </div>
+                  <h2 className="text-xl font-semibold">Powerful</h2>
+                  <p className="text-gray-500 dark:text-gray-400">Kryptik supports <span className="text-sky-400">10+</span> blockchains from a single app.</p>
               </div>
-              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 px-2 rounded drop-shadow-lg max-w-[50%] min-h-[120px]">
-                  <h2 className="text-xl font-semibold">🙏🏽 Free</h2>
+              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 pt-2 pb-8 px-4 rounded drop-shadow-lg max-w-[60%] md:max-w-[80%] min-h-[140px]">
+                  <div className="text-5xl">🙏🏽 </div>
+                  <h2 className="text-xl font-semibold">Free</h2>
                   <p className="text-gray-500 dark:text-gray-400">Kryptik is <span className="text-green-400">100%</span> free. No hidden fees and no service charges.</p>
               </div>
-              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 px-2 rounded drop-shadow-lg max-w-[50%] min-h-[120px]">
-                  <h2 className="text-xl font-semibold">🔮 Magical</h2>
-                  <p className="text-gray-500 dark:text-gray-400">Kryptik is designed with lots of care and a dash of magic.</p>
+              <div className="hover:cursor-pointer hover:z-10 transition ease-in-out hover:scale-110 bg-gray-200 dark:bg-[#111112] border border-gray-300 my-2 pt-2 pb-8 px-4 rounded drop-shadow-lg max-w-[60%] md:max-w-[80%] min-h-[140px]">
+                  <div className="text-5xl">🔮 </div>
+                  <h2 className="text-xl font-semibold">Magical</h2>
+                  <p className="text-gray-500 dark:text-gray-400">Kryptik is designed with lots of care and a dash of <span className="text-purple-300">magic</span>.</p>
+              </div>
+              </div>
               </div>
             </div>
-            
+
           </div>
+          
+          
 
           <div className="min-h-[100vh] max-w-2xl mx-auto pt-4 md:pt-20 mb-5 md:mb-20">
           <div className="mb-10">
@@ -67,12 +85,12 @@ const About: NextPage = () => {
               data: [500, 45000, 20000],
               backgroundColor:[
                 "#c41230",
-                "#A7A7A7",
+                "#6B6EF9",
                 "#00FFA3"
               ]
             }],  
             labels: [
-                'Carnegie Mellon Research Grant',
+                'Carnegie Mellon Research',
                 'Near Foundation',
                 'Solana Foundation'
             ]}}
