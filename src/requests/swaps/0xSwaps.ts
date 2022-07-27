cefcf77045e6af7e2dcc2184d7206967ce5301c4
jettblu <jettblu.fabrications@gmail.com>
2022-07-27 16:05:34 -0700

    Add taker and generic id to 0x swap request

--- src/requests/swaps/0xSwaps.ts
+++ src/requests/swaps/0xSwaps.ts
@@ -1,18 +1,23 @@
 import { KryptikFetch } from "../../kryptikFetch";
+import { IEVMSwapData, parse0xdata } from "../../parsers/0xData";
 
 
-export const fetch0xSwapOptions = async function(buyTokenticker:string, sellTokenTicker:string, sellAmount:number):Promise<null>{
-    buyTokenticker = buyTokenticker.toUpperCase();
-    sellTokenTicker = sellTokenTicker.toUpperCase();
+export const fetch0xSwapOptions = async function(buyTokenId:string, sellTokenId:string, sellAmount:number, takerAddress?:string):Promise<null|IEVMSwapData>{
     try { // add support for multiple pages
-        const url = `https://api.0x.org/swap/v1/quote?buyToken=${buyTokenticker}&sellToken=${sellTokenTicker}&sellAmount=${sellAmount}`;
+        let url:string;
+        if(takerAddress){
+          url = `https://api.0x.org/swap/v1/quote?buyToken=${buyTokenId}&sellToken=${sellTokenId}&sellAmount=${sellAmount}&takerAddress=${takerAddress}`;
+        }
+        else{
+          url = `https://api.0x.org/swap/v1/quote?buyToken=${buyTokenId}&sellToken=${sellTokenId}&sellAmount=${sellAmount}`;
+        }
         const dataResponse = await KryptikFetch(url, {
           timeout: 10000, // 10 secs
         });
         if(!dataResponse || !dataResponse.data) return null;
         // parse api response and return
-        console.log(dataResponse);
-        return null;
+        const evmSwapData:IEVMSwapData = parse0xdata(dataResponse.data);
+        return evmSwapData;
       }
     catch(e){
       console.log("Error while fetching 0x swap data");
