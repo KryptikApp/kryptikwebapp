366a7cbe0283fdda9e64e5eef91db89a3d4dd321
jettblu <jettblu.fabrications@gmail.com>
2022-07-27 16:02:09 -0700

    Add logic for swap router

--- src/handlers/swaps/index.ts
+++ src/handlers/swaps/index.ts
@@ -1,15 +1,31 @@
+import { Network, NetworkFamily } from "hdseedloop";
+import { networkFromNetworkDb } from "../../helpers/utils/networkUtils";
+import { KryptikEVMTransaction, KryptikTransaction } from "../../models/transactions";
 import { KryptikProvider } from "../../services/models/provider";
 import { TokenAndNetwork } from "../../services/models/token";
-import { IBuildEVMSwapParams } from "./EVMSwap";
+import { BuildEVMSwapTransaction, IBuildEVMSwapParams } from "./EVMSwap";
 import { isSwapAvailable } from "./utils";
 
 export interface IBuildSwapParams{
     sellTokenAndNetwork:TokenAndNetwork,
     buyTokenAndNetwork:TokenAndNetwork,
+    tokenAmount:number,
+    // price of the sell base network token
+    sellNetworkTokenPriceUsd:number,
     fromAccount:string,
     kryptikProvider:KryptikProvider
 }
 
-export async function SwapToken(evmSwapParams?:IBuildEVMSwapParams){
-    
+export async function BuildSwapTokenTransaction(swapParams:IBuildSwapParams):Promise<KryptikTransaction|null>{
+    const{sellTokenAndNetwork, buyTokenAndNetwork, sellNetworkTokenPriceUsd, fromAccount, kryptikProvider} = {...swapParams}
+    const sellBaseNetwork:Network = networkFromNetworkDb(sellTokenAndNetwork.baseNetworkDb);
+    const buyBaseNetwork:Network = networkFromNetworkDb(buyTokenAndNetwork.baseNetworkDb);
+    if(!isSwapAvailable(buyTokenAndNetwork.baseNetworkDb, sellTokenAndNetwork.baseNetworkDb)){
+        return null;
+    }
+    if(buyBaseNetwork.networkFamily == NetworkFamily.EVM && sellBaseNetwork.networkFamily == NetworkFamily.EVM){
+        let kryptikEVMTx:KryptikEVMTransaction|null = await BuildEVMSwapTransaction(swapParams);
+        return kryptikEVMTx;
+    }
+    return null;
 }
