bec80babb0c6a6b8172e4162e91193ba8b7fea7a
jettblu <jettblu.fabrications@gmail.com>
2022-07-28 12:47:16 -0700

    Update poap parser to filter traits

--- src/parsers/poap.ts
+++ src/parsers/poap.ts
@@ -1,6 +1,6 @@
 // poap parser adapted from rainbowme wallet
 import { get } from 'lodash';
-import { INFTMetadata } from './nftEthereum';
+import { INFTMetadata, ITraitType } from './nftEthereum';
 
 /**
  * @desc parse poaps
@@ -10,8 +10,9 @@ import { INFTMetadata } from './nftEthereum';
 
 export const parsePoaps = function(data:any):INFTMetadata[]{
   const poaps = get(data, 'data', null);
-  return poaps.map((poap:any) => {
-    return {
+  let poapsToReturn:INFTMetadata[] = [];
+  for(const poap of poaps){
+    let poapToAdd:INFTMetadata = {
       animation_url: poap.event.image_url,
       asset_contract: {
         address: '0x22c1f6050e56d2876009903609a2cc3fef83b415',
@@ -19,9 +20,7 @@ export const parsePoaps = function(data:any):INFTMetadata[]{
         nft_version: '3.0',
         schema_name: 'ERC721',
         symbol: 'The Proof of Attendance Protocol',
-        total_supply: null,
       },
-      background: null,
       collection: {
         description: 'The Proof of Attendance Protocol',
         external_url: 'https://poap.xyz/',
@@ -32,40 +31,42 @@ export const parsePoaps = function(data:any):INFTMetadata[]{
       },
       description:  poap.event.description,
       external_link:  poap.event.event_url,
-      familyImage:
-        'https://lh3.googleusercontent.com/FwLriCvKAMBBFHMxcjqvxjTlmROcDIabIFKRp87NS3u_QfSLxcNThgAzOJSbphgQqnyZ_v2fNgMZQkdCYHUliJwH-Q=s60',
-      familyName: 'POAP',
       id:  poap.event.id,
       image_url:  poap.event.image_url,
       isPoap: true,
-      isSendable: false,
-      lastPrice: null,
-      lastSalePaymentToken: null,
       name:  poap.event.name,
       permalink:  poap.event.event_url,
-      traits: [
-        {
-          trait_type: 'country',
-          value:  poap.event.country,
-        },
-        {
-          trait_type: 'startDate',
-          value:  poap.event.start_date,
-        },
-        {
-          trait_type: 'endDate',
-          value:  poap.event.start_date,
-        },
-        {
-          trait_type: 'city',
-          value:  poap.event.city,
-        },
-        {
-          trait_type: 'eventURL',
-          value:  poap.event.event_url,
-        },
-      ],
-      uniqueId: `0x22c1f6050e56d2876009903609a2cc3fef83b415_${poap.event.id}`,
+      networkTicker: "eth"
     };
-  });
+    let traitsToAdd:ITraitType[] = []
+    if(poap.event.city){
+      traitsToAdd.push(
+        {trait_type: 'city',
+        value:  poap.event.city,})
+    }
+    if(poap.event.country){
+      traitsToAdd.push(
+        {trait_type: 'country',
+        value:  poap.event.country,})
+    }
+    if(poap.event.start_date){
+      traitsToAdd.push(
+        {trait_type: 'startDate',
+        value:  poap.event.start_date,})
+    }
+    if(poap.event.end_date){
+      traitsToAdd.push(
+        {trait_type: 'endDate',
+        value:  poap.event.end_date,})
+    }
+    if(poap.event.event_url){
+      traitsToAdd.push(
+        {trait_type: 'eventUrl',
+        value:  poap.event.event_url,})
+    }
+    poapToAdd.traits = traitsToAdd;
+    poapsToReturn.push(poapToAdd);
+  }
+  
+  return poapsToReturn;
 };
