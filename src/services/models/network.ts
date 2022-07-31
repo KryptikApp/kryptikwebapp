009bd35962a03cede890329dc3c9ebb11d4d4958
jettblu <jettblu.fabrications@gmail.com>
2022-07-31 16:08:53 -0700

    Add default uniswap token data

--- src/services/models/network.ts
+++ src/services/models/network.ts
@@ -1,7 +1,7 @@
 // data model for cryptocurrency networks (blockchains) pulled from firebase
 
 import { Network, NetworkFromTicker } from "hdseedloop"
-import { TokenAndNetwork } from "./token"
+import { TokenAndNetwork, TokenDb } from "./token"
 
 
 export interface EVMData{
@@ -51,12 +51,46 @@ export const defaultNetworkDb:NetworkDb = {
     isTestnet: false
 }
 
+// refer to blockchain assets repo for up to date uniswap token data
+const defaultUniswapTokenData:TokenDb = {
+    name: "Uniswap",
+    symbol: "UNI",
+    coingeckoId: "uniswap",
+    hexColor: "#FF007A",
+    decimals: 18,
+    chainData:[{
+        chainId: 1,
+        ticker: "eth",
+        address: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
+    },
+    {
+        chainId: 3,
+        ticker: "eth(ropsten)",
+        address: "0xC8F88977E21630Cf93c02D02d9E8812ff0DFC37a"
+    }],
+    logoURI: "https://assets.coingecko.com/coins/images/12504/large/uniswap-uni.png?1600306604",
+    extensions: {
+       link: "https://uniswap.org/",
+       description: "UNI is the governance token for Uniswap. UNI was introduced on September 16th, 2020 through an airdrop to users who have interacted with the protocol either by swapping tokens or by providing liquidity. The UNI token allows token holders to participate in the governance of the protocol. Key decisions such as usage of the treasury or future upgrades can be decided through a governance vote."
+    },
+    tags:[
+        "defi"
+    ]
+}
+
 export const placeHolderEVMAddress:string = "0xb794f5ea0ba39494ce839613fffba74279579268";
 
 export const placeHolderSolAddress:string = "DYw8jCTfwHNRJhhmFcbXvVDTqWMEVFBX6ZKUmG5CNSKK"
 
 export const defaultTokenAndNetwork:TokenAndNetwork = {
     baseNetworkDb:defaultNetworkDb
+}
+
+export const defaultUniswapTokenAndNetwork:TokenAndNetwork = {
+    baseNetworkDb:defaultNetworkDb,
+    tokenData:{
+        tokenDb: defaultUniswapTokenData
+    }
 }
 
 export interface NetworkBalanceParameters{
