e9c8e0518106a542dd0e59cc7a90698de34c5e88
jettblu <jettblu.fabrications@gmail.com>
2022-08-08 17:50:40 -0700

    Delete balance items params

--- components/lists/ListItemBalance.tsx
+++ components/lists/ListItemBalance.tsx
@@ -1,43 +1,60 @@
+import { add } from "lodash";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { formatTicker } from "../../src/helpers/utils/networkUtils";
+import { roundCryptoAmount, roundUsdAmount } from "../../src/helpers/utils/numberUtils";
+import { TokenAndNetwork } from "../../src/services/models/token";
 import { useKryptikThemeContext } from "../ThemeProvider";
 
 interface Props{
-    title:string,
-    subtitle:string,
-    imgSrc:string,
-    imgSrcSecondary?:string,
-    amount:string,
-    amountUSD:string,
-    coinGeckoId: string,
+    tokenAndNetwork:TokenAndNetwork
     // used for headers to asset
-    infoLink?:boolean,
-    networkTicker?:string,
-    tokenTicker?:string
+    addInfoLink?:boolean,
+    htmlKey:number
 }
 
 
 const ListItemBalance:NextPage<Props> = (props) => {
     const router = useRouter();
-    const {title, subtitle, imgSrc, amount, amountUSD, infoLink, imgSrcSecondary, networkTicker, tokenTicker} = props;
+    const {addInfoLink=true, tokenAndNetwork, htmlKey} = props;
     const {hideBalances} = useKryptikThemeContext();
+    const title = tokenAndNetwork.tokenData?tokenAndNetwork.tokenData.tokenDb.name:tokenAndNetwork.baseNetworkDb.fullName;
+    const imgSrcPrimary = tokenAndNetwork.tokenData?tokenAndNetwork.tokenData.tokenDb.logoURI:tokenAndNetwork.baseNetworkDb.iconPath;
+    const imgSrcSecondary = tokenAndNetwork.tokenData?tokenAndNetwork.baseNetworkDb.iconPath:null;
+    const subtitle = tokenAndNetwork.tokenData?formatTicker(tokenAndNetwork.tokenData.tokenDb.symbol):formatTicker(formatTicker(tokenAndNetwork.baseNetworkDb.ticker));
+    let amount = null;
+    let amountFiat = null;
+    if(tokenAndNetwork.tokenData?.tokenBalance){
+
+        amount = roundCryptoAmount(Number(tokenAndNetwork.tokenData.tokenBalance.amountCrypto));
+        amountFiat = roundUsdAmount(Number(tokenAndNetwork.tokenData.tokenBalance.amountUSD));
+    }
+    else{
+        if(tokenAndNetwork.networkBalance){
+            amount = roundCryptoAmount(Number(tokenAndNetwork.networkBalance.amountCrypto));
+            amountFiat = roundUsdAmount(Number(tokenAndNetwork.networkBalance.amountUSD));
+        }
+    }
 
     // redirect to asset info page if infolink provided
     const handleOnClick = function(){
-        if(infoLink){
-            if(tokenTicker){
-                router.push( { pathname: '../coins/coinInfo', query:{networkTicker:networkTicker, tokenTicker:tokenTicker} } );
+        if(addInfoLink){
+            if(tokenAndNetwork.tokenData){
+                router.push( { pathname: '../coins/coinInfo', query:{networkTicker:tokenAndNetwork.baseNetworkDb.ticker, tokenTicker:tokenAndNetwork.tokenData.tokenDb.symbol}} );
             }
             else{
-                router.push( { pathname: '../coins/coinInfo', query:{networkTicker:networkTicker} } );
+                router.push( { pathname: '../coins/coinInfo', query:{networkTicker:tokenAndNetwork.baseNetworkDb.ticker} } );
             }
         }
     }
     return(
-      <li key={`${title} ${Math.random()}`} className="py-3 sm:py-4 hover:cursor-pointer hover:gray-100 hover:dark:bg-[#141414]" onClick={()=>handleOnClick()}>
+      <div>
+      {
+        (amount && amountFiat) &&
+        <li key={`${htmlKey}`} className="py-3 sm:py-4 hover:cursor-pointer hover:gray-100 hover:dark:bg-[#141414]" onClick={()=>handleOnClick()}>
           <div className="flex items-center space-x-4">
               <div className="flex-shrink-0">
-                  <img className="w-8 h-8 rounded-full inline" src={imgSrc} alt={`${title} image`}/>
+                  <img className="w-8 h-8 rounded-full inline" src={imgSrcPrimary} alt={`${title} image`}/>
                   {
                     imgSrcSecondary &&
                     <img className="w-4 h-4 -ml-2 drop-shadow-lg mt-4 rounded-full inline" src={imgSrcSecondary} alt={`${title} secondary image`}/>
@@ -58,7 +75,7 @@ const ListItemBalance:NextPage<Props> = (props) => {
               <div className="flex-1 min-w-0">
                   <p className={`text-lg font-medium text-gray-900 truncate dark:text-gray-200 ${hideBalances?"font-bold blur-sm":""}`}>
                       $
-                      {amountUSD}
+                      {amountFiat}
                   </p>
                   <p className={`text-sm text-gray-500 truncate dark:text-gray-400 ${hideBalances?"font-bold blur-sm":""}`}>
                       {amount}
@@ -66,6 +83,9 @@ const ListItemBalance:NextPage<Props> = (props) => {
               </div>
           </div>
       </li>
+      }
+      </div>
+      
     )   
 }
 
