4f952029c1f5e9b57135e197e0b942e0005388ce
jettblu <jettblu.fabrications@gmail.com>
2022-08-11 00:54:13 -0700

    Update erc20 abi to include allowance

--- src/abis/erc20Abi.ts
+++ src/abis/erc20Abi.ts
@@ -8,6 +8,15 @@ let erc20Abi:string[] = [
   
     // Send some of your tokens to someone else
     "function transfer(address to, uint amount)",
+
+    // How many tokens spender is allowed to spend on your behalf
+    "function allowance(address owner, address spender) view returns (uint)",
+    
+    // Sets amount as the allowance of spender over the caller’s tokens.
+    "function approve(address spender, uint amount) returns (bool)",
+
+    // Sets amount as the allowance of spender over the caller’s tokens.
+    "function increaseAllowance(address spender, uint amount) returns (bool)",
   
     // An event triggered whenever anyone transfers to someone else
     "event Transfer(address indexed from, address indexed to, uint amount)"
