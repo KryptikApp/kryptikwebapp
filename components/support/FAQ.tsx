9d18afe023c1f32272ad92fb1ecf0b26604dc360
jettblu <jettblu.fabrications@gmail.com>
2022-08-11 12:43:54 -0700

    Add dark hover border to FAQ

--- components/support/FAQ.tsx
+++ components/support/FAQ.tsx
@@ -9,7 +9,7 @@ interface Props {
 const FAQ:NextPage<Props> = (props) => {
     const faq = props.faq;
     return(
-        <div className="max-w-full bg-gray-50 dark:bg-[#070707] border border-gray-600 dark:border-gray-200 pt-2 pb-4 text-black dark:text-white rounded px-2 hover:border-sky-400">
+        <div className="max-w-full bg-gray-50 dark:bg-[#070707] border border-gray-600 dark:border-gray-200 pt-2 pb-4 text-black dark:text-white rounded px-2 hover:border-sky-400 dark:hover:border-sky-400">
             <h1 className="text-xl font-bold text-slate-700 dark:text-slate-100 text-left">{faq.question}</h1>
             <p className="text-gray-800 dark:text-gray-100 text-lg">{faq.answer}</p>
         </div>
