5ca20812fc618c73367af1e3114315ce2571d0d7
jettblu <jettblu.fabrications@gmail.com>
2022-08-19 13:22:22 -0700

    Update testing page greeting

--- pages/support/testing.tsx
+++ pages/support/testing.tsx
@@ -18,7 +18,7 @@ const Testing: NextPage = () => {
                 <h1 className="text-5xl font-bold sans mb-5">
                         You're Excited!
                 </h1>
-                <p className="leading-loose text-2xl text-justify"><span className="text-sky-500 dark:text-sky-400 font-semibold">So are we.</span></p>
+                <p className="leading-loose text-2xl text-justify"><span className="text-sky-500 dark:text-sky-400 font-semibold">So are we (:</span></p>
                 <p className="leading-loose mb-2 text-xl text-justify">Right now... the Kryptik wallet is being improved by a small group of testers. To get early access to our powerful wallet, join the waitlist <span className="hover:cursor-pointer text-green-400 hover:text-green-500"><Link href="../waitlist">here</Link></span>.</p>
             </div>
         </div>
