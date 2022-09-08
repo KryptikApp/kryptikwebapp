01a017c5c61f5940aa2adea7f5b76edfbcb55d2a
jettblu <jettblu.fabrications@gmail.com>
2022-09-08 14:18:09 -0400

    Update timeout for price requests

--- src/requests/prices/index.ts
+++ src/requests/prices/index.ts
@@ -5,7 +5,7 @@ import { KryptikFetch } from "../../kryptikFetch";
 export const fetchServerHistoricalPrices = async function(id: string,
     days:number):Promise<CoinMarketChartResponse|null>{
     // request balance data from api
-    let dataResponse = await KryptikFetch('/api/prices', {method:"POST", timeout:2000, headers:{'Content-Type': 'application/json',}, body: JSON.stringify({id:id, days:days})})
+    let dataResponse = await KryptikFetch('/api/prices', {method:"POST", timeout:10000, headers:{'Content-Type': 'application/json',}, body: JSON.stringify({id:id, days:days})})
     if(dataResponse.status != 200) return null
     let historicalData = await dataResponse.data.priceData;
     if(!historicalData) return null;
