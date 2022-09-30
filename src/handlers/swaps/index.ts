b0c641046737dceea89280b6c6df5a143a61dae2
jettblu <jettblu.fabrications@gmail.com>
2022-09-30 19:54:43 -0400

    Add hop bridge to swap router

--- src/handlers/swaps/index.ts
+++ src/handlers/swaps/index.ts
@@ -3,9 +3,10 @@ import { networkFromNetworkDb } from "../../helpers/utils/networkUtils";
 import { KryptikTransaction } from "../../models/transactions";
 import { KryptikProvider } from "../../services/models/provider";
 import { TokenAndNetwork } from "../../services/models/token";
+import { BuildHopBridgeTransaction } from "./bridges/hop/hopBridge";
 import { BuildEVMSwapTransaction } from "./EVMSwap";
 import { BuildSolSwapTransaction } from "./SolanaSwap";
-import { isSwapAvailable } from "./utils";
+import { isSwapAvailable, isValidHopBridge } from "./utils";
 
 export interface IBuildSwapParams{
     // price of the underlying network coin
@@ -27,11 +28,19 @@ export async function BuildSwapTokenTransaction(swapParams:IBuildSwapParams):Pro
     if(!isSwapAvailable(buyTokenAndNetwork, sellTokenAndNetwork)){
         return null;
     }
+    // make sure we route same network swaps to 0x
+    if(isValidHopBridge(buyTokenAndNetwork, sellTokenAndNetwork)){
+        console.log("Routing swap to hop bridge builder")
+        let kryptikBridgeTx:KryptikTransaction|null = await BuildHopBridgeTransaction(swapParams);
+        return kryptikBridgeTx;
+    }
     if(buyBaseNetwork.networkFamily == NetworkFamily.EVM && sellBaseNetwork.networkFamily == NetworkFamily.EVM){
+        console.log("Routing swap to 0x swap builder")
         let kryptikEVMTx:KryptikTransaction|null = await BuildEVMSwapTransaction(swapParams);
         return kryptikEVMTx;
     }
     if(buyBaseNetwork.networkFamily == NetworkFamily.Solana && sellBaseNetwork.networkFamily == NetworkFamily.Solana){
+        console.log("Routing swap to Solana swap builder")
         let kryptikSolTx:KryptikTransaction|null = await BuildSolSwapTransaction(swapParams);
         return kryptikSolTx;
     }
