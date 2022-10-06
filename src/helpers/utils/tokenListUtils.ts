350a7a291bad5497645959714402ee84789f3e34
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-05 20:33:22 -0400

    Delete unused utils

--- src/helpers/utils/tokenListUtils.ts
+++
@@ -1,6 +0,0 @@
-import { TokenDb } from "../../services/models/token";
-
-// UPDATE TO FILTER TOKENS THAT HAVE MIN. 1 NEAR FAMILY CHAINDATA
-export const getNep141Tokens = function(tokenList:TokenDb[]){
-    return;
-}
