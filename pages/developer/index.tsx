2073a0c67f0c3e0436be2255ab8ac71cd858585c
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-09 01:19:53 -0400

    Add developer docs pages

---
+++ pages/developer/index.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next"
+import { Toaster } from "react-hot-toast"
+
+export default function DevDocsHome(){
+    return (
+      <div className="max-h-full overflow-y-auto pt-10">
+          <Toaster/>
+          
+          <div className="text-black dark:text-white max-w-3xl mx-auto">
+                <div className="mb-4">
+                  <p className="text-4xl font-bold mb-2">Developer Documentation</p>
+                  <p className="text-xl text-slate-500 dark:text-slate-400">A builders manual for wallets. By builders, for builders.</p>
+                </div>
+                <div>
+                  <p className="text-xl font-semibold mb-1">What's Kryptik?</p>
+                  <div className="text-lg">
+                  <p>Kryptik is a noncustodial wallet for digital assets. The Kryptik wallet works across multiple blockchains and allows users to authenticate with simple magic links.</p>
+                  </div>
+                </div>
+          </div>
+  
+      </div>
+   
+    )
+}
+
