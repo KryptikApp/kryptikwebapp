d6068489443123c3339ecc3f5dcccc09efa71fdb
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-09 01:17:06 -0400

    Update divider style

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 
 const Divider:NextPage = () => {
     return(
-        <hr className="my-4"/>
+        <div className="my-4 h-[1px] bg-gray-50 dark:bg-gray-800"/>
     )   
 }
 
