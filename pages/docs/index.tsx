4dbdfae8b9e26f02086eb284a473bc6b048d75b0
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-09 01:19:06 -0400

    Add developer link to docs home

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Divider from '../../components/Divider';
 import DocCategoryPreview from '../../components/docs/docCategoryPreview';
 import { useKryptikThemeContext } from '../../components/ThemeProvider';
 import { getAllDocs } from '../../src/helpers/docs';
@@ -11,8 +12,9 @@ type Props = {
 
 export default function DocsHome({allDocs}:Props){
   const {isDark} = useKryptikThemeContext();
-  const essentialDocs = allDocs.filter(d=>d.category=="essentials");
-  const web3BasicsDocs = allDocs.filter(d=>d.category=="web3 basics");
+  const essentialDocs = allDocs.filter(d=>d.category.toLowerCase()=="essentials");
+  const web3BasicsDocs = allDocs.filter(d=>d.category.toLowerCase() =="web3 basics");
+  const securityDocs = allDocs.filter(d=>d.category.toLowerCase() =="security");
 
   return (
 
@@ -36,11 +38,14 @@ export default function DocsHome({allDocs}:Props){
                 <div className='flex flex-col space-y-8'>
                     <DocCategoryPreview docs={essentialDocs} categoryName={'The Essentials'} description={'Create your first wallet and start exploring the decentralized web.'} flip={false}/>
                     <DocCategoryPreview docs={web3BasicsDocs} categoryName={'Web3 Basics'} description={'Learn more about decentralized networks and emerging applications.'} flip={true}/>
+                    <DocCategoryPreview docs={securityDocs} categoryName={'Security'} description={'Keep your wallet safe with best practices.'} flip={false}/>
                 </div>
             </div>
             {/* supported token */}
             <div className="max-w-3xl mx-auto my-8">
-                <div className="flex flex-col lg:flex-row">
+            <Divider/>
+                <p className="text-3xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-b from-green-400 to-sky-600">More Resources</p>
+                <div className="flex flex-col space-y-8">
                     <Link href="../support/supportedProtocols">
                         <div className="rounded-md border-l-4 border-sky-400 px-2 py-4 hover:cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-900">
                             <h1 className="text-2xl text-left">
@@ -48,6 +53,16 @@ export default function DocsHome({allDocs}:Props){
                             </h1>
                             <h2 className="text-lg text-gray-800 dark:text-gray-200">
                                 Learn about what tokens Kryptik supports.
+                            </h2>
+                        </div>
+                    </Link>
+                    <Link href="../developer">
+                        <div className="rounded-md border-l-4 border-green-500 px-2 py-4 hover:cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-900">
+                            <h1 className="text-2xl text-left">
+                            Developers
+                            </h1>
+                            <h2 className="text-lg text-gray-800 dark:text-gray-200">
+                                Start contributing to essential wallet software.
                             </h2>
                         </div>
                     </Link>
@@ -69,7 +84,8 @@ export default function DocsHome({allDocs}:Props){
 }
 
 export const getStaticProps = async () => {
-    const allDocs = getAllDocs([
+    const allDocs = getAllDocs({
+        fields:[
         "slug",
         "title",
         "lastUpdate",
@@ -78,7 +94,7 @@ export const getStaticProps = async () => {
         "content",
         "category",
         "emoji"
-    ])
+    ]})
   
     return {
       props: { allDocs },
