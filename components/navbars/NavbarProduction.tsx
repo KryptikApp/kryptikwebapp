bae9754f0b5829124f47c0aa76402393b6f71158
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-11 22:32:02 -0400

    Add doc link to production navbar

--- components/navbars/NavbarProduction.tsx
+++ components/navbars/NavbarProduction.tsx
@@ -72,6 +72,10 @@ const NavbarProduction:NextPage = () => {
                 :<Link href="../about"><span className={`p-2 lg:px-4 md:mx-2 text-gray-400 rounded hover:bg-gray-200 hover:cursor-pointer hover:text-gray-700 dark:hover:bg-gray-100 dark:hover:text-black transition-colors duration-300 ${router.pathname == "/about" ? "font-bold" : ""} `}>About</span></Link>
                 }
                 {
+                    !authUser &&<Link href="../docs"><span className={`p-2 lg:px-4 md:mx-2 text-gray-400 rounded hover:bg-gray-200 hover:cursor-pointer hover:text-gray-700 dark:hover:bg-gray-100 dark:hover:text-black transition-colors duration-300 ${router.pathname.startsWith("/docs") ? "font-bold" : ""} `}>Docs</span></Link>
+
+                }
+                {
                     authUser?
                     <Link href="../explore"><span className={`p-2 lg:px-4 md:mx-2 text-gray-400 rounded hover:bg-gray-200 hover:cursor-pointer hover:text-gray-700 dark:hover:bg-gray-100 dark:hover:text-black transition-colors duration-300 ${router.pathname == "/explore" ? "font-bold" : ""} `}>Explore</span></Link>:
                     <Link href="../vision"><span className={`p-2 lg:px-4 md:mx-2 text-gray-400 rounded hover:bg-gray-200 hover:cursor-pointer hover:text-gray-700 dark:hover:bg-gray-100 dark:hover:text-black transition-colors duration-300 ${router.pathname == "/vision" ? "font-bold" : ""} `}>Vision</span></Link>
