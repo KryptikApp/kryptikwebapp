c1c546311f84a79726042c26e2bfc8633bbae43a
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-13 11:31:04 -0400

    Add blog pages

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,118 @@
+import { useRouter } from "next/router"
+import { getAllDocs, getDocBySlug, getDocsByCategory } from "../../src/helpers/docs"
+import markdownToHtml from "../../src/helpers/docs/markdownFormat"
+import Custom404 from "../404"
+import { DocType, DocTypeEnum } from "../../src/helpers/docs/types"
+import DocHeader from "../../components/docs/docHeader"
+import DocContent from "../../components/docs/docContent"
+import DocKeepReadingPreview from "../../components/docs/docKeepReadingPreview"
+import EditThisPage from "../../components/EditThisPage"
+
+type Props = {
+    doc: DocType
+    recommendedDocs?: DocType[]
+}
+
+export default function Post({ doc, recommendedDocs }: Props) {
+    const router = useRouter()
+    // TODO: 2x check to make sure we correctly set 404
+    if (!router.isFallback && !doc?.slug) {
+      console.warn(`Unable to find this doc!`)
+      return <Custom404/>;
+    }
+    const githubLink:string = `https://github.com/KryptikApp/kryptikwebapp/blob/main/docs/${doc.slug}.md`
+    const readNext:DocType[] = recommendedDocs?recommendedDocs:[];
+    return (
+          <div>
+            <div className="max-w-2xl mx-auto">
+                {
+                router.isFallback?
+                <h1 className="text-2xl text-black dark:text-white">Loading....</h1>
+                :
+                <div>
+                    <DocHeader title={doc.title} image={doc.image||undefined} lastUpdated={doc.lastUpdate} emoji={doc.emoji||undefined}/>
+                    <DocContent content={doc.content}/>
+                    {
+                      readNext.length!=0 &&
+                      <div className="my-8">
+                        <h1 className="font-bold text-3xl text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-emerald-600 text-left">Keep Learning</h1>
+                        <hr className="mt-2 mb-4"/>
+                        <div className="flex flex-col space-y-4">
+                        {
+                          readNext.map((doc:DocType, index:number)=>
+                          <DocKeepReadingPreview baseUrl="/docs/" key={`keep reading preview ${index}`} title={doc.title} image={doc.image||undefined} emoji={doc.emoji||undefined} oneLiner={doc.oneLiner} slug={doc.slug}/>
+                          )
+                        }
+                        </div>
+                    </div>
+                    }
+                    
+                </div>
+                }
+                {/* edit this page */}
+                <div className="max-w-3xl mx-auto my-8">
+                    <EditThisPage link={githubLink}/>
+                </div>
+            </div>
+            <div className="h-[24vh]">
+              {/* padding div for space between top and main elements */}
+            </div>
+          </div>
+    )
+  }
+  
+  type Params = {
+    params: {
+      slug: string
+    }
+  }
+  
+  export async function getStaticProps({ params }: Params) {
+    let newDoc:DocType = getDocBySlug({slug: params.slug, fields:[
+        "slug",
+        "title",
+        "lastUpdate",
+        "image",
+        "oneLiner",
+        "content",
+        "category",
+        "emoji"
+    ],
+    docEnum:DocTypeEnum.Blog})
+    // create html from markdown content
+    const content:string = await markdownToHtml(newDoc.content || '');
+    newDoc.content = content
+    const newRecommendedDocs:DocType[] = getDocsByCategory({category:newDoc.category, fields:[
+      "slug",
+      "title",
+      "lastUpdate",
+      "image",
+      "oneLiner",
+      "content",
+      "category",
+      "emoji"
+    ],
+    slugToExclude: newDoc.slug,
+    docEnum:DocTypeEnum.Blog});
+    return {
+      props: {
+        doc: newDoc,
+        recommendedDocs: newRecommendedDocs
+      },
+    }
+  }
+  
+  export async function getStaticPaths() {
+    const docs = getAllDocs({fields:['slug'], docEnum:DocTypeEnum.Blog})
+  
+    return {
+      paths: docs.map((doc) => {
+        return {
+          params: {
+            slug: doc.slug,
+          },
+        }
+      }),
+      fallback: false,
+    }
+  }
---
+++ pages/blog/index.tsx
@@ -0,0 +1,102 @@
+import Link from 'next/link';
+import Divider from '../../components/Divider';
+import DocCategoryPreview from '../../components/docs/docCategoryPreview';
+import { useKryptikThemeContext } from '../../components/ThemeProvider';
+import { getAllDocs } from '../../src/helpers/docs';
+import { DocType, DocTypeEnum } from '../../src/helpers/docs/types';
+
+
+type Props = {
+    allDocs: DocType[]
+}
+
+export default function BlogHome({allDocs}:Props){
+  const {isDark} = useKryptikThemeContext();
+  const essentialDocs = allDocs.filter(d=>d.category.toLowerCase()=="essentials");
+  const web3BasicsDocs = allDocs.filter(d=>d.category.toLowerCase() =="web3 basics");
+  const securityDocs = allDocs.filter(d=>d.category.toLowerCase() =="security");
+
+  return (
+
+    <div>
+        <div className="dark:text-white graphPaper2">
+            <div className="max-w-2xl mx-auto mb-8">
+                <div className="mb-4 rounded-lg bg-gray-100 dark:bg-gray-900 border border-sky-400 hover:shadow-md hover:shadow-sky-400 px-2 py-4 ">
+                    <div className="flex flex-row space-x-1">
+                        <p className="text-6xl pt-2">📜</p>
+                        <div className="flex flex-col space-y-2">
+                            <h1 className="text-3xl text-left font-bold sans">
+                                    Docs &amp; Threads
+                            </h1>
+                            <p className="leading-loose text-xl text-justify dark:text-gray-400">A collection of helpful how-tos</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* categories and article previews */}
+            <div className="max-w-3xl mx-auto">
+                <div className='flex flex-col space-y-8'>
+                    <DocCategoryPreview docs={essentialDocs} categoryName={'The Essentials'} description={'Create your first wallet and start exploring the decentralized web.'} flip={false}/>
+                    <DocCategoryPreview docs={web3BasicsDocs} categoryName={'Web3 Basics'} description={'Learn more about decentralized networks and emerging applications.'} flip={true}/>
+                    <DocCategoryPreview docs={securityDocs} categoryName={'Security'} description={'Keep your wallet safe with best practices.'} flip={false}/>
+                </div>
+            </div>
+            {/* supported token */}
+            <div className="max-w-3xl mx-auto my-8">
+            <Divider/>
+                <p className="text-3xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-b from-green-400 to-sky-600">More Resources</p>
+                <div className="flex flex-col space-y-8">
+                    <Link href="../support/supportedProtocols">
+                        <div className="rounded-md border-l-4 border-sky-400 px-2 py-4 hover:cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-900">
+                            <h1 className="text-2xl text-left">
+                            Asset Finder
+                            </h1>
+                            <h2 className="text-lg text-gray-800 dark:text-gray-200">
+                                Learn about what tokens Kryptik supports.
+                            </h2>
+                        </div>
+                    </Link>
+                    <Link href="../developer">
+                        <div className="rounded-md border-l-4 border-green-500 px-2 py-4 hover:cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-900">
+                            <h1 className="text-2xl text-left">
+                            Developers
+                            </h1>
+                            <h2 className="text-lg text-gray-800 dark:text-gray-200">
+                                Start contributing to essential wallet software.
+                            </h2>
+                        </div>
+                    </Link>
+                    
+                </div>
+            </div>
+        </div>
+
+
+        <div className="h-[6rem]">
+          {/* padding div for space between bottom and main elements */}
+        </div>
+
+    </div>
+       
+
+ 
+  )
+}
+
+export const getStaticProps = async () => {
+    const allDocs = getAllDocs({
+        fields:[
+        "slug",
+        "title",
+        "lastUpdate",
+        "image",
+        "oneLiner",
+        "content",
+        "category",
+        "emoji"
+    ], docEnum:DocTypeEnum.Blog})
+  
+    return {
+      props: { allDocs },
+    }
+}
