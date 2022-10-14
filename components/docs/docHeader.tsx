62b5794060dfbb40138e6170811f7062a73ddef6
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-13 23:54:38 -0400

    Add icon toggle param

--- components/docs/docHeader.tsx
+++ components/docs/docHeader.tsx
@@ -8,9 +8,10 @@ type Props = {
     emoji?: string
     hideBackButton?:boolean
     lastUpdated: string
+    hideIcon?: boolean
   }
   
-  const DocHeader = ({ title, image, lastUpdated, emoji, hideBackButton}: Props) => {
+  const DocHeader = ({ title, image, lastUpdated, emoji, hideBackButton, hideIcon}: Props) => {
     return (
       <div className="flex flex-col space-y-4 max-w-3xl mx-auto">
             {
@@ -19,13 +20,17 @@ type Props = {
             }
             <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
               {
-                image!=undefined?
-                <img src={image} width="40"/>:
-                emoji &&
-                <p className="text-3xl dark:text-white">{emoji}</p>
+                !hideIcon &&
+                <div>
+                  {
+                    image!=undefined?
+                    <img src={image} width="40"/>:
+                    emoji &&
+                    <p className="text-3xl dark:text-white">{emoji}</p>
+                  }
+                </div>
               }
-                
-                <h1 className="text-black dark:text-white text-4xl font-bold">{title}</h1>
+              <h1 className="text-black dark:text-white text-4xl font-bold">{title}</h1>
             </div>
             <DateFormatter dateString={lastUpdated}/>
       </div>
