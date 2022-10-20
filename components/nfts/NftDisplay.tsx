1fb3c554c3563869004a9c946c4447dc0dff5de4
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-20 18:41:04 -0400

    Add click to close function for nft view

--- components/nfts/NftDisplay.tsx
+++ components/nfts/NftDisplay.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { INFTMetadata, ITraitType } from "../../src/parsers/nftEthereum";
 import { defaultNetworkDb } from "../../src/services/models/network";
 import { useKryptikAuthContext } from "../KryptikAuthProvider";
@@ -14,12 +14,32 @@ interface Props {
 }
 const NftDisplay:NextPage<Props> = (props) => {
     const{nftMetaData} = {...props};
+    const cointainerId = `${nftMetaData.id}Container`;
+    const nftCardId = `${nftMetaData.name?nftMetaData.name:nftMetaData.collection.name}Card`;
+    const modalId = `${nftMetaData.id}Modal`;
     const{isDark} = useKryptikThemeContext();
     const {kryptikService} = useKryptikAuthContext();
     const [showModal, setShowModal] = useState(false);
     const networkDb = kryptikService.getNetworkDbByTicker(nftMetaData.networkTicker)||defaultNetworkDb;
+    useEffect(()=>{
+        if(!document) return;
+        const modal = document.getElementById(modalId);
+        if(!modal) return;
+        // close modal if you click area around the nft card
+        modal.addEventListener("click", function(e){
+            console.log("clicked!");
+            if(showModal) setShowModal(false);
+        })
+        // if you click the nft card itself, don't close the card!
+        // stop propogation
+        const nftCard = document.getElementById(nftCardId);
+        if(!nftCard) return;
+        nftCard.addEventListener("click", function(e){
+            e.stopPropagation();
+        })
+    },[])
     return(
-        <div className="">
+        <div id="" className="">
         {
             (nftMetaData.image_preview_url || nftMetaData.image_url)?
             <div className="hover:cursor-pointer transition ease-in-out hover:scale-110" onClick={()=>setShowModal(true)}>
@@ -40,7 +60,7 @@ const NftDisplay:NextPage<Props> = (props) => {
         }
         
         {/* nft modal */}
-        <div id={`${nftMetaData.id}Modal`} tabIndex={-1} aria-hidden={showModal?"false":"true"} className={`${!showModal && "hidden"} modal fixed w-full h-full top-0 left-0 z-50 flex items-center justify-center overflow-y-auto`} style={{backgroundColor:`${isDark?"rgba(0, 0, 0, 0.9)":"rgba(0, 0, 0, 0.9)"}`}}>
+        <div id={modalId} tabIndex={-1} aria-hidden={showModal?"false":"true"} className={`${!showModal && "hidden"} modal fixed w-full h-full top-0 left-0 z-50 flex items-center justify-center overflow-y-auto`} style={{backgroundColor:`${isDark?"rgba(0, 0, 0, 0.9)":"rgba(0, 0, 0, 0.9)"}`}}>
             {/* top right fixed close button  */}
             <button type="button" className="invisible md:visible text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto fixed top-4 right-5 items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>setShowModal(false)}>
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  
@@ -69,7 +89,7 @@ const NftDisplay:NextPage<Props> = (props) => {
                 </div>
                     
                 {/* nft info card */}
-                <div className="flex-1 bg-white dark:bg-black md:ml-6 mt-8 md:mt-0 rounded-lg min-h-[30rem] md:min-h-[25rem] h-fit md:max-h-[40rem] dark:border dark:border-gray-100 md:overflow-x-hidden overflow-y-auto no-scrollbar">
+                <div id={nftCardId} className="flex-1 bg-white dark:bg-black md:ml-6 mt-8 md:mt-0 rounded-lg min-h-[30rem] md:min-h-[25rem] h-fit md:max-h-[40rem] dark:border dark:border-gray-100 md:overflow-x-hidden overflow-y-auto no-scrollbar">
                     <div className="mx-3 mt-3 dark:text-white">
                         <div>
                             {
