6e5b59d6c8b5b9c39cbd3a532c6769f83e77aaf2
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-29 13:10:31 -0400

    Add styled scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('tailwind-scrollbar'),
     plugin(function({ addUtilities }) {
     addUtilities({
       /* Hide scrollbar for Chrome, Safari and Opera */
