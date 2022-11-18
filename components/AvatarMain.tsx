cc35c73bba6839f44fd23660ffdec59ae0fba16b
Jett Hays <jettblu.fabrications@gmail.com>
2022-11-17 23:27:05 -0500

    Update avatar to cover full dimensions

--- components/AvatarMain.tsx
+++ components/AvatarMain.tsx
@@ -10,7 +10,7 @@ const AvatarMain:NextPage<Props> = (props) => {
                {
                    photoPath==""?
                    <div className="shadow rounded-full bg-sky-600 max-w-full h-auto align-middle border-none transition ease-in-out delay-100 transform hover:-translate-y-1"/>:
-                   <img src={photoPath} alt="Profile Image" className="shadow rounded-full max-w-full h-auto align-middle border-none transition ease-in-out delay-100 transform hover:-translate-y-1"/>
+                   <img src={photoPath} alt="Profile Image" className="object-cover rounded-full max-w-full h-auto align-middle border-none transition ease-in-out delay-100 transform hover:-translate-y-1"/>
                }
         </div>
     )   
