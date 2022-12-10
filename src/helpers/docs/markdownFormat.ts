d3051867e59384d78efc1d5ac46776c331f96961
Jett Hays <jettblu.fabrications@gmail.com>
2022-12-09 19:01:55 -0500

    Update markdown to html converter

--- src/helpers/docs/markdownFormat.ts
+++ src/helpers/docs/markdownFormat.ts
@@ -4,9 +4,9 @@ import remarkRehype from 'remark-rehype'
 import highlight from "rehype-highlight"
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
-
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
+import rehypeSlug from 'rehype-slug'
 
 
 
@@ -14,6 +14,7 @@ export default async function markdownToHtml(markdown: string) {
   const result = await unified().use(remarkParse)
   .use(remarkGfm)
   .use(remarkRehype)
+  .use(rehypeSlug)
   .use(remarkMath)
   .use(rehypeKatex)
   .use(rehypeStringify).use(highlight).process(markdown);
