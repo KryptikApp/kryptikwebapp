5b86ff4cd6f3211254baa9b351eb6448cb51fcca
jettblu <jettblu.fabrications@gmail.com>
2023-01-23 16:55:13 -0500

    Fix checked warning

--- pages/profile/settings.tsx
+++ pages/profile/settings.tsx
@@ -127,6 +127,7 @@ const Settings: NextPage = () => {
             <input
               type="checkbox"
               checked={isVisible ? true : false}
+              onChange={() => {}}
               id={isVisible ? "checked-toggle" : "default-toggle"}
               className="sr-only peer"
               onClick={() => handleUpdateWalletVisibility()}
@@ -162,6 +163,7 @@ const Settings: NextPage = () => {
                 name="inlineRadioOptions"
                 id="inlineRadioLight"
                 value="light"
+                onChange={() => {}}
                 checked={isDark ? false : true}
               />
               <label
@@ -184,6 +186,7 @@ const Settings: NextPage = () => {
                 name="inlineRadioOptions"
                 id="inlineRadioDark"
                 value="dark"
+                onChange={() => {}}
                 checked={isDark ? true : false}
               />
               <label
@@ -211,6 +214,7 @@ const Settings: NextPage = () => {
               checked={isAdvanced ? true : false}
               id={isAdvanced ? "checked-toggle" : "default-toggle"}
               className="sr-only peer"
+              onChange={() => {}}
               onClick={() => handleUpdateIsAdvanced(!isAdvanced)}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-3 peer-focus:ring-blue-300 dark:peer-focus:ring-sky-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-sky-500"></div>
