069f130ad3a2986d0e9ef6db5e1035e2ff1a80de
jettblu <jettblu.fabrications@gmail.com>
2023-01-23 16:53:48 -0500

    Update modal style

--- components/modals/modal.tsx
+++ components/modals/modal.tsx
@@ -1,6 +1,4 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import { useKryptikThemeContext } from "../ThemeProvider";
+import { useEffect } from "react";
 
 type Props = {
   children: any;
@@ -12,8 +10,9 @@ type Props = {
 // TODO: Update to support dynamic headers
 export default function Modal(props: Props) {
   const { children, isOpen, onRequestClose, dark } = { ...props };
-  const modalId = `${Math.random()}Modal`;
-  const childrenId = `${Math.random()}Children`;
+  const randomId: string = Math.random().toString();
+  const modalId = `${randomId}Modal`;
+  const childrenId = `${randomId}Children`;
 
   useEffect(() => {
     if (!document) return;
@@ -65,27 +64,34 @@ export default function Modal(props: Props) {
             ></path>
           </svg>
         </button>
-        {/* close button shown on small screens */}
-        <button
-          type="button"
-          className="md:hidden mb-2 mt-4 text-black bg-white rounded-full font-bold text-sm p-1.5 ml-auto items-center dark:bg-white dark:text-black transition ease-in-out hover:scale-110"
-          onClick={() => onRequestClose()}
-        >
-          <svg
-            className="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="flex flex-col">
+          {/* close button shown on small screens */}
+          <button
+            type="button"
+            className="md:hidden mb-2 mt-28 mx-4 text-black bg-white rounded-full font-bold text-sm p-1.5 ml-auto items-center dark:bg-white dark:text-black transition ease-in-out hover:scale-110"
+            onClick={() => onRequestClose()}
           >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        {/* show wrapped content */}
-        <div id={childrenId}>{children}</div>
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          {/* show wrapped content */}
+          <div
+            className="max-h-screen m-4 md:min-w-[60%] max-w-[90%] md:max-w-[900px]"
+            id={childrenId}
+          >
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   );
