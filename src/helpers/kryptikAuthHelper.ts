2891507f83c42bcf10944fd4af9301789ca97e3f
jettblu <jettblu.fabrications@gmail.com>
2023-01-29 21:12:40 -0500

    Update auth context handlers

--- src/helpers/kryptikAuthHelper.ts
+++ src/helpers/kryptikAuthHelper.ts
@@ -1,33 +1,20 @@
 // helps with integrating web3service into app. context
-import {
-  signInWithCustomToken,
-  updateProfile,
-  User,
-  UserCredential,
-} from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
-import { useCallback, useEffect, useState } from "react";
-import {
-  firebaseAuth,
-  firestore,
-  formatAuthUser,
-  readExtraUserData,
-  getUserPhotoPath,
-} from "./firebaseHelper";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { getUserPhotoPath } from "./firebaseHelper";
 import SignClient from "@walletconnect/sign-client";
 import { SignClientTypes } from "@walletconnect/types/dist/types/sign-client";
 
-import { updateVaultSeedloop } from "../handlers/wallet/vaultHandler";
 import { defaultWallet } from "../models/defaultWallet";
-import { defaultUser, UserDB, UserExtraData } from "../models/user";
-import Web3Service, { IConnectWalletReturn } from "../services/Web3Service";
-import { networkFromNetworkDb } from "./utils/networkUtils";
+import { UserDB } from "../models/user";
+import Web3Service from "../services/Web3Service";
 import { IWallet, WalletStatus } from "../models/KryptikWallet";
-import { connectKryptikWallet } from "./wallet";
+import { ConnectWalletLocalandRemote } from "./wallet";
 import { NetworkDb } from "../services/models/network";
 import { createSignClient } from "../handlers/connect/walletConnect";
 import ModalStore from "../handlers/store/ModalStore";
 import { signingMethods } from "../handlers/connect/types";
+import { getActiveUser, updateProfile } from "./user";
+import { handleApprove, logout } from "./auth";
 
 export function useKryptikAuth() {
   //create service
@@ -36,20 +23,16 @@ export function useKryptikAuth() {
   const [kryptikService, setKryptikService] = useState(initServiceState);
   const [kryptikWallet, setKryptikWallet] = useState(defaultWallet);
   const [walletStatus, setWalletStatus] = useState(defaultWallet.status);
-  const [authUser, setAuthUser] = useState<UserDB | null>(defaultUser);
+  const [authUser, setAuthUser] = useState<UserDB | null>(null);
   const [loadingAuthUser, setLoadingAuthUser] = useState<boolean>(false);
   const [loadingWallet, setLoadingWallet] = useState<boolean>(false);
   const [signClient, setSignClient] = useState<SignClient | null>(null);
   const [walletConnectInitialized, setWalletConnectInitialized] =
     useState(false);
-  // wallet state change event handler
-  const walletStateChanged = function (wallet: IWallet) {
-    console.log("Wallet event handler fired!");
-    setKryptikWallet(wallet);
-  };
+  const authWorker = useRef<Worker>();
 
   // routine to run when auth. state changes
-  const authStateChanged = async (user: any, seed?: string) => {
+  async function authStateChanged(user: UserDB) {
     // TODO: ADD CONDITION FOR INTERNET CONNECTION
     // if(window && window.)
     if (!user) {
@@ -66,66 +49,14 @@ export function useKryptikAuth() {
       console.warn(e);
       console.warn("Error: unable to update kryptik auth context");
     }
-  };
-
-  // connects wallet with local service and updates remote share on server if necessary
-  const ConnectWalletLocalandRemote = async function (
-    ks: Web3Service,
-    user: UserDB,
-    seed?: string
-  ): Promise<IWallet> {
-    let UserExtraData: UserExtraData = await readExtraUserData(user);
-    console.log("running kryptik connect method...");
-    let networksToAdd: NetworkDb[] = ks.getSupportedNetworkDbs();
-    let kryptikConnectionObject: IConnectWalletReturn =
-      await connectKryptikWallet(
-        user.uid,
-        networksToAdd,
-        UserExtraData.remoteShare,
-        seed
-      );
-    console.log("finished kryptik connect!");
-    // update remote share on db if value generated on local computer is different
-    if (
-      !UserExtraData.remoteShare ||
-      kryptikConnectionObject.remoteShare != UserExtraData.remoteShare
-    ) {
-      console.log("UPDATING REMOTE SHARE ON DB");
-      // update extra user data to reflect updated remote share
-      let updatedUserExtraData: UserExtraData = {
-        remoteShare: kryptikConnectionObject.remoteShare,
-        isTwoFactorAuth: UserExtraData.isTwoFactorAuth,
-        bio: UserExtraData.bio,
-      };
-      try {
-        // write updated extra user data to DB
-        await writeExtraUserData(user, updatedUserExtraData);
-      } catch (err) {
-        throw new Error(
-          "Error writing extra user data to database. Check firestore connection."
-        );
-      }
-    }
-    return kryptikConnectionObject.wallet;
-  };
+  }
 
   // update standard firestore user's profile
-  const updateCurrentUserKryptik = async (user: UserDB) => {
-    let userFirebase: User | null = firebaseAuth.currentUser;
-    if (userFirebase == null) throw new Error("No user is signed in.");
-    // get current extra user data
-    let extraUserData: UserExtraData = await readExtraUserData(user);
-    let extraUserDataUpdated: UserExtraData = {
-      remoteShare: extraUserData.remoteShare,
-      bio: user.bio,
-      isTwoFactorAuth: extraUserData.isTwoFactorAuth,
-    };
-    await writeExtraUserData(user, extraUserDataUpdated);
-    await updateProfile(userFirebase, {
-      displayName: user.name,
-      photoURL: user.photoUrl,
-    });
-  };
+  async function updateCurrentUserKryptik(user: UserDB) {
+    try {
+      await updateProfile(user);
+    } catch (e) {}
+  }
 
   const updateWalletStatus = function (newWalletStatus: WalletStatus) {
     kryptikWallet.status = newWalletStatus;
@@ -133,37 +64,92 @@ export function useKryptikAuth() {
   };
 
   // sign in with external auth token
-  const signInWithToken = async (
-    customToken: string,
-    seed?: string,
-    isRefresh?: boolean
-  ) => {
-    // this value is used to prevent auth state from being changed from updating auth context...
-    // when signing in with custom seed
-    sessionStorage.setItem("isSigniningInWithToken", "true");
-    let userCred: UserCredential = await signInWithCustomToken(
-      firebaseAuth,
-      customToken
-    );
-    // remove temporary session flag
-    sessionStorage.removeItem("isSigniningInWithToken");
-    if (isRefresh) return;
+  // TODO: UPDATE TO REMOVE DEPENDANCY ON EMAIL
+  async function signInWithToken(
+    token: string,
+    email: string,
+    seed?: string
+  ): Promise<boolean> {
+    const approved = await handleApprove(email, token);
+    if (!approved) return false;
+    const user: UserDB | null = await getActiveUser();
+    if (!user) return false;
     // now we are manually updating the context and connecting the wallet
     // only done when not a refresh of authentication
-    updateAuthContext(userCred.user, seed, "sign in with token function call");
-    // note: auth state changed will run after the line above
+    await updateAuthContext(user, seed);
+    return true;
+  }
+
+  async function refreshUserAndWallet() {
+    const user: UserDB | null = await getActiveUser();
+    console.log("RUNNING REFRESH USER AND WALLET");
+    if (!user) {
+      console.log("No user available. Running clear...");
+      clear();
+      return;
+    }
+    console.log("UPDATING AUTH CONTEXT");
+    await updateAuthContext(user);
+    console.log("finished running refresh...");
+  }
+
+  const updateAuthContext = async (user: UserDB, seed?: string) => {
+    let isSigningInWithToken = sessionStorage.getItem("isSigniningInWithToken");
+    if (isSigningInWithToken) {
+      return;
+    }
+    // update loading state
+    setLoadingAuthUser(true);
+    setLoadingWallet(true);
+    let formattedUser: UserDB = user;
+    // start web3 kryptik service
+    let ks = await kryptikService.StartSevice();
+    setKryptikService(ks);
+    let newWalletKryptik: IWallet;
+    console.log("connecting kryptik wallet local and remote");
+    // get networks to add to seedloop
+    const networksToAdd: NetworkDb[] = ks.getAllNetworkDbs(true);
+    const uid: string = formattedUser.uid;
+    // connect with provided seed
+    if (seed != "") {
+      const connectionObject = await ConnectWalletLocalandRemote({
+        uid: uid,
+        seed: seed,
+        networksToAdd: networksToAdd,
+      });
+      newWalletKryptik = connectionObject.wallet;
+    }
+    // otherwise new seed will be generated
+    else {
+      const connectionObject = await ConnectWalletLocalandRemote({
+        uid: uid,
+        networksToAdd: networksToAdd,
+      });
+      newWalletKryptik = connectionObject.wallet;
+    }
+    console.log("finished connecting");
+    // set data
+    setKryptikWallet(newWalletKryptik);
+    setWalletStatus(newWalletKryptik.status);
+    setAuthUser(formattedUser);
+    setLoadingAuthUser(false);
+    setLoadingWallet(false);
   };
 
-  /******************************************************************************
-   * WC: Open session proposal modal for confirmation / rejection
-   *****************************************************************************/
-  const onSessionProposal = useCallback(
-    (proposal: SignClientTypes.EventArguments["session_proposal"]) => {
-      console.log("PROPOSAL REQUESTED");
-      ModalStore.open("SessionProposalModal", { proposal });
-    },
-    []
-  );
+  function signOut() {
+    logout().then(() => {
+      clear();
+    });
+  }
+
+  // clear current kryptik web 3 service state
+  const clear = () => {
+    setAuthUser(null);
+    setKryptikWallet(defaultWallet);
+    setKryptikService(new Web3Service());
+    setLoadingAuthUser(false);
+    setLoadingWallet(false);
+  };
 
   /******************************************************************************
    * WC: Open request handling modal based on method that was used
@@ -227,6 +213,16 @@ export function useKryptikAuth() {
     },
     []
   );
+  /******************************************************************************
+   * WC: Open session proposal modal for confirmation / rejection
+   *****************************************************************************/
+  const onSessionProposal = useCallback(
+    (proposal: SignClientTypes.EventArguments["session_proposal"]) => {
+      console.log("PROPOSAL REQUESTED");
+      ModalStore.open("SessionProposalModal", { proposal });
+    },
+    []
+  );
 
   async function initializeSignClient() {
     const newSignClient: SignClient = await createSignClient();
@@ -252,117 +248,23 @@ export function useKryptikAuth() {
     setSignClient(newSignClient);
   }
 
-  /******************************************************************************
-   * Back to non wallet connect methods...
-   *****************************************************************************/
-
-  const updateAuthContext = async (user: User, seed?: string, id?: string) => {
-    let isSigningInWithToken = sessionStorage.getItem("isSigniningInWithToken");
-    if (isSigningInWithToken) {
-      return;
-    }
-    setLoadingAuthUser(true);
-    setLoadingWallet(true);
-    let formattedUser: UserDB = formatAuthUser(user);
-    // read extra user data from db
-    let userExtraData = await readExtraUserData(formattedUser);
-    formattedUser.bio = userExtraData.bio;
-    // start web3 kryptik service
-    let ks = await kryptikService.StartSevice();
-    setKryptikService(ks);
-    let newWalletKryptik: IWallet;
-    console.log("connecting kryptik wallet local and remote");
-    if (seed != "") {
-      newWalletKryptik = await ConnectWalletLocalandRemote(
-        ks,
-        formattedUser,
-        seed
-      );
-    } else {
-      newWalletKryptik = await ConnectWalletLocalandRemote(ks, formattedUser);
-    }
-    console.log("finished connecting");
-    try {
-      console.log("Updating wallet networks:");
-      newWalletKryptik = updateWalletNetworks(
-        newWalletKryptik,
-        kryptikService,
-        formattedUser,
-        userExtraData
-      );
-      // UPDATE: TO DO if wallet is set as visible update remote addresses
-      console.log("wallet up to date");
-    } catch (e) {
-      throw new Error("Error: unable to synchronize remote networks.");
-    }
-    // set data
-    setKryptikWallet(newWalletKryptik);
-    setWalletStatus(newWalletKryptik.status);
-    setAuthUser(formattedUser);
-    setLoadingAuthUser(false);
-    setLoadingWallet(false);
-  };
-
-  // update wallets on local seedloop to match networks supported by app.
-  const updateWalletNetworks = function (
-    wallet: IWallet,
-    ks: Web3Service,
-    user: UserDB,
-    extraUserData: UserExtraData
-  ): IWallet {
-    // flag for if networks are added to seedloop
-    let isUpdated: boolean = false;
-    let supprtedDbs = ks.getSupportedNetworkDbs();
-    for (const networkDb of supprtedDbs) {
-      let network = networkFromNetworkDb(networkDb);
-      if (!wallet.seedLoop.networkOnSeedloop(network)) {
-        isUpdated = true;
-        wallet.seedLoop.addKeyRingByNetwork(network);
-      }
-    }
-    // save updated seedloop in local vault
-    if (isUpdated) {
-      try {
-        console.log(`Updating kryptik vault with id: ${user.uid}`);
-        updateVaultSeedloop(user.uid, extraUserData.remoteShare, wallet);
-        console.log("Vault updated");
-      } catch (e) {
-        console.warn(
-          "Error: Unable to update vault with network synchronized seedloop"
-        );
-        // throw(new Error("Error: Unable to update vault with network synchronized seedloop"));
-      }
-    }
-    return wallet;
-  };
-
-  const writeExtraUserData = async function (
-    user: UserDB,
-    data: UserExtraData
-  ) {
-    await setDoc(doc(firestore, "users", user.uid), data);
-  };
-
-  const signOut = () => firebaseAuth.signOut().then(clear);
-
   useEffect(() => {
-    const unsubscribe = firebaseAuth.onAuthStateChanged(authStateChanged);
-    return () => unsubscribe();
-  }, []);
+    // add auth web worker
+    authWorker.current = new Worker(new URL("/authWorker.ts", import.meta.url));
 
-  // clear current kryptik web 3 service state
-  const clear = () => {
-    setAuthUser(null);
-    setKryptikWallet(defaultWallet);
-    setKryptikService(new Web3Service());
-    setLoadingAuthUser(false);
-    setLoadingWallet(false);
-  };
+    // should fire once upon refresh
+    authWorker.current.onmessage = (event: MessageEvent<boolean>) => {
+      if (event.data) {
+        refreshUserAndWallet();
+      }
+    };
+  }, []);
 
   return {
     authUser,
     loadingAuthUser,
     loadingWallet,
+    refreshUserAndWallet,
     signInWithToken,
     updateCurrentUserKryptik,
     getUserPhotoPath,
