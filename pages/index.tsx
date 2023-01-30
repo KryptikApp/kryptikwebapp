71c4d4dfcbf542d2c3da74c885486f2c51f6893f
jettblu <jettblu.fabrications@gmail.com>
2023-01-29 21:19:35 -0500

    Delete unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,7 @@ import LaunchPage from "../components/LaunchPage";
 import LandingPage from "../components/LandingPage";
 
 const Home: NextPage = () => {
-  const { kryptikWallet, authUser } = useKryptikAuthContext();
   const appMode = process.env.NEXT_PUBLIC_APP_MODE;
-
-  const handleGetStarted = async () => {
-    console.log("Handle get started!");
-    toast("Handle get started");
-  };
 
   return <div>{appMode == "prelaunch" ? <LaunchPage /> : <LandingPage />}</div>;
 };
