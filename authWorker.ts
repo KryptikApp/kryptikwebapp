f6aa6204091489c454cb00530ea691a6a51399bf
jettblu <jettblu.fabrications@gmail.com>
2023-01-29 21:23:49 -0500

    Add authentication background worker

---
+++ authWorker.ts
@@ -0,0 +1,31 @@
+import { getActiveUser } from "./src/helpers/user";
+import { KryptikFetch } from "./src/kryptikFetch";
+
+async function handleRefreshTokens() {
+  console.log("refreshing auth tokens......");
+  try {
+    const res = await KryptikFetch("/api/auth/refresh", {
+      method: "POST",
+      timeout: 8000,
+      headers: { "Content-Type": "application/json" },
+    });
+    if (res.status != 200) {
+      throw new Error("Bad request");
+    }
+  } catch (e) {
+    // console.warn("Unable to refresh auth token. May need to log in again.");
+  }
+}
+handleRefreshTokens().then(() => {
+  postMessage(true);
+});
+// silent auth refresh
+// runs every twelve minutes
+setInterval(() => {
+  handleRefreshTokens().then(() => {
+    getActiveUser().then((u) => {
+      console.log("User after auth refresh:");
+      console.log(u);
+    });
+  });
+}, 720000);
