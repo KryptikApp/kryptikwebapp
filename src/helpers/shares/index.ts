b0c9ba30177c5cf80f5af83a8032a8b7460e4c2e
jettblu <jettblu.fabrications@gmail.com>
2023-02-01 00:12:59 -0500

    Add seperate share helper file

--- src/helpers/shares/index.ts
+++ src/helpers/shares/index.ts
@@ -1,13 +1,12 @@
 import { KryptikFetch } from "../../kryptikFetch";
-import { UserDB } from "../../models/user";
 
-export async function updateProfile(user: UserDB) {
+export async function createShareOnDb(share: string): Promise<boolean> {
   const params = {
-    user: user,
+    share: share,
   };
   // try to create share on db
   try {
-    const res = await KryptikFetch("/api/user/updateProfile", {
+    const res = await KryptikFetch("/api/shares/create", {
       method: "POST",
       body: JSON.stringify(params),
       timeout: 8000,
@@ -23,20 +22,19 @@ export async function updateProfile(user: UserDB) {
   }
 }
 
-export async function getActiveUser(): Promise<UserDB | null> {
-  // try to create share on db
+export async function getRemoteShare(): Promise<string | null> {
+  // try to fetch share from server
   try {
-    const res = await KryptikFetch("/api/user/activeUser", {
+    const res = await KryptikFetch("/api/shares/retrieve", {
       method: "POST",
       timeout: 8000,
       headers: { "Content-Type": "application/json" },
     });
-    const user: UserDB | undefined = res.data.user;
-    if (res.status != 200 || !user) {
-      return null;
+    const share = res.data.share;
+    if (res.status != 200 || !share) {
+      throw "Unable to get share.";
     }
-    console.log(user);
-    return user;
+    return share;
   } catch (e) {
     return null;
   }
