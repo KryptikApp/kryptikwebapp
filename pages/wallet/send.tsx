30fcd3702a11950e547d20315a4b63183e3751e7
Alex Rasskin <xrasskin1@gmail.com>
2023-02-04 20:52:23 +0000

    update interface for sending transactions

--- pages/wallet/send.tsx
+++ pages/wallet/send.tsx
@@ -101,7 +101,7 @@ const Send: NextPage = () => {
     "Unable to complete transaction"
   );
   const [isLoading, setisLoading] = useState(false);
-  const [progress, setProgress] = useState<TxProgress>(TxProgress.Begin);
+  const [progress, setProgress] = useState<TxProgress>(TxProgress.SetParamaters);
   const [selectedTokenAndNetwork, setSelectedTokenAndNetwork] = useState(
     defaultTokenAndNetwork
   );
@@ -531,13 +531,7 @@ const Send: NextPage = () => {
       <div className="text-center max-w-xl mx-auto content-center">
         {progress == TxProgress.SetParamaters && (
           <div>
-            <div>
-              <AiOutlineQrcode
-                className="hover:cursor-pointer dark:text-white inline"
-                onClick={() => handleQrScanner()}
-                size="30"
-              />
-            </div>
+            
             <div>
               <Modal isOpen={showModal} onRequestClose={closeModalWrapper} dark={isDark}>
                 <KryptikScanner show={showScanner} onScan={handleOnScan} />
@@ -643,10 +637,12 @@ const Send: NextPage = () => {
         )}
         {progress == TxProgress.SetParamaters && (
           <div>
+            {/* Top Text */}
+            <h1 className="text-4xl fond-bold text-white mt-0 mb-6">You are about to send</h1>
             {/* amount indicator */}
-            <div className="border rounded border-solid border-grey-600 w-40 mx-7 py-3">
+            <div className="py-3 items-center">
               <img
-                className="w-8 h-8 rounded-full inline"
+                className="w-12 h-12 rounded-full inline align-middle"
                 src={
                   selectedTokenAndNetwork.tokenData
                     ? selectedTokenAndNetwork.tokenData.tokenDb.logoURI
@@ -661,48 +657,62 @@ const Send: NextPage = () => {
                   alt={`${selectedTokenAndNetwork.baseNetworkDb.fullName} secondary image`}
                 />
               )}
-              <span className="inline mx-2 dark:text-white">
+              <span className="inline mx-2 align-middle dark:text-white text-8xl">
                 ${roundUsdAmount(Number(amountUSD))}
               </span>
             </div>
             <div className="px-5 py-5 m-2 rounded mt-0 mb-0">
               {/* to label */}
-              <div className="text-left">
-                <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4 inline">
-                  To
-                </label>
-                {isResolverLoading && (
-                  <svg
-                    role="status"
-                    className="inline w-4 h-4 ml-3 mb-1 text-white animate-spin"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="#E5E7EB"
+                <div className="text-left">
+                  <label className="block text-gray-500 font-bold mb-1 md:mb-0 pr-4 inline">
+                    To
+                  </label>
+                  {isResolverLoading && (
+                    <svg
+                      role="status"
+                      className="inline w-4 h-4 ml-3 mb-1 text-white animate-spin"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="#E5E7EB"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  )}
+                </div>
+                {/* to input */}
+              <div className="relative items-center pb-8">
+                  <button
+                      onClick={() => handleQrScanner()}
+                      className={`absolute bg-transparent text-gray-500 text-l  
+                                  top-3 right-0 inline-flex items-center`}
+                      >
+                    Scan in address
+                    <AiOutlineQrcode
+                      className="hover:cursor-pointer dark:text-white h-13 w-13 fill-current mr-2 ml-2 right-4 top-0"
+                      onClick={() => handleQrScanner()}
+                      size="30" 
                     />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                )}
+                  </button>
+                  <input
+                    className="text-l bg-white appearance-none border-2 border-gray-400 rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:dark:bg-[#141414] focus:border-blue-400 dark:bg-[#141414] dark:text-white"
+                    value={toAddress}
+                    onChange={(e) => handleToAddressChange(e.target.value)}
+                    id="inline-to"
+                  />
               </div>
-              {/* to input */}
-              <input
-                className="bg-white appearance-none border-2 border-gray-400 rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:dark:bg-[#141414] focus:border-blue-400 dark:bg-[#141414] dark:text-white"
-                value={toAddress}
-                onChange={(e) => handleToAddressChange(e.target.value)}
-                id="inline-to"
-              />
               {/* next button... to review */} */
               <button
                 onClick={() => handleStartReview()}
-                className={`bg-transparent hover:bg-sky-400 text-sky-500 font-semibold hover:text-white text-2xl py-2 px-20 ${
+                className={`bg-transparent hover:bg-sky-400 text-sky-500 font-semibold hover:text-white w-full text-2xl py-2 px-20 ${
                   isLoading ? "hover:cursor-not-allowed" : ""
-                } border border-sky-400 hover:border-transparent rounded-lg my-5`}
+                } border border-sky-400 hover:border-transparent rounded`}
                 disabled={isLoading}
               >
                 Review
