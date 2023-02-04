014e64366cef1dcc81da43d381545ff57f3f7e64
jettblu <jettblu.fabrications@gmail.com>
2023-02-04 15:34:38 -0500

    Add expandable option to button

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -8,11 +8,12 @@ type Props = {
   clickHandler: () => any;
   color?: ColorEnum;
   isDisabled?: boolean;
+  expand?: boolean;
   text: string;
 };
 
 export default function Button(props: Props) {
-  const { color, clickHandler, isDisabled, text } = { ...props };
+  const { color, clickHandler, isDisabled, text, expand } = { ...props };
   console.log(color);
   const colorString =
     color != undefined
@@ -21,7 +22,9 @@ export default function Button(props: Props) {
   return (
     <button
       onClick={() => clickHandler()}
-      className={`bg-transparent my-2 hover:bg-${colorString} text-${colorString} text-xl font-semibold hover:text-white py-2 px-8 border border-${colorString} hover:border-transparent rounded`}
+      className={`bg-transparent my-2 ${
+        expand && "w-full"
+      } hover:bg-${colorString} text-${colorString} text-xl font-semibold hover:text-white py-2 px-8 border border-${colorString} hover:border-transparent rounded`}
       disabled={isDisabled}
     >
       {text}
