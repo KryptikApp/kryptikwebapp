2a75a6a80a2f894fd12ec69c1470794e83a77cb0
jettblu <jettblu.fabrications@gmail.com>
2023-02-21 15:30:29 -0500

    Ass sync button

---
+++ components/buttons/ButtonSync.tsx
@@ -0,0 +1,28 @@
+import {
+  ColorEnum,
+  createColorString,
+  defaultColor,
+} from "../../src/helpers/utils";
+import Button from "./Button";
+
+type Props = {
+  clickHandler: () => any;
+  color?: ColorEnum;
+  isDisabled?: boolean;
+  isLoading?: boolean;
+  text: string;
+};
+
+export default function ButtonSync(props: Props) {
+  const { color, clickHandler, isDisabled, text, isLoading } = { ...props };
+  return (
+    <Button
+      clickHandler={clickHandler}
+      text={text}
+      color={color}
+      isDisabled={isDisabled}
+      isLoading={isLoading}
+      expand={true}
+    />
+  );
+}
