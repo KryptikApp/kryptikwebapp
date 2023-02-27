4763372a03fb163c04a57627fb41abaa12910ad8
jettblu <jettblu.fabrications@gmail.com>
2023-02-26 20:24:46 -0500

    Update scanner

--- components/kryptikScanner.tsx
+++ components/kryptikScanner.tsx
@@ -1,5 +1,5 @@
-import { QrReader as ReactQrReader } from "react-qr-reader";
-import { ViewFinder } from "./viewfinder";
+import QrScanner from "qr-scanner";
+import { useEffect, useRef, useState } from "react";
 
 /**
  * Types
@@ -14,25 +14,56 @@ interface IProps {
  */
 export default function KryptikScanner(iprops: IProps) {
   const { show, onScan } = { ...iprops };
+  const [qrScanner, setQrScanner] = useState<QrScanner | null>();
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const handleScan = async function (data: any) {
     if (data && data.text && typeof data.text == "string") {
-      await onScan(data.text);
+      onScan(data.text);
     }
   };
 
+  useEffect(() => {
+    console.log("HERE!");
+    if (!videoRef.current) {
+      console.log("No video ref available.");
+      return;
+    }
+    const newQrScanner = new QrScanner(
+      videoRef.current,
+      (result) => {
+        console.log("RESULT");
+        console.log(result);
+      },
+      {
+        preferredCamera: "environment",
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+        onDecodeError: () => {
+          //pass
+        },
+      }
+    );
+    newQrScanner.setInversionMode("both");
+    setQrScanner(newQrScanner);
+  }, []);
+
+  useEffect(() => {
+    if (!qrScanner) return;
+    if (show) {
+      qrScanner.start();
+    } else {
+      qrScanner.stop();
+    }
+  }, [show]);
+
   return (
-    <div>
-      {show ? (
-        <div className="w-full max-w-[80vh]">
-          <ReactQrReader
-            constraints={{ facingMode: "environment" }}
-            className={""}
-            onResult={(data: any) => handleScan(data)}
-          />
-        </div>
-      ) : (
-        <div></div>
-      )}
+    <div className="">
+      <video
+        ref={videoRef}
+        className="rounded-xl object-cover w-full max-w-sm h-96 mb-8"
+        muted
+        id="scanner"
+      />
     </div>
   );
 }
