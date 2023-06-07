8184066cca96ae3faa0f23bf5d32f3df723fd052
jettblu <jettblu.fabrications@gmail.com>
2023-06-07 00:59:24 -0700

    Update favicon path

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
       <Head>
         <title>Kryptik Wallet</title>
         <meta name="description" content="Crypto made simple." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.ico" />
       </Head>
       <Toaster />
 
