83ad4eabebdf77095ad81fe82e99c89dd873f78e
jettblu <jettblu.fabrications@gmail.com>
2023-06-09 17:49:44 -0700

    Add access token handler to passkey auth

--- pages/api/auth/passkey/verifyAuth.ts
+++ pages/api/auth/passkey/verifyAuth.ts
@@ -4,6 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { verifyAuthenticationResponse } from "@simplewebauthn/server";
 import {
+  addRefreshTokenToWhitelist,
   findAuthenticatorById,
   findCurrentChallenge,
   findUserByEmail,
@@ -11,6 +12,9 @@ import {
 } from "../../../../prisma/script";
 import { User } from "@prisma/client";
 import { rpID } from "../../../../src/constants/passkeyConstants";
+import { setCookie } from "cookies-next";
+import { v4 } from "uuid";
+import { generateTokens } from "../../../../src/helpers/auth/jwt";
 
 // The URL at which registrations and authentications should occur
 const origin = `https://${rpID}`;
@@ -63,6 +67,23 @@ export default async function handler(
     if (!verified) {
       throw new Error("Unable to verify registration response.");
     }
+    // add auth cookies
+    const jti = v4();
+    const { accessToken, refreshToken } = generateTokens(user, jti);
+    await addRefreshTokenToWhitelist(jti, refreshToken, user.id);
+    setCookie("accessToken", accessToken, {
+      req,
+      res,
+      secure: process.env.APP_STAGE == "production",
+      httpOnly: true,
+    });
+    setCookie("refreshToken", refreshToken, {
+      req,
+      res,
+      secure: process.env.APP_STAGE == "production",
+      httpOnly: true,
+    });
+    // return response
     return res.status(200).send({
       msg: "Successfully verified registration response.",
       verified: verified,
