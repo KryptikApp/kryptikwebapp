0fd987c2544ca35c5d4c14a4725a5eb0029d7f92
jettblu <jettblu.fabrications@gmail.com>
2023-06-16 17:30:47 -0700

    Add top apps to explore page

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,21 +1,32 @@
 import type { NextPage } from "next";
-import { Toaster } from "react-hot-toast";
 
 import SearchNetwork from "../components/search/searchNetwork";
+import ListApps from "../components/lists/ListApps";
+import { topApps } from "../src/explore/apps/topApps";
 
 const Explore: NextPage = () => {
   return (
-    <div>
-      <div className="h-[24vh]">
+    <div className="">
+      <div className="h-[4vh]">
         {/* padding div for space between top and main elements */}
+      </div>
+
+      <div className="max-w-3xl mx-auto text-left border-b border-green-400/30 mb-8 py-2 p-4">
+        <h1 className="text-left text-3xl font-semibold">Explore</h1>
+        <p className="text-xl text-gray-600 dark:text-gray-300">
+          See what's happening in Web3.
+        </p>
       </div>
 
       <div className="text-center max-w-2xl mx-auto content-center">
         <SearchNetwork />
       </div>
-
-      <div className="h-[24vh]">
-        {/* padding div for space between top and main elements */}
+      <div className="max-w-3xl mx-auto px-4 bg-gray-100/10 dark:bg-gray-700/10 my-8 rounded-xl hover:outline outline-w-1 dark:hover:outline-gray-800/40 hover:outline-gray-400/40">
+        <div className="max-w-3xl text-2xl">Top Apps</div>
+        <ListApps apps={topApps} />
+      </div>
+      <div className="h-[4vh]">
+        {/* padding div for space between bottom and main elements */}
       </div>
     </div>
   );
