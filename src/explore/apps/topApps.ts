3800f5bde273d2c731df51e89cabbc8368ea8997
jettblu <jettblu.fabrications@gmail.com>
2023-06-16 17:28:23 -0700

    Add top apps list

---
+++ src/explore/apps/topApps.ts
@@ -0,0 +1,46 @@
+import { Web3App } from "./types";
+
+export const topApps: Web3App[] = [
+  {
+    name: "Uniswap",
+    description: "Trade tokens",
+    url: "https://app.uniswap.org/",
+    icon: "/apps/uniswap.webp",
+    tags: ["exchange"],
+  },
+  {
+    name: "PoolTogether",
+    description: "No-loss prize games",
+    url: "https://app.pooltogether.com/",
+    icon: "/apps/poolTogether.webp",
+    tags: ["games"],
+  },
+  {
+    name: "Sablier",
+    description: "Stream money in real-time",
+    url: "https://app.sablier.finance/",
+    icon: "/apps/sablier.webp",
+    tags: ["payments"],
+  },
+  {
+    name: "Ethereum Name Service",
+    description: "Decentralized naming for wallets, websites, & more",
+    url: "https://app.ens.domains/",
+    icon: "/apps/ens.webp",
+    tags: ["domains"],
+  },
+  {
+    name: "Aave",
+    description: "Lend and borrow with variable interest rates",
+    url: "https://aave.com/",
+    icon: "/apps/aave.webp",
+    tags: ["defi"],
+  },
+  {
+    name: "Zapper",
+    description: "Track and manage DeFi assets",
+    url: "https://zapper.fi/",
+    icon: "/apps/zapper.webp",
+    tags: ["dashboard"],
+  },
+];
