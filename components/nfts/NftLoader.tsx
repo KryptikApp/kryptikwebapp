3eac1d258bf621146365be0bfbf2de40b9443ce4
jettblu <jettblu.fabrications@gmail.com>
2023-07-31 15:35:59 -0700

    Add nft loader

---
+++ components/nfts/NftLoader.tsx
@@ -0,0 +1,15 @@
+export default function NftLoader() {
+  return (
+    <div className="">
+      <div className="w-56 h-56 rounded-md drop-shadow-lg animate-pulse border border-gray-200 dark:border-gray-800 bg-gray-700/80 dark:bg-gray-300/80" />
+
+      <div className="flex">
+        <div className="my-2 w-full h-6 animate-pulse bg-gray-700/80 dark:bg-gray-300/80 rounded-md" />
+
+        <div className="flex-grow mt-3">
+          <div className="h-4 w-4 animate-pulse bg-gray-900/80 bg-gray-100/80 rounded-lg ml-2 my-auto" />
+        </div>
+      </div>
+    </div>
+  );
+}
