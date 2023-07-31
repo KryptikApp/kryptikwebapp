a4be1d046658307459db72ec9bc3159a723177c8
jettblu <jettblu.fabrications@gmail.com>
2023-07-31 16:36:13 -0700

    Update features components

--- components/lists/ListFeatures.tsx
+++ components/lists/ListFeatures.tsx
@@ -66,112 +66,10 @@ const ListFeatures: NextComponentType = () => {
             id="featureContainer"
             className="relative flex flex-row min-h-[100px] py-4 space-x-2 overflow-x-auto snap-x px-2 no-scrollbar"
           >
-            <div
-              className={`${
-                loading && "invisible"
-              } flex flex-col max-w-xl h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-sky-400 rounded-xl snap-center px-2 py-4`}
-              style={{ minWidth: featureWidth }}
-            >
-              <h4 className="text-3xl font-semibold mb-2">Multichain Magic</h4>
-              <p className="text-xl text-gray-700 dark:text-gray-200">
-                {" "}
-                With Kryptik you can access a world of possibilities across
-                <span className="font-semibold"> 10+ blockchains</span> from a
-                single app.
-              </p>
-              <Image
-                src="/icons/chainLogos.gif"
-                alt="Wavy chain logos"
-                className="mx-auto object-contain snap-center"
-                width={300}
-                height={300}
-              />
-              <div className="absolute bottom-8 flex flex-row space-x-2 mt-2">
-                <div className="rounded-xl h-fit border px-2 py-1 bg-gray-900/10">
-                  <p>WalletConnect</p>
-                </div>
-                <div className="rounded-xl h-fit border px-2 py-1 bg-gray-900/10">
-                  <p>10+ Networks</p>
-                </div>
-              </div>
-            </div>
-
-            <div
-              className={`${
-                loading && "invisible"
-              } flex flex-col h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-green-400 rounded-xl snap-center px-2 py-4`}
-              style={{ minWidth: featureWidth }}
-            >
-              <h4 className="text-3xl font-semibold mb-2">Delightful Design</h4>
-              <p className="text-xl text-gray-700 dark:text-gray-200">
-                Kryptik is designed with lots of care and a dash of{" "}
-                <span className="text-sky-300">magic</span>. Every pixel is
-                engineered with unforgiving precision.
-              </p>
-              <Image
-                src="/kryptikBrand/kryptik balance screen.webp"
-                alt="Kryptik sample screen"
-                className="mx-auto object-contain snap-center"
-                width={135}
-                height={135}
-              />
-              <div className="absolute bottom-8 flex flex-row space-x-2 mt-2">
-                <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
-                  <p>Simple</p>
-                </div>
-                <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
-                  <p>Precise</p>
-                </div>
-              </div>
-            </div>
-
-            <div
-              className={`${
-                loading && "invisible"
-              } flex flex-col h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-sky-400 rounded-xl snap-center px-2 py-4`}
-              style={{ minWidth: featureWidth }}
-            >
-              <h4 className="text-3xl font-semibold mb-2">Lockdown Security</h4>
-              <p className="text-xl text-gray-700 dark:text-gray-200">
-                Kyptik uses threshold cryptography to secure your funds. Your
-                secrets never leave your device.
-              </p>
-              <Image
-                src="/icons/keyWave.gif"
-                alt="Wavy key"
-                className="mx-auto object-contain snap-center"
-                width={300}
-                height={300}
-              />
-              <div className="absolute bottom-8 flex flex-row space-x-2 mt-2">
-                <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
-                  <p>WebAuthn</p>
-                </div>
-                <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
-                  <p>Threshold Crypto</p>
-                </div>
-              </div>
-            </div>
-            <div
-              className={`${
-                loading && "invisible"
-              } flex flex-col h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-green-400 rounded-xl snap-center px-2 py-4`}
-              style={{ minWidth: featureWidth }}
-            >
-              <h4 className="text-3xl font-semibold mb-2">Open Source</h4>
-              <p className="text-xl text-gray-700 dark:text-gray-200">
-                All of Kryptik's code is public and open source. We believe in
-                100% transparency.
-              </p>
-              <a
-                className="text-green-400 font-semibold mt-32 text-2xl text-center rounded-xl px-6 py-1 bg-green-400/10 w-fit mx-auto hover:cursor-pointer hover:border border-green-400"
-                href={`https://github.com/KryptikApp/kryptikwebapp`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                View Code
-              </a>
-            </div>
+            <MultiChain featureWidth={featureWidth} loading={loading} />
+            <Design featureWidth={featureWidth} loading={loading} />
+            <Secure featureWidth={featureWidth} loading={loading} />
+            <OpenSource featureWidth={featureWidth} loading={loading} />
           </div>
         </div>
       </div>
@@ -180,3 +78,131 @@ const ListFeatures: NextComponentType = () => {
 };
 
 export default ListFeatures;
+
+function MultiChain(props: { featureWidth: number; loading: boolean }) {
+  const { featureWidth, loading } = { ...props };
+  return (
+    <div
+      className={`${
+        loading && "invisible"
+      } flex flex-col max-w-xl h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-sky-400 rounded-xl snap-center px-2 py-4`}
+      style={{ minWidth: featureWidth }}
+    >
+      <h4 className="text-3xl font-semibold mb-2">Multichain Magic</h4>
+      <p className="text-xl text-gray-700 dark:text-gray-200">
+        {" "}
+        With Kryptik you can access a world of possibilities across
+        <span className="font-semibold"> 10+ blockchains</span> from a single
+        app.
+      </p>
+      <Image
+        src="/icons/chainLogos.gif"
+        alt="Wavy chain logos"
+        className="mx-auto object-contain snap-center"
+        width={300}
+        height={300}
+      />
+      <div className="absolute bottom-8 flex flex-row space-x-2 mt-2">
+        <div className="rounded-xl h-fit border px-2 py-1 bg-gray-900/10">
+          <p>WalletConnect</p>
+        </div>
+        <div className="rounded-xl h-fit border px-2 py-1 bg-gray-900/10">
+          <p>10+ Networks</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function OpenSource(props: { featureWidth: number; loading: boolean }) {
+  const { featureWidth, loading } = { ...props };
+  return (
+    <div
+      className={`${
+        loading && "invisible"
+      } flex flex-col h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-green-400 rounded-xl snap-center px-2 py-4`}
+      style={{ minWidth: featureWidth }}
+    >
+      <h4 className="text-3xl font-semibold mb-2">Open Source</h4>
+      <p className="text-xl text-gray-700 dark:text-gray-200">
+        All of Kryptik's code is public and open source. We believe in 100%
+        transparency.
+      </p>
+      <a
+        className="text-green-400 font-semibold mt-32 text-2xl text-center rounded-xl px-6 py-1 bg-green-400/10 w-fit mx-auto hover:cursor-pointer hover:border border-green-400"
+        href={`https://github.com/KryptikApp/kryptikwebapp`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        View Code
+      </a>
+    </div>
+  );
+}
+
+function Secure(props: { featureWidth: number; loading: boolean }) {
+  const { featureWidth, loading } = { ...props };
+  return (
+    <div
+      className={`${
+        loading && "invisible"
+      } flex flex-col h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-sky-400 rounded-xl snap-center px-2 py-4`}
+      style={{ minWidth: featureWidth }}
+    >
+      <h4 className="text-3xl font-semibold mb-2">Lockdown Security</h4>
+      <p className="text-xl text-gray-700 dark:text-gray-200">
+        Kyptik uses threshold cryptography to secure your funds. Your secrets
+        never leave your device.
+      </p>
+      <Image
+        src="/icons/keyWave.gif"
+        alt="Wavy key"
+        className="mx-auto object-contain snap-center"
+        width={300}
+        height={300}
+      />
+      <div className="absolute bottom-8 flex flex-row space-x-2 mt-2">
+        <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
+          <p>WebAuthn</p>
+        </div>
+        <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
+          <p>Threshold Crypto</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Design(props: { featureWidth: number; loading: boolean }) {
+  const { featureWidth, loading } = { ...props };
+  return (
+    <div
+      className={`${
+        loading && "invisible"
+      } flex flex-col h-[500px] bg-gray-50/90 dark:bg-gray-900/40 border border-green-400 rounded-xl snap-center px-2 py-4`}
+      style={{ minWidth: featureWidth }}
+    >
+      <h4 className="text-3xl font-semibold mb-2">Delightful Design</h4>
+      <p className="text-xl text-gray-700 dark:text-gray-200">
+        Kryptik is designed with lots of care and a dash of{" "}
+        <span className="text-sky-300">magic</span>. Every pixel is engineered
+        with unforgiving precision.
+      </p>
+      <Image
+        src="/kryptikBrand/kryptik balance screen.webp"
+        alt="Kryptik sample screen"
+        className="mx-auto object-contain snap-center"
+        width={135}
+        height={135}
+      />
+      <div className="absolute bottom-8 flex flex-row space-x-2 mt-2">
+        <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
+          <p>Simple</p>
+        </div>
+        <div className="rounded-xl border px-2 py-1 bg-gray-900/10">
+          <p>Precise</p>
+        </div>
+      </div>
+    </div>
+  );
+}
