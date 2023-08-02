c02b003157c2668150f832b196a9afe3e68e18d9
jettblu <jettblu.fabrications@gmail.com>
2023-08-02 14:49:51 -0700

    Update toast style

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Navbar from "./navbars/Navbar";
 import { Toaster } from "react-hot-toast";
+import { useKryptikThemeContext } from "./ThemeProvider";
 
 // TODO: Update to support dynamic headers
 export default function Layout({ children }) {
+  const { isDark } = useKryptikThemeContext();
   return (
     <>
       <Head>
@@ -11,7 +13,18 @@ export default function Layout({ children }) {
         <meta name="description" content="Crypto made simple." />
         <link rel="icon" href="/icon.ico" />
       </Head>
-      <Toaster />
+      {/* almost black background if dark mode, almost white if light mode */}
+      <Toaster
+        toastOptions={{
+          style: {
+            backgroundColor: isDark
+              ? "rgba(25, 25, 25, 1)"
+              : "rgba(242, 242, 242, 1)",
+            color: isDark ? "#fff" : "#000",
+            border: "1px solid rgba(36, 165, 247, 0.53)",
+          },
+        }}
+      />
 
       <main
         className={`min-h-screen dark:bg-[#0c0c0c] bg-white
