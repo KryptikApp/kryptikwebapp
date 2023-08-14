7e0b3f930f7145311ab310467da172d3e683340e
jettblu <jettblu.fabrications@gmail.com>
2023-08-14 16:17:38 -0700

    Update action bar icons

--- components/wallet/ActionBar.tsx
+++ components/wallet/ActionBar.tsx
@@ -1,10 +1,10 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import {
-  AiFillDownCircle,
-  AiFillPayCircle,
-  AiFillPlusCircle,
-  AiFillUpCircle,
+  AiOutlineArrowDown,
+  AiOutlineArrowUp,
+  AiOutlinePlus,
+  AiOutlineSwap,
 } from "react-icons/ai";
 import { useKryptikAuthContext } from "../KryptikAuthProvider";
 import { useEffect, useState } from "react";
@@ -86,7 +86,7 @@ const ActionBar: NextPage<Props> = (props) => {
       } border-gray-500 dark:border-gray-300 rounded-xl `}
     >
       <div
-        className={`flex-1 ${
+        className={`group flex-1 ${
           !asVertical
             ? "border-r rounded-tl-xl rounded-bl-xl"
             : "w-full rounded-tr-xl rounded-tl-xl px-2"
@@ -96,20 +96,20 @@ const ActionBar: NextPage<Props> = (props) => {
         onClick={() => actionHandler(ActionEnum.Swap)}
       >
         <div
-          className={`${asVertical && "flex flex-row space-x-1 w-fit"}   ${
+          className={`${asVertical && "flex flex-row space-x-1 w-fit py-2"}   ${
             active
-              ? "hover:font-semibold hover:animate-pulse hover:cursor-pointer hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
+              ? "group-hover:font-semibold group-hover:animate-pulse group-hover:cursor-pointer group-hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
               : "text-slate-500"
           } `}
         >
-          <AiFillPayCircle className="mx-auto" size="30" />
+          <AiOutlineSwap className="mx-auto" size="30" />
           <span className="text-gray-700 dark:text-gray-200 font-semibold">
             Swap
           </span>
         </div>
       </div>
       <div
-        className={`flex-1 ${
+        className={`group flex-1 ${
           !asVertical ? "border-r" : "w-full px-2"
         } border-gray-500 dark:border-gray-300 py-2 
            ${active ? "hover:bg-sky-500" : "disabled"}`}
@@ -117,21 +117,21 @@ const ActionBar: NextPage<Props> = (props) => {
       >
         <div
           className={`text-center ${
-            asVertical && "flex flex-row space-x-1 w-fit"
+            asVertical && "flex flex-row space-x-1 w-fit py-2"
           }  ${
             active
-              ? "hover:font-semibold hover:animate-pulse hover:cursor-pointer hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
+              ? "group-hover:font-semibold group-hover:animate-pulse group-hover:cursor-pointer group-hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
               : "text-slate-500"
           } `}
         >
-          <AiFillPlusCircle className="mx-auto" size="30" />
+          <AiOutlinePlus className="mx-auto" size="30" />
           <span className="text-gray-700 dark:text-gray-200 font-semibold my-auto">
             Connect
           </span>
         </div>
       </div>
       <div
-        className={`flex-1 ${
+        className={`group flex-1 ${
           !asVertical ? "border-r" : "w-full px-2"
         } border-gray-500 dark:border-gray-300 py-2 ${
           active ? "hover:bg-green-500" : "disabled"
@@ -139,38 +139,38 @@ const ActionBar: NextPage<Props> = (props) => {
       >
         <div
           className={`text-center ${
-            asVertical && "flex flex-row space-x-1 w-fit"
+            asVertical && "flex flex-row space-x-1 w-fit py-2"
           }   ${
             active
-              ? "hover:font-semibold hover:animate-pulse hover:cursor-pointer hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
+              ? "group-hover:font-semibold group-hover:animate-pulse group-hover:cursor-pointer group-hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
               : "text-slate-500"
           } `}
           onClick={() => actionHandler(ActionEnum.Receive)}
         >
-          <AiFillDownCircle className="mx-auto" size="30" />
+          <AiOutlineArrowDown className="mx-auto rotate-45" size="30" />
           <span className="text-gray-700 dark:text-gray-200 font-semibold">
             Receive
           </span>
         </div>
       </div>
       <div
-        className={`flex-1 py-2 ${
+        className={`group flex-1 py-2 ${
           !asVertical
             ? "border-r rounded-tr-xl rounded-br-xl"
-            : "w-full rounded-br-xl rounded-bl-xl px-2"
+            : "w-full rounded-br-xl rounded-bl-xl px-2 py-2"
         } ${active ? "hover:bg-sky-500" : "disabled"}`}
         onClick={() => actionHandler(ActionEnum.Send)}
       >
         <div
           className={`text-center ${
-            asVertical && "flex flex-row space-x-1 w-fit"
+            asVertical && "flex flex-row space-x-1 w-fit py-2"
           }   ${
             active
-              ? "hover:font-semibold hover:animate-pulse hover:cursor-pointer hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
+              ? "group-hover:font-semibold group-hover:animate-pulse group-hover:cursor-pointer group-hover:text-sky-800 text-sky-500 transition duration-300 ease-in-out"
               : "text-slate-500"
           } `}
         >
-          <AiFillUpCircle className="mx-auto" size="30" />
+          <AiOutlineArrowUp className="mx-auto rotate-45" size="30" />
           <span className="text-gray-700 dark:text-gray-200 font-semibold">
             Send
           </span>
