48779861e9d81e72172efd4f0f73ed95aa3ee596
jettblu <jettblu.fabrications@gmail.com>
2023-08-14 19:01:41 -0700

    Fix main button color with light theme

--- components/navbars/NavbarUnknownVisitor.tsx
+++ components/navbars/NavbarUnknownVisitor.tsx
@@ -4,7 +4,7 @@ import Menu, { MenuItem } from "../menu/menu";
 
 export default function NavbarUnknownVisitor() {
   const mainButtonClassName =
-    "p-2 lg:px-4 md:mx-2 md:text-center md:border md:border-solid border-gray-300 dark:border-gray-600 dark:hover:border-sky-200 rounded-full hover:cursor-pointer hover:bg-green-400 text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1";
+    "p-2 lg:px-4 md:mx-2 md:text-center md:border md:border-solid border-gray-300 dark:border-gray-600 dark:hover:border-sky-200 rounded-full hover:cursor-pointer hover:bg-green-400 text-black dark:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1";
   const router = useRouter();
   return (
     <Menu>
