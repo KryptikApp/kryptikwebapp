721cbceb15129c3690ea8660c4cdaff60e26d906
jettblu <jettblu.fabrications@gmail.com>
2023-08-23 01:45:24 -0700

    Update profile list loader

--- components/lists/ListProfiles.tsx
+++ components/lists/ListProfiles.tsx
@@ -10,30 +10,34 @@ export default function ListProfiles() {
     null
   );
   const { kryptikService } = useKryptikAuthContext();
+  const [failed, setFailed] = useState(false);
 
   async function getProfiles() {
-    const maxRetries = 20;
+    const maxRetries = 25;
     let retryCount = 0;
     while (retryCount < maxRetries) {
       try {
         console.log("loading top profiles...");
         const newTopAccounts = await loadTopProfiles(kryptikService);
         setTopProfiles(newTopAccounts);
+        setFailed(false);
         return;
       } catch (e) {
-        console.warn("retrying");
+        console.warn(e);
+        console.log(`retrying... ${retryCount}`);
         // wait .25 second
         await new Promise((r) => setTimeout(r, 250));
         retryCount++;
       }
     }
+    setFailed(true);
   }
 
   const loadingQuantity = 6;
 
   useEffect(() => {
     console.log("Running effect");
-    if (topProfiles && topProfiles.length > 0) return;
+    if (kryptikService.NetworkDbs.length == 0) return;
     if (kryptikService.serviceState != ServiceState.started) {
       kryptikService.StartSevice();
       return;
@@ -49,6 +53,11 @@ export default function ListProfiles() {
         Array(loadingQuantity)
           .fill(0)
           .map((_) => <PillProfileLoading key={_} />)}
+      {failed && (
+        <div className="text-center text-gray-500 dark:text-gray-400">
+          Failed to load profiles.
+        </div>
+      )}
     </div>
   );
 }
