1f573ba6ccde91c7b9a410116ca2e1a3dc488e1f
jettblu <jettblu.fabrications@gmail.com>
2023-08-24 10:22:03 -0700

    Add contract type converter

--- src/contracts/types.ts
+++ src/contracts/types.ts
@@ -1,10 +1,10 @@
+import { AppContract, NetworkDb } from "@prisma/client";
 import { Web3App } from "../explore/apps/types";
 
 export interface IContract {
   address: string;
   networkTicker: string;
-  stats: Stats;
-  numberOfTransactions: number;
+  stats?: Stats;
   appMetaData: Web3App;
 }
 
@@ -13,3 +13,31 @@ export type Stats = {
   lastBlockChecked: number;
   updatedAt: number;
 };
+
+/**
+ *
+ * @param contractDb contract model from db with network data included
+ * @returns
+ */
+export function contractFromDb(
+  contractDb: AppContract & { NetworkDb: NetworkDb }
+): IContract {
+  // parse contract data
+  const contract: IContract = {
+    address: contractDb.address,
+    networkTicker: contractDb.NetworkDb.ticker,
+    appMetaData: {
+      name: contractDb.name,
+      description: contractDb.description,
+      url: contractDb.url,
+      icon: contractDb.icon,
+      tags: contractDb.tags,
+    },
+    stats: {
+      totalTransactionsLastHour: contractDb.totalTransactionsLastHour,
+      lastBlockChecked: contractDb.lastBlockChecked,
+      updatedAt: contractDb.updatedAt.getTime(),
+    },
+  };
+  return contract;
+}
