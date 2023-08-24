873d0be1de49781e3080dd8a1ec0d1de9f2bcd58
jettblu <jettblu.fabrications@gmail.com>
2023-08-23 23:11:00 -0700

    Add eth blocks per hour constant

--- src/constants/evmConstants.ts
+++ src/constants/evmConstants.ts
@@ -2,3 +2,6 @@ export const ETH_CONTRACT_ADDRESS: string =
   "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 export const EVM_NULL_ADDRESS: string =
   "0x0000000000000000000000000000000000000000";
+
+// assuming <15s block time and 7158 blocks per day
+export const ETH_NUM_BLOCKS_PER_HOUR: number = 298;
