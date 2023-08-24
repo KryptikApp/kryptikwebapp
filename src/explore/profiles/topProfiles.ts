2de7dd297e58036610fb088aa243677a849b8d20
jettblu <jettblu.fabrications@gmail.com>
2023-08-23 19:41:20 -0700

    Add solana top profiles

--- src/explore/profiles/topProfiles.ts
+++ src/explore/profiles/topProfiles.ts
@@ -20,12 +20,6 @@ export const topProfiles: IResolvedAccount[] = [
   },
   {
     address: "",
-    names: ["tombeiko.eth"],
-    isResolved: false,
-    networkTicker: "eth",
-  },
-  {
-    address: "",
     names: ["ric.eth"],
     isResolved: false,
     networkTicker: "eth",
@@ -36,19 +30,33 @@ export const topProfiles: IResolvedAccount[] = [
     isResolved: false,
     networkTicker: "eth",
   },
-
   {
-    address: "",
-    names: ["jettblu.eth"],
-    isResolved: false,
-    networkTicker: "eth",
+    address: "2gRW6SUFnib7q3Z2Mucrn2GrnJ4JQEFV8oup8Ed4pSwu",
+    names: ["👻.sol"],
+    isResolved: true,
+    networkTicker: "sol",
+    avatarPath: "/media/avatars/community/ghost.jpg",
   },
+  {
+    address: "83qm84m1sgYunVmRKciGxbpVtFVCF2ATZGuTFwjoDcoV",
+    isResolved: true,
+    networkTicker: "sol",
+    avatarPath: "/media/avatars/community/8k4jj.svg",
+  },
+  // {
+  //   address: "",
+  //   isResolved: true,
+  //   names: ["betaso.near"],
+  //   networkTicker: "near",
+  //   avatarPath: "/media/avatars/community/betaso.jpg",
+  // },
 ];
 
 export async function loadTopProfiles(ks: Web3Service) {
   // run parallel request to resolve all top profiles
   const res = await Promise.all(
     topProfiles.map((p) => {
+      if (p.isResolved) return Promise.resolve(p);
       const networkDb: NetworkDb =
         ks.getNetworkDbByTicker(p.networkTicker || defaultNetworkDb.ticker) ||
         defaultNetworkDb;
