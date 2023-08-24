3a6d77d9497d3cd342b17555081ada1e3f5756db
jettblu <jettblu.fabrications@gmail.com>
2023-08-24 10:19:44 -0700

    Update contract metadata

--- src/explore/apps/topApps.ts
+++ src/explore/apps/topApps.ts
@@ -1,46 +1,54 @@
 import { Web3App } from "./types";
 
+export const UNISWAP_META = {
+  name: "Uniswap",
+  description: "Trade tokens",
+  url: "https://app.uniswap.org/",
+  icon: "/apps/uniswap.webp",
+  tags: ["exchange"],
+};
+
+export const POOLTOGETHER_META = {
+  name: "PoolTogether",
+  description: "No-loss prize games",
+  url: "https://app.pooltogether.com/",
+  icon: "/apps/poolTogether.webp",
+  tags: ["games"],
+};
+export const SABLIER_META = {
+  name: "Sablier",
+  description: "Stream money in real-time",
+  url: "https://app.sablier.finance/",
+  icon: "/apps/sablier.webp",
+  tags: ["payments"],
+};
+export const ENS_META = {
+  name: "Ethereum Name Service",
+  description: "Decentralized naming for wallets, websites, & more",
+  url: "https://app.ens.domains/",
+  icon: "/apps/ens.webp",
+  tags: ["domains"],
+};
+export const AAVE_META = {
+  name: "Aave",
+  description: "Lend and borrow with variable interest rates",
+  url: "https://aave.com/",
+  icon: "/apps/aave.webp",
+  tags: ["defi"],
+};
+export const ZAPPER_META = {
+  name: "Zapper",
+  description: "Track and manage DeFi assets",
+  url: "https://zapper.fi/",
+  icon: "/apps/zapper.webp",
+  tags: ["dashboard"],
+};
+
 export const topApps: Web3App[] = [
-  {
-    name: "Uniswap",
-    description: "Trade tokens",
-    url: "https://app.uniswap.org/",
-    icon: "/apps/uniswap.webp",
-    tags: ["exchange"],
-  },
-  {
-    name: "PoolTogether",
-    description: "No-loss prize games",
-    url: "https://app.pooltogether.com/",
-    icon: "/apps/poolTogether.webp",
-    tags: ["games"],
-  },
-  {
-    name: "Sablier",
-    description: "Stream money in real-time",
-    url: "https://app.sablier.finance/",
-    icon: "/apps/sablier.webp",
-    tags: ["payments"],
-  },
-  {
-    name: "Ethereum Name Service",
-    description: "Decentralized naming for wallets, websites, & more",
-    url: "https://app.ens.domains/",
-    icon: "/apps/ens.webp",
-    tags: ["domains"],
-  },
-  {
-    name: "Aave",
-    description: "Lend and borrow with variable interest rates",
-    url: "https://aave.com/",
-    icon: "/apps/aave.webp",
-    tags: ["defi"],
-  },
-  {
-    name: "Zapper",
-    description: "Track and manage DeFi assets",
-    url: "https://zapper.fi/",
-    icon: "/apps/zapper.webp",
-    tags: ["dashboard"],
-  },
+  UNISWAP_META,
+  POOLTOGETHER_META,
+  SABLIER_META,
+  ENS_META,
+  AAVE_META,
+  ZAPPER_META,
 ];
