803355707059c21731cba2c73fb234a55add6265
jettblu <jettblu.fabrications@gmail.com>
2023-08-24 23:18:33 -0700

    Update trending app display

--- components/lists/ListTrendingApps.tsx
+++ components/lists/ListTrendingApps.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { IContract } from "../../src/contracts/types";
 import Image from "next/image";
 import { getAppContracts } from "../../src/requests/contracts";
+import { useKryptikAuthContext } from "../KryptikAuthProvider";
+import { NetworkDb } from "@prisma/client";
+import { ServiceState } from "../../src/services/types";
 
 export default function ListTrendingApps() {
   const [loading, setLoading] = useState(true);
@@ -30,8 +33,9 @@ export default function ListTrendingApps() {
   return (
     <div className="flex flex-col space-y-4 w-full px-2">
       {loading &&
-        !failed &&
-        Array(loadingQuantity).map((_) => <LoadingCard key={_} />)}
+        Array(loadingQuantity)
+          .fill(0)
+          .map((_) => <LoadingCard key={_} />)}
       {failed && (
         <div className="text-center text-gray-500 dark:text-gray-400">
           Failed to load apps.
@@ -47,44 +51,73 @@ export default function ListTrendingApps() {
 
 function AppCard(props: { contract: IContract; index: number }) {
   const { contract, index } = { ...props };
-  const tagLine = contract.stats
-    ? `Used ${contract.stats.totalTransactionsLastHour} time${
-        contract.stats.totalTransactionsLastHour != 1 ? "s" : ""
-      } in the last hour.`
-    : contract.appMetaData.description;
+  const [networkDb, setNetworkDb] = useState<NetworkDb | null>(null);
+  const { kryptikService } = useKryptikAuthContext();
+  const tagLine = getTagline(contract);
+  useEffect(() => {
+    if (kryptikService.serviceState != ServiceState.started) {
+      return;
+    }
+    try {
+      const newNetorkDb = kryptikService.getNetworkDbByTicker(
+        contract.networkTicker
+      );
+      setNetworkDb(newNetorkDb);
+    } catch (e: any) {
+      console.log("failed to get network db");
+    }
+  }, [kryptikService.serviceState]);
+
   return (
-    <div className="h-[120px] rounded-md flex flex-row px-2 py-3 dark:bg-gray-800/10 py-1 ring-[1px] ring-gray-500/10 border border-gray-400/10 hover:border-green-400/20 transform-color duration-300 bg-gray-200/30 dark:bg-gray-700/30 hover:brightness-105 relative hover:ring-[3px] hover:cursor-pointer">
-      <div className="">
-        <Image
-          className="float-left pl-2 rounded-md bg-gradient-to-r from-sky-400/10 to-indigo-500/10 background-animate px-2 py-2"
-          src={contract.appMetaData.icon}
-          alt={`${contract.appMetaData.name} image`}
-          width={50}
-          height={50}
-        />
-      </div>
-      <div className="flex flex-col px-2">
-        <h1 className="text-2xl font-semibold text-left text-gray-800 dark:text-white">
-          {contract.appMetaData.name}
-        </h1>
-        <p className="text-left text-gray-500 dark:text-gray-400 text-xl">
-          {tagLine}
-        </p>
-        <div className="flex flex-row flex-wrap justify-right space-x-2">
-          {contract.appMetaData.tags.map((tag) => (
-            <div
-              key={tag}
-              className="px-2 py-1 mt-2 text-sm text-gray-700 bg-sky-200/20 rounded-md dark:bg-gray-700/20 dark:text-gray-400 h-fit ring-gray-500/10 ring-1"
-            >
-              {tag}
-            </div>
-          ))}
+    <a
+      href={contract.appMetaData.url}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="group h-[120px] rounded-md flex flex-row px-2 py-3 dark:bg-gray-800/10 py-1 ring-[1px] ring-gray-500/10 border border-gray-400/10 hover:border-green-400/20 transform-color duration-300 bg-gray-200/30 dark:bg-gray-700/30 hover:brightness-105 relative hover:ring-[3px] hover:cursor-pointer">
+        <div className="">
+          <Image
+            className="float-left pl-2 rounded-md bg-gradient-to-r from-sky-400/10 to-indigo-500/10 background-animate px-2 py-2"
+            src={contract.appMetaData.icon}
+            alt={`${contract.appMetaData.name} image`}
+            width={50}
+            height={50}
+          />
         </div>
+        <div className="flex flex-col px-2">
+          <h1 className="text-2xl font-semibold text-left text-gray-800 dark:text-white">
+            {contract.appMetaData.name}
+          </h1>
+          <p className="text-left text-gray-500 dark:text-gray-400 text-xl">
+            {tagLine}
+          </p>
+          <div className="flex flex-row flex-wrap justify-right space-x-2">
+            {contract.appMetaData.tags.map((tag) => (
+              <div
+                key={tag}
+                className="px-2 py-1 mt-2 text-sm text-gray-700 bg-sky-200/20 rounded-md dark:bg-gray-700/20 dark:text-gray-400 h-fit ring-gray-500/10 ring-1"
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="p-1 font-semibold absolute right-4 top-2 text-gray-500">
+          {index + 1}
+        </div>
+        {networkDb && (
+          <div className="absolute bottom-2 right-2 text-gray-500">
+            <img
+              src={networkDb.iconPath}
+              alt={`${networkDb.ticker} icon`}
+              width={20}
+              height={20}
+              className="grayscale group-hover:grayscale-0 opacity-25 group-hover:opacity-100"
+            />
+          </div>
+        )}
       </div>
-      <div className="p-1 font-semibold absolute right-4 top-2 text-gray-500">
-        {index + 1}
-      </div>
-    </div>
+    </a>
   );
 }
 
@@ -101,4 +134,16 @@ function LoadingCard() {
       <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
     </div>
   );
+}
+
+function getTagline(contract: IContract) {
+  if (!contract.stats) return contract.appMetaData.description;
+  if (contract.appMetaData.name.includes("name service")) {
+    return `Registered ${contract.stats.totalTransactionsLastHour} name${
+      contract.stats.totalTransactionsLastHour != 1 ? "s" : ""
+    } in the last hour.`;
+  }
+  return `Used ${contract.stats.totalTransactionsLastHour} time${
+    contract.stats.totalTransactionsLastHour != 1 ? "s" : ""
+  } in the last hour.`;
 }
